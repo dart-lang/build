e2d96caa683e381d2c56eaa43c7e7e7cec95b6ae
deimon <fibulwinter@gmail.com>
2014-07-14 21:45:45 +1000

    Upload to GitHub

---
+++ builder_pkgs/mockito/lib/mockito.dart
@@ -0,0 +1,369 @@
+library mockito;
+
+import 'package:matcher/matcher.dart';
+import 'dart:mirrors';
+
+bool _whenInProgress = false; 
+bool _verificationInProgress = false; 
+_WhenCall _whenCall=null;
+List<_VerifyCall> _verifyCalls = [];
+_TimeStampProvider _timer = new _TimeStampProvider();
+List _capturedArgs = [];
+
+class Mock {
+  List<RealCall> realCalls = [];
+  List<CannedResponse> _responses = [];
+  String _givenName = null;
+  int _givenHashCode = null;
+  var _defaultResponse = ()=>new CannedResponse(null, (_)=>null);
+  
+  void _setExpected(CannedResponse cannedResponse){
+    _responses.add(cannedResponse);
+  }
+  
+  dynamic noSuchMethod(Invocation invocation){
+    if(_whenInProgress){
+      _whenCall = new _WhenCall(this, invocation);
+      return null;
+    }else if(_verificationInProgress){
+      _verifyCalls.add(new _VerifyCall(this, invocation));
+      return null;
+    }else{
+      realCalls.add(new RealCall(this, invocation));
+      var cannedResponse = _responses.lastWhere((cr)=>cr.matcher.matches(invocation), 
+          orElse: _defaultResponse);
+      var response = cannedResponse.response(invocation);
+      return response;
+    }
+  }
+
+  int get hashCode => _givenHashCode==null ? super.hashCode : _givenHashCode;
+  
+  bool operator ==(other) => (_givenHashCode!=null && other is Mock) 
+      ? _givenHashCode==other._givenHashCode : super==other;
+
+  String toString() => _givenName != null ? _givenName : runtimeType.toString();  
+}
+
+named(dynamic mock, {String name, int hashCode}) => mock
+  .._givenName=name.._givenHashCode = hashCode;
+
+reset(var mock){
+  mock.realCalls.clear();
+  mock._responses.clear();
+}
+
+clearInteractions(var mock){
+  mock.realCalls.clear();
+}
+
+class PostExpectation {  
+  thenReturn(expected){
+    return _completeWhen((_)=>expected);
+  }
+  
+  thenAnswer(Answering answer){
+    return _completeWhen(answer);
+  }
+
+  _completeWhen(Answering answer) {
+    _whenCall._setExpected(answer);
+    var mock = _whenCall.mock;
+    _whenCall = null;
+    _whenInProgress=false;
+    return mock;
+  }
+}
+
+class InvocationMatcher {
+  Invocation roleInvocation;
+  
+  InvocationMatcher(this.roleInvocation);
+  
+  bool matches(Invocation invocation){
+    var isMatching = _isMethodMatches(invocation) && _isArgumentsMatches(invocation);
+    if(isMatching){
+      _captureArguments(invocation);
+    }
+    return isMatching;
+  }
+
+  bool _isMethodMatches(Invocation invocation) {
+    if(invocation.memberName!=roleInvocation.memberName){
+      return false;
+    }
+    if((invocation.isGetter!=roleInvocation.isGetter)
+        || (invocation.isSetter!=roleInvocation.isSetter)
+        || (invocation.isMethod!=roleInvocation.isMethod)
+        ){
+      return false;  
+    }
+    return true;
+  }
+
+  void _captureArguments(Invocation invocation) {
+    int index = 0;
+    for(var roleArg in roleInvocation.positionalArguments){
+      var actArg = invocation.positionalArguments[index];
+      if(roleArg is _ArgMatcher && roleArg._capture){
+        _capturedArgs.add(actArg);
+      }
+      index++;
+    }
+    for(var roleKey in roleInvocation.namedArguments.keys){
+      var roleArg = roleInvocation.namedArguments[roleKey];
+      var actArg = invocation.namedArguments[roleKey];
+      if(roleArg is _ArgMatcher){
+        if(roleArg is _ArgMatcher && roleArg._capture){
+          _capturedArgs.add(actArg);
+        }
+      }
+    }
+  }
+
+  bool _isArgumentsMatches(Invocation invocation) {
+    if(invocation.positionalArguments.length!=roleInvocation.positionalArguments.length){
+      return false;
+    }
+    if(invocation.namedArguments.length!=roleInvocation.namedArguments.length){
+      return false;
+    }
+    int index=0;
+    for(var roleArg in roleInvocation.positionalArguments){
+      var actArg = invocation.positionalArguments[index];
+      if(!isMatchingArg(roleArg, actArg)){
+        return false;
+      }
+      index++;
+    }
+    for(var roleKey in roleInvocation.namedArguments.keys){
+      var roleArg = roleInvocation.namedArguments[roleKey];
+      var actArg = invocation.namedArguments[roleKey];
+      if(!isMatchingArg(roleArg, actArg)){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool isMatchingArg(roleArg, actArg) {
+    if(roleArg is _ArgMatcher){
+      return roleArg._matcher==null || roleArg._matcher.matches(actArg, null);
+//    } else if(roleArg is Mock){
+//      return identical(roleArg, actArg);
+    }else{  
+      return roleArg == actArg;
+    }
+  }
+}
+
+class CannedResponse{
+  InvocationMatcher matcher;
+  Answering response;
+  
+  CannedResponse(this.matcher, this.response);  
+}
+
+class _TimeStampProvider{
+  int _now = 0;
+  DateTime now(){
+    var candidate = new DateTime.now();
+    if(candidate.millisecondsSinceEpoch<=_now){
+      candidate = new DateTime.fromMillisecondsSinceEpoch(_now+1);
+    }
+    _now = candidate.millisecondsSinceEpoch;
+    return candidate;
+  }
+}
+
+class RealCall{
+  DateTime _timeStamp;
+  final Mock mock;
+  final Invocation invocation;
+  bool verified = false;
+  RealCall(this.mock, this.invocation){
+    _timeStamp =  _timer.now();
+  }
+  
+  DateTime get timeStamp => _timeStamp;
+  
+  String toString(){
+    var verifiedText = verified ? "[VERIFIED] " : "";
+    List<String> posArgs = invocation.positionalArguments.map((v)=>v==null?"null":v.toString()).toList();
+    List<String> mapArgList = invocation.namedArguments.keys.map((key){
+      return "${MirrorSystem.getName(key)}: ${invocation.namedArguments[key]}";
+    }).toList(growable: false);
+    if(mapArgList.isNotEmpty){
+      posArgs.add("{${mapArgList.join(", ")}}");      
+    }
+    String args = posArgs.join(", ");
+    String method = MirrorSystem.getName(invocation.memberName);    
+    if(invocation.isMethod){
+      method = ".$method($args)";
+    }else if(invocation.isGetter){
+      method = ".$method";
+    }else{
+      method = ".$method=$args";
+    }
+    return "$verifiedText$mock$method";
+  }
+}
+
+class _WhenCall {
+  Mock mock;
+  Invocation whenInvocation;
+  _WhenCall(this.mock, this.whenInvocation);
+  
+  void _setExpected(Answering answer){
+    mock._setExpected(new CannedResponse(new InvocationMatcher(whenInvocation), answer));
+  }
+}
+
+class _VerifyCall {
+  Mock mock;
+  Invocation verifyInvocation;
+  List<RealCall> matchingInvocations;
+  
+  _VerifyCall(this.mock, this.verifyInvocation){
+    var expectedMatcher = new InvocationMatcher(verifyInvocation);
+    matchingInvocations = mock.realCalls.where((RealCall recordedInvocation){
+      return !recordedInvocation.verified && expectedMatcher.matches(recordedInvocation.invocation);}).toList();
+  }
+  
+  _findAfter(DateTime dt){
+    return matchingInvocations.firstWhere((inv)=>!inv.verified && inv.timeStamp.isAfter(dt), orElse: ()=>null);
+  }
+  
+  _checkWith(bool never){
+    if(!never && matchingInvocations.isEmpty){
+      var otherCallsText = "";
+      if(mock.realCalls.isNotEmpty){ 
+        otherCallsText = " All calls: ";
+      }
+      var calls = mock.realCalls.join(", ");
+      fail("No matching calls.$otherCallsText$calls");
+    }
+    if(never && matchingInvocations.isNotEmpty){
+      var calls = mock.realCalls.join(", ");
+      fail("Unexpected calls. All calls: $calls");
+    }
+    matchingInvocations.forEach((inv){inv.verified=true;});
+  }
+}
+
+class _ArgMatcher{
+  Matcher _matcher;
+  bool _capture;
+  
+  _ArgMatcher(this._matcher, this._capture);
+}
+
+
+get any => new _ArgMatcher(null, false);
+get captureAny => new _ArgMatcher(null, true);
+captureThat(Matcher matcher) => new _ArgMatcher(matcher, true);
+argThat(Matcher matcher) => new _ArgMatcher(matcher, false);
+
+class VerificationResult{
+  List captured = [];
+  int callCount;
+  
+  VerificationResult(this.callCount){
+    captured = new List.from(_capturedArgs, growable: false);
+    _capturedArgs.clear();
+  }
+  
+  void called(matcher){
+    expect(callCount, wrapMatcher(matcher), reason: "Unexpected number of calls");
+  }
+}
+
+typedef dynamic Answering(Invocation realInvocation);
+
+typedef VerificationResult Verification(matchingInvocations);
+
+typedef void InOrderVerification(recordedInvocations);
+
+Verification get verifyNever => _makeVerify(true);
+
+Verification get verify => _makeVerify(false);
+
+Verification _makeVerify(bool never) {
+  if(_verifyCalls.isNotEmpty){
+    throw new StateError(_verifyCalls.join());
+  }
+  _verificationInProgress = true;
+  return (mock){
+    _verificationInProgress = false;
+    if(_verifyCalls.length==1){
+      _VerifyCall verifyCall = _verifyCalls.removeLast();
+      var result = new VerificationResult(verifyCall.matchingInvocations.length);
+      verifyCall._checkWith(never);
+      return result;
+    }else{
+      fail("Used on non-mockito");
+    }
+  };
+}
+
+InOrderVerification get verifyInOrder {
+  if(_verifyCalls.isNotEmpty){
+    throw new StateError(_verifyCalls.join());
+  }
+  _verificationInProgress = true;
+  return (verifyCalls){
+    _verificationInProgress = false;
+    DateTime dt = new DateTime.fromMillisecondsSinceEpoch(0);
+    var tmpVerifyCalls = new List.from(_verifyCalls);
+    _verifyCalls.clear();
+    List<RealCall> matchedCalls = [];
+    for(_VerifyCall verifyCall in tmpVerifyCalls){
+      RealCall matched = verifyCall._findAfter(dt);
+      if(matched!=null){
+        matchedCalls.add(matched);
+        dt=matched.timeStamp;
+      }else{
+        Set<Mock> mocks = tmpVerifyCalls.map((_VerifyCall vc)=>vc.mock).toSet();
+        List<RealCall> allInvocations = mocks.expand((m)=>m.realCalls).toList(growable: false);
+        allInvocations.sort((inv1, inv2)=>inv1.timeStamp.compareTo(inv2.timeStamp));
+        String otherCalls = "";
+        if(allInvocations.isNotEmpty){
+          otherCalls = " All calls: ${allInvocations.join(", ")}";
+        }
+        fail("Matching call #${tmpVerifyCalls.indexOf(verifyCall)} not found.$otherCalls");
+      }
+    }
+    matchedCalls.forEach((rc){rc.verified=true;});
+  };
+}
+
+verifyNoMoreInteractions(var mock) {
+  var unverified = mock.realCalls.where((inv)=>!inv.verified).toList();
+  if(unverified.isNotEmpty){
+    fail("No more calls expected, but following found: "+unverified.join());
+  }  
+}
+
+verifyZeroInteractions(var mock) {
+  if(mock.realCalls.isNotEmpty){
+    fail("No interaction expected, but following found: "+mock.realCalls.join());
+  }
+}
+
+typedef PostExpectation Expectation(x);
+
+Expectation get when {
+  _whenInProgress = true;
+  return (_){
+    _whenInProgress = false;
+    return new PostExpectation();
+  };
+}
+
+logInvocations(List<Mock> mocks){
+  List<RealCall> allInvocations = mocks.expand((m)=>m.realCalls).toList(growable: false);
+  allInvocations.sort((inv1, inv2)=>inv1.timeStamp.compareTo(inv2.timeStamp));
+  allInvocations.forEach((inv){
+    print(inv.toString()); 
+  });
+}
---
+++ builder_pkgs/mockito/test/mockitoSpec.dart
@@ -0,0 +1,354 @@
+import 'package:unittest/unittest.dart';
+import '../lib/mockito.dart';
+
+class RealClass {
+  String methodWithoutArgs() => "Real";  
+  String methodWithNormalArgs(int x) => "Real";  
+  String methodWithPositionalArgs(int x, [int y]) => "Real";  
+  String methodWithNamedArgs(int x, {int y}) => "Real";  
+  String get getter => "Real";  
+  void set setter(String arg){
+    throw new StateError("I must be mocked");
+  }  
+}
+
+class MockedClass extends Mock implements RealClass{}
+
+expectFail(String expectedMessage, expectedToFail()){
+  try{
+    expectedToFail();
+    fail("It was expected to fail!");
+  }catch(e){
+    if(!(e is TestFailure)){
+      throw e;
+    }else{
+      if(expectedMessage!=e.message){
+        throw new TestFailure("Failed, but with wrong message: ${e.message}");
+      }
+    }
+  }
+}
+
+main(){
+  RealClass mock;
+  
+  setUp((){
+    mock = new MockedClass();
+  });
+  
+  group("when()", (){
+    test("should mock method without args", (){
+      when(mock.methodWithoutArgs()).thenReturn("A");
+      expect(mock.methodWithoutArgs(), equals("A"));
+    });  
+    test("should mock method with normal args", (){
+      when(mock.methodWithNormalArgs(42)).thenReturn("Ultimate Answer");
+      expect(mock.methodWithNormalArgs(43), isNull);
+      expect(mock.methodWithNormalArgs(42), equals("Ultimate Answer"));
+    });  
+    test("should mock method with positional args", (){
+      when(mock.methodWithPositionalArgs(42, 17)).thenReturn("Answer and...");
+      expect(mock.methodWithPositionalArgs(42), isNull);
+      expect(mock.methodWithPositionalArgs(42, 18), isNull);
+      expect(mock.methodWithPositionalArgs(42, 17), equals("Answer and..."));
+    });  
+    test("should mock method with named args", (){
+      when(mock.methodWithNamedArgs(42, y: 17)).thenReturn("Why answer?");
+      expect(mock.methodWithNamedArgs(42), isNull);
+      expect(mock.methodWithNamedArgs(42, y:18), isNull);
+      expect(mock.methodWithNamedArgs(42, y:17), equals("Why answer?"));
+    });  
+    test("should mock method with argument matcher", (){
+      when(mock.methodWithNormalArgs(argThat(greaterThan(100)))).thenReturn("A lot!");
+      expect(mock.methodWithNormalArgs(100), isNull);
+      expect(mock.methodWithNormalArgs(101), equals("A lot!"));
+    });  
+    test("should mock method with any argument matcher", (){
+      when(mock.methodWithNormalArgs(any)).thenReturn("A lot!");
+      expect(mock.methodWithNormalArgs(100), equals("A lot!"));
+      expect(mock.methodWithNormalArgs(101), equals("A lot!"));
+    });  
+    test("should mock method with mix of argument matchers and real things", (){
+      when(mock.methodWithPositionalArgs(argThat(greaterThan(100)), 17)).thenReturn("A lot with 17");
+      expect(mock.methodWithPositionalArgs(100, 17), isNull);
+      expect(mock.methodWithPositionalArgs(101, 18), isNull);
+      expect(mock.methodWithPositionalArgs(101, 17), equals("A lot with 17"));
+    });  
+    test("should mock getter", (){
+      when(mock.getter).thenReturn("A");
+      expect(mock.getter, equals("A"));
+    });
+    test("should mock hashCode", (){
+      named(mock, hashCode: 42);
+      expect(mock.hashCode, equals(42));
+    });
+    test("should have hashCode when it is not mocked", (){
+      expect(mock.hashCode, isNotNull);
+    });
+//    test("should n't mock toString", (){
+//      when(mock.toString()).thenReturn("meow");
+//      expect(mock.toString(), equals("meow"));
+//    });
+    test("should have default toString when it is not mocked", (){
+      expect(mock.toString(), equals("MockedClass"));
+    });
+    test("should have toString as name when it is not mocked", (){
+      named(mock, name: "Cat");
+      expect(mock.toString(), equals("Cat"));
+    });
+    test("should mock equals between mocks when givenHashCode is equals", (){
+      var anotherMock = named(new MockedClass(), hashCode: 42);
+      named(mock, hashCode: 42);
+      expect(mock==anotherMock, isTrue);
+    });
+    test("should use identical equality between it is not mocked", (){
+      var anotherMock = new MockedClass();
+      expect(mock==anotherMock, isFalse);
+      expect(mock==mock, isTrue);
+    });
+    //no need to mock setter, except if we will have spies later...
+    test("should mock method with calculated result", (){
+      when(mock.methodWithNormalArgs(any)).thenAnswer((Invocation inv)=>inv.positionalArguments[0].toString());
+      expect(mock.methodWithNormalArgs(43), equals("43"));
+      expect(mock.methodWithNormalArgs(42), equals("42"));
+    });  
+    test("should return mock to make simple oneline mocks", (){
+      RealClass mockWithSetup = when(new MockedClass().methodWithoutArgs()).thenReturn("oneline");
+      expect(mockWithSetup.methodWithoutArgs(), equals("oneline"));
+    });  
+    test("should use latest matching when definition", (){
+      when(mock.methodWithoutArgs()).thenReturn("A");
+      when(mock.methodWithoutArgs()).thenReturn("B");
+      expect(mock.methodWithoutArgs(), equals("B"));
+    });  
+    
+  });
+
+  group("verify()", (){
+    test("should verify method without args", (){
+      mock.methodWithoutArgs();
+      verify(mock.methodWithoutArgs());
+    });  
+    test("should verify method with normal args", (){
+      mock.methodWithNormalArgs(42);
+      expectFail("No matching calls. All calls: MockedClass.methodWithNormalArgs(42)", (){ 
+        verify(mock.methodWithNormalArgs(43)); 
+      });
+      verify(mock.methodWithNormalArgs(42));
+    });  
+    test("should mock method with positional args", (){
+      mock.methodWithPositionalArgs(42, 17);
+      expectFail("No matching calls. All calls: MockedClass.methodWithPositionalArgs(42, 17)", (){ 
+        verify(mock.methodWithPositionalArgs(42)); 
+      });
+      expectFail("No matching calls. All calls: MockedClass.methodWithPositionalArgs(42, 17)", (){
+        verify(mock.methodWithPositionalArgs(42, 18)); 
+      });
+      verify(mock.methodWithPositionalArgs(42, 17));
+    });  
+    test("should mock method with named args", (){
+      mock.methodWithNamedArgs(42, y: 17);
+      expectFail("No matching calls. All calls: MockedClass.methodWithNamedArgs(42, {y: 17})", (){
+        verify(mock.methodWithNamedArgs(42)); 
+      });
+      expectFail("No matching calls. All calls: MockedClass.methodWithNamedArgs(42, {y: 17})", (){
+        verify(mock.methodWithNamedArgs(42, y:18)); 
+      });
+      verify(mock.methodWithNamedArgs(42, y:17));
+    });  
+    test("should mock method with argument matcher", (){
+      mock.methodWithNormalArgs(100);
+      expectFail("No matching calls. All calls: MockedClass.methodWithNormalArgs(100)", (){
+        verify(mock.methodWithNormalArgs(argThat(greaterThan(100)))); 
+      });
+      verify(mock.methodWithNormalArgs(argThat(greaterThanOrEqualTo(100))));
+    });  
+    test("should mock method with argument capturer", (){
+      mock.methodWithNormalArgs(50);
+      mock.methodWithNormalArgs(100);
+      expect(verify(mock.methodWithNormalArgs(captureAny)).captured, equals([50, 100]));
+    });  
+    test("should mock method with argument matcher and capturer", (){
+      mock.methodWithNormalArgs(50);
+      mock.methodWithNormalArgs(100);
+      var captured = 0;
+      expect(verify(mock.methodWithNormalArgs(captureThat(greaterThan(75)))).captured.single, equals(100));
+      expect(verify(mock.methodWithNormalArgs(captureThat(lessThan(75)))).captured.single, equals(50));
+    });  
+    test("should mock method with mix of argument matchers and real things", (){
+      mock.methodWithPositionalArgs(100, 17);
+      expectFail("No matching calls. All calls: MockedClass.methodWithPositionalArgs(100, 17)", (){
+        verify(mock.methodWithPositionalArgs(argThat(greaterThanOrEqualTo(100)), 18)); 
+      });
+      expectFail("No matching calls. All calls: MockedClass.methodWithPositionalArgs(100, 17)", (){ 
+        verify(mock.methodWithPositionalArgs(argThat(greaterThan(100)), 17)); 
+      });
+      verify(mock.methodWithPositionalArgs(argThat(greaterThanOrEqualTo(100)), 17));
+    });  
+    test("should mock getter", (){
+      mock.getter;
+      verify(mock.getter);
+    });
+    test("should mock setter", (){
+      mock.setter = "A";
+      expectFail("No matching calls. All calls: MockedClass.setter==A", (){
+        verify(mock.setter="B"); 
+      });
+      verify(mock.setter="A");
+    });
+  });
+  group("verify() qualifies", (){
+    group("unqualified as at least one", (){
+      test("zero fails", (){
+        expectFail("No matching calls.", (){ 
+          verify(mock.methodWithoutArgs()); 
+        });
+      });
+      test("one passes", (){
+        mock.methodWithoutArgs();
+        verify(mock.methodWithoutArgs());
+      });
+      test("more than one passes", (){
+        mock.methodWithoutArgs();
+        mock.methodWithoutArgs();
+        verify(mock.methodWithoutArgs());
+      });
+    });
+    group("counts calls", (){
+      test("zero fails", (){
+        expectFail("No matching calls.", (){ 
+          verify(mock.methodWithoutArgs()).called(1); 
+        });
+      });
+      test("one passes", (){
+        mock.methodWithoutArgs();
+        verify(mock.methodWithoutArgs()).called(1); 
+      });
+      test("more than one fails", (){
+        mock.methodWithoutArgs();
+        mock.methodWithoutArgs();
+        expectFail("Expected: <1>\n  Actual: <2>\nUnexpected number of calls\n", (){ 
+          verify(mock.methodWithoutArgs()).called(1); 
+        });
+      });
+    });
+    group("verifyNever", (){
+      test("zero passes", (){
+        verifyNever(mock.methodWithoutArgs());
+      });
+      test("one fails", (){
+        mock.methodWithoutArgs();
+        expectFail("Unexpected calls. All calls: MockedClass.methodWithoutArgs()", (){ 
+          verifyNever(mock.methodWithoutArgs()); 
+        });
+      });
+    });
+    group("doesn't count already verified again", (){
+      test("fail case", (){
+        mock.methodWithoutArgs();
+        verify(mock.methodWithoutArgs());
+        expectFail("No matching calls. All calls: [VERIFIED] MockedClass.methodWithoutArgs()", (){ 
+          verify(mock.methodWithoutArgs()); 
+        });
+      });
+      test("pass case", (){
+        mock.methodWithoutArgs();
+        verify(mock.methodWithoutArgs());
+        mock.methodWithoutArgs();
+        verify(mock.methodWithoutArgs());
+      });
+    });
+  });
+  
+  group("verifyZeroInteractions()", (){
+    test("never touched pass", (){
+      verifyZeroInteractions(mock);
+    });
+    test("any touch fails", (){
+      mock.methodWithoutArgs();
+      expectFail("No interaction expected, but following found: MockedClass.methodWithoutArgs()", (){ 
+        verifyZeroInteractions(mock); 
+      });
+    });
+    test("verifired call fails", (){
+      mock.methodWithoutArgs();
+      verify(mock.methodWithoutArgs());
+      expectFail("No interaction expected, but following found: [VERIFIED] MockedClass.methodWithoutArgs()", (){ 
+        verifyZeroInteractions(mock); 
+      });
+    });
+  });
+  group("verifyNoMoreInteractions()", (){
+    test("never touched pass", (){
+      verifyNoMoreInteractions(mock);
+    });
+    test("any unverified touch fails", (){
+      mock.methodWithoutArgs();
+      expectFail("No more calls expected, but following found: MockedClass.methodWithoutArgs()", (){ 
+        verifyNoMoreInteractions(mock); 
+      });
+    });
+    test("verified touch passes", (){
+      mock.methodWithoutArgs();
+      verify(mock.methodWithoutArgs());
+      verifyNoMoreInteractions(mock);
+    });
+  });
+  group("verifyInOrder()", (){
+    test("right order passes", (){
+      mock.methodWithoutArgs();
+      mock.getter;
+      verifyInOrder([mock.methodWithoutArgs(), mock.getter]);
+    });
+    test("wrong order fails", (){
+      mock.methodWithoutArgs();
+      mock.getter;
+      expectFail("Matching call #1 not found. All calls: MockedClass.methodWithoutArgs(), MockedClass.getter", (){
+        verifyInOrder([mock.getter, mock.methodWithoutArgs()]); 
+      });
+    });
+    test("uncomplete fails", (){
+      mock.methodWithoutArgs();
+      expectFail("Matching call #1 not found. All calls: MockedClass.methodWithoutArgs()", (){ 
+        verifyInOrder([mock.methodWithoutArgs(), mock.getter]); 
+      });
+    });
+    test("methods can be called again and again", (){
+      mock.methodWithoutArgs();
+      mock.getter;
+      mock.methodWithoutArgs();
+      verifyInOrder([mock.methodWithoutArgs(), mock.getter, mock.methodWithoutArgs()]);
+    });
+    test("methods can be called again and again - fail case", (){
+      mock.methodWithoutArgs();
+      mock.methodWithoutArgs();
+      mock.getter;
+      expectFail("Matching call #2 not found. All calls: MockedClass.methodWithoutArgs(), MockedClass.methodWithoutArgs(), MockedClass.getter", (){ 
+        verifyInOrder([mock.methodWithoutArgs(), mock.getter, mock.methodWithoutArgs()]); 
+      });
+    });
+  });
+  
+  group("capture", (){
+    test("capture should work as captureOut", (){
+      mock.methodWithNormalArgs(42);
+      expect(verify(mock.methodWithNormalArgs(captureAny)).captured.single, equals(42));
+    });
+    test("should captureOut multiple arguments", (){
+      mock.methodWithPositionalArgs(1, 2);
+      expect(verify(mock.methodWithPositionalArgs(captureAny, captureAny)).captured, equals([1, 2]));
+    });
+    test("should captureOut with matching arguments", (){
+      mock.methodWithPositionalArgs(1);
+      mock.methodWithPositionalArgs(2, 3);
+      expect(verify(mock.methodWithPositionalArgs(captureAny, captureAny)).captured, equals([2, 3]));
+    });
+    test("should captureOut multiple invocations", (){
+      mock.methodWithNormalArgs(1);
+      mock.methodWithNormalArgs(2);
+      expect(verify(mock.methodWithNormalArgs(captureAny)).captured, equals([1, 2]));
+    });
+  });
+
+}
+
