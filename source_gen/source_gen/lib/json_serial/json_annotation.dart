95603c1f9607c1d09bfdcdcbb2179cbd380affe7
Kevin Moore <kevmoo@google.com>
2015-02-02 20:31:04 -0800

    Add doc comment about why the annotation library is separate from the generator library

--- source_gen/source_gen/lib/json_serial/json_annotation.dart
+++ source_gen/source_gen/lib/json_serial/json_annotation.dart
@@ -1,3 +1,6 @@
+/// This is kept as a seperate library from the generator intentionally to
+/// minimize transitive dependencies of types that are annotated as
+/// serializable.
 library source_gen.json_serial.annotation;
 
 class JsonSerializable {
