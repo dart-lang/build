da3a4773f8d5853cbfb4d5d0dae7bfa5791ad7cb
Kevin Moore <kevmoo@google.com>
2015-02-11 18:25:53 -0800

    tiny reformat

--- source_gen/source_gen/build.dart
+++ source_gen/source_gen/build.dart
@@ -4,8 +4,10 @@ import 'package:source_gen/build_helper.dart' as build_helper;
 import 'package:source_gen/json_serial/json_generator.dart' as json;
 
 void main(List<String> args) {
-  build_helper.build(args, const [const json.JsonGenerator()],
-      librarySearchPaths: ['example']).then((msg) {
+  build_helper
+      .build(args, const [const json.JsonGenerator()],
+          librarySearchPaths: ['example'])
+      .then((msg) {
     print(msg);
   });
 }
