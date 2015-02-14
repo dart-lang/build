cb087d79a9c8d80bfa4dcde8dc9ee6865df86c17
Kevin Moore <kevmoo@google.com>
2015-02-14 12:22:07 -0800

    ordered items in lib/src/utils.dart

--- source_gen/source_gen/lib/src/utils.dart
+++ source_gen/source_gen/lib/src/utils.dart
@@ -16,71 +16,23 @@ import 'package:analyzer/src/generated/source_io.dart';
 import 'package:cli_util/cli_util.dart' as cli;
 import 'package:path/path.dart' as p;
 
-Set<LibraryElement> getLibraries(
-    AnalysisContext context, Iterable<String> filePaths) {
-  return filePaths.fold(new Set<LibraryElement>(), (set, path) {
-    var elementLibrary = getLibraryElementForSourceFile(context, path);
+String findPartOf(String source) {
+  try {
+    var unit = parseCompilationUnit(source);
 
-    if (elementLibrary != null) {
-      set.add(elementLibrary);
+    var partOf = unit.directives.firstWhere((d) => d is PartOfDirective,
+        orElse: () => null);
+
+    if (partOf == null) {
+      return null;
     }
 
-    return set;
-  });
-}
+    var offset = partOf.offset;
 
-/// [dartFiles] is a [Stream] of paths to [.dart] files.
-Iterable<LibraryElement> getLibraryElements(
-    List<String> dartFiles, AnalysisContext context) => dartFiles
-    .map((path) => _getLibraryElement(path, context))
-    .where((lib) => lib != null);
-
-/// [foundFiles] is the list of files to consider for the context.
-Future<AnalysisContext> getAnalysisContextForProjectPath(
-    String projectPath, List<String> foundFiles) async {
-  // TODO: fail more clearly if this...fails
-  var sdkPath = cli.getSdkDir().path;
-
-  JavaSystemIO.setProperty("com.google.dart.sdk", sdkPath);
-  DartSdk sdk = DirectoryBasedDartSdk.defaultSdk;
-
-  var packagesPath = p.join(projectPath, 'packages');
-
-  var packageDirectory = new JavaFile(packagesPath);
-
-  // TODO(kevmoo): Can we get of one of these?
-  var resolvers = [
-    new DartUriResolver(sdk),
-    new ResourceUriResolver(PhysicalResourceProvider.INSTANCE),
-    new PackageUriResolver([packageDirectory])
-  ];
-
-  var context = AnalysisEngine.instance.createAnalysisContext()
-    ..sourceFactory = new SourceFactory(resolvers);
-
-  // ensures all libraries defined by the set of files are resolved
-  getLibraryElements(foundFiles, context).toList();
-
-  return context;
-}
-
-LibraryElement getLibraryElementForSourceFile(
-    AnalysisContext context, String sourcePath) {
-  Source source = new FileBasedSource.con1(new JavaFile(sourcePath));
-
-  var libs = context.getLibrariesContaining(source);
-
-  if (libs.length > 1) {
-    throw "We don't support multiple libraries for a source.";
-  }
-
-  if (libs.isEmpty) {
+    return source.substring(offset);
+  } on AnalyzerErrorGroup catch (e) {
     return null;
   }
-
-  var libSource = libs.single;
-
-  return context.getLibraryElement(libSource);
 }
 
 String frieldlyNameForElement(Element element) {
@@ -113,10 +65,71 @@ String frieldlyNameForElement(Element element) {
   return names.join(' ');
 }
 
+/// [foundFiles] is the list of files to consider for the context.
+Future<AnalysisContext> getAnalysisContextForProjectPath(
+    String projectPath, List<String> foundFiles) async {
+  // TODO: fail more clearly if this...fails
+  var sdkPath = cli.getSdkDir().path;
+
+  JavaSystemIO.setProperty("com.google.dart.sdk", sdkPath);
+  DartSdk sdk = DirectoryBasedDartSdk.defaultSdk;
+
+  var packagesPath = p.join(projectPath, 'packages');
+
+  var packageDirectory = new JavaFile(packagesPath);
+
+  // TODO(kevmoo): Can we get of one of these?
+  var resolvers = [
+    new DartUriResolver(sdk),
+    new ResourceUriResolver(PhysicalResourceProvider.INSTANCE),
+    new PackageUriResolver([packageDirectory])
+  ];
+
+  var context = AnalysisEngine.instance.createAnalysisContext()
+    ..sourceFactory = new SourceFactory(resolvers);
+
+  // ensures all libraries defined by the set of files are resolved
+  _getLibraryElements(foundFiles, context).toList();
+
+  return context;
+}
+
 Iterable<Element> getElementsFromLibraryElement(LibraryElement unit) =>
     unit.units
         .expand((cu) => cu.unit.declarations)
         .expand((compUnitMember) => _getElements(compUnitMember));
+
+Set<LibraryElement> getLibraries(
+    AnalysisContext context, Iterable<String> filePaths) {
+  return filePaths.fold(new Set<LibraryElement>(), (set, path) {
+    var elementLibrary = getLibraryElementForSourceFile(context, path);
+
+    if (elementLibrary != null) {
+      set.add(elementLibrary);
+    }
+
+    return set;
+  });
+}
+
+LibraryElement getLibraryElementForSourceFile(
+    AnalysisContext context, String sourcePath) {
+  Source source = new FileBasedSource.con1(new JavaFile(sourcePath));
+
+  var libs = context.getLibrariesContaining(source);
+
+  if (libs.length > 1) {
+    throw "We don't support multiple libraries for a source.";
+  }
+
+  if (libs.isEmpty) {
+    return null;
+  }
+
+  var libSource = libs.single;
+
+  return context.getLibraryElement(libSource);
+}
 
 Iterable<Element> _getElements(CompilationUnitMember member) {
   if (member is TopLevelVariableDeclaration) {
@@ -132,26 +145,6 @@ Iterable<Element> _getElements(CompilationUnitMember member) {
   return [element];
 }
 
-String findPartOf(String source) {
-  try {
-    var unit = parseCompilationUnit(source);
-
-    var partOf = unit.directives.firstWhere((d) => d is PartOfDirective,
-        orElse: () => null);
-
-    if (partOf == null) {
-      return null;
-    }
-
-    var offset = partOf.offset;
-
-    return source.substring(offset);
-  } on AnalyzerErrorGroup catch (e) {
-    return null;
-  }
-}
-
-// may return `null` if [path] doesn't refer to a library.
 LibraryElement _getLibraryElement(String path, AnalysisContext context) {
   Source source = new FileBasedSource.con1(new JavaFile(path));
   if (context.computeKindOf(source) == SourceKind.LIBRARY) {
@@ -159,3 +152,10 @@ LibraryElement _getLibraryElement(String path, AnalysisContext context) {
   }
   return null;
 }
+
+// may return `null` if [path] doesn't refer to a library.
+/// [dartFiles] is a [Stream] of paths to [.dart] files.
+Iterable<LibraryElement> _getLibraryElements(
+    List<String> dartFiles, AnalysisContext context) => dartFiles
+    .map((path) => _getLibraryElement(path, context))
+    .where((lib) => lib != null);
