23e3007db5e5ee9f8bdf76af19db3d61dcf7189f
Kevin Moore <kevmoo@google.com>
2015-02-16 13:34:21 -0800

    JsonSerializableGenerator - refactoring

--- source_gen/source_gen/lib/generators/json_serializable_generator.dart
+++ source_gen/source_gen/lib/generators/json_serializable_generator.dart
@@ -25,81 +25,77 @@ class JsonSerializableGenerator
     }
 
     var classElement = element as ClassElement;
+    var className = classElement.name;
 
     var finalFields = new Set<String>();
 
-    var fieldMap = <String, String>{};
+    var fields = <String, String>{};
     for (FieldElement field in classElement.fields) {
       if (field.isFinal) {
         finalFields.add(field.name);
       }
-      fieldMap[field.name] = field.type.name;
+      fields[field.name] = field.type.name;
     }
 
     if (finalFields.isNotEmpty) {
-      var friendlyName = frieldlyNameForElement(element);
       throw new InvalidGenerationSourceError(
-          'Generator cannot target `$friendlyName`.',
+          'Generator cannot target `$className`.',
           todo: 'Make the following fields writable: ${finalFields.join(', ')}.');
     }
 
-    return _populateTemplate(classElement.displayName, fieldMap, annotation);
+    var prefix = '_\$${className}';
+
+    var buffer = new StringBuffer();
+
+    if (annotation.createFactory) {
+
+      //
+      // Generate the static factory method
+      //
+      buffer.writeln();
+      buffer
+          .writeln('$className ${prefix}FromJson(Map<String, Object> json) =>');
+      buffer.write('    new $className()');
+      if (fields.isEmpty) {
+        buffer.writeln(';');
+      } else {
+        fields.forEach((name, type) {
+          buffer.writeln();
+          buffer.write("      ..${name} = json['$name']");
+        });
+        buffer.writeln(';');
+      }
+      buffer.writeln();
+    }
+
+    if (annotation.createToJson) {
+      //
+      // Generate the mixin class
+      //
+      buffer.writeln('abstract class ${prefix}SerializerMixin {');
+
+      // write fields
+      fields.forEach((k, v) {
+        buffer.writeln('  $v get $k;');
+      });
+
+      // write toJson method
+      buffer.writeln('  Map<String, Object> toJson() => {');
+
+      var pairs = <String>[];
+      fields.forEach((k, v) {
+        pairs.add("'$k': $k");
+      });
+      buffer.writeln(pairs.join(','));
+
+      buffer.writeln('  };');
+
+      buffer.write('}');
+    }
+
+    return buffer.toString();
   }
 
   @override
   String toString() => 'JsonGenerator';
-}
-
-String _populateTemplate(
-    String className, Map<String, String> fields, JsonSerializable annotation) {
-  var prefix = '_\$${className}';
-
-  var buffer = new StringBuffer();
-
-  if (annotation.createFactory) {
-
-    //
-    // Generate the static factory method
-    //
-    buffer.writeln();
-    buffer.writeln('$className ${prefix}FromJson(Map<String, Object> json) =>');
-    buffer.write('    new $className()');
-    if (fields.isEmpty) {
-      buffer.writeln(';');
-    } else {
-      fields.forEach((name, type) {
-        buffer.writeln();
-        buffer.write("      ..${name} = json['$name']");
-      });
-      buffer.writeln(';');
-    }
-    buffer.writeln();
-  }
-
-  if (annotation.createToJson) {
-    //
-    // Generate the mixin class
-    //
-    buffer.writeln('abstract class ${prefix}SerializerMixin {');
-
-    // write fields
-    fields.forEach((k, v) {
-      buffer.writeln('  $v get $k;');
-    });
-
-    // write toJson method
-    buffer.writeln('  Map<String, Object> toJson() => {');
-
-    var pairs = <String>[];
-    fields.forEach((k, v) {
-      pairs.add("'$k': $k");
-    });
-    buffer.writeln(pairs.join(','));
-
-    buffer.writeln('  };');
-
-    buffer.write('}');
-  }
-
-  return buffer.toString();
 }
