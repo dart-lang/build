94dc2189d0001b27aedce8bcfe4ca3918b541505
Stephen Turley <turley714@gmail.com>
2015-05-06 22:38:09 -0400

    removed deprecated UnitTest framework and added missing Test import in mockito.dart

--- builder_pkgs/mockito/lib/mockito.dart
+++ builder_pkgs/mockito/lib/mockito.dart
@@ -1,6 +1,7 @@
 library mockito;
 
 import 'package:matcher/matcher.dart';
+import 'package:test/test.dart';
 import 'dart:mirrors';
 
 bool _whenInProgress = false; 
--- builder_pkgs/mockito/test/mockitoSpec.dart
+++ builder_pkgs/mockito/test/mockitoSpec.dart
@@ -1,4 +1,4 @@
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import '../lib/mockito.dart';
 
 class RealClass {
