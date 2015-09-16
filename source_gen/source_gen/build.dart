d7b6635fbd790951256f41b06a0f1044c87e7293
Kevin Moore <kevmoo@google.com>
2015-09-16 14:38:28 -0700

    use async in build.dart

--- source_gen/source_gen/build.dart
+++ source_gen/source_gen/build.dart
@@ -8,14 +8,13 @@ import 'package:source_gen/generators/json_literal_generator.dart' as literal;
 import 'package:source_gen/generators/json_serializable_generator.dart' as json;
 import 'package:source_gen/source_gen.dart';
 
-void main(List<String> args) {
-  build(args, const [
+main(List<String> args) async {
+  var msg = await build(args, const [
     const json.JsonSerializableGenerator(),
     const literal.JsonLiteralGenerator()
   ], librarySearchPaths: [
     'example',
     'test/test_files/json_test_example.dart'
-  ]).then((msg) {
-    print(msg);
-  });
+  ]);
+  print(msg);
 }
