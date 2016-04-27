c3e113b25623c6b6e2b81aea938fabdb3b15654b
Jacob MacDonald <jakemac@google.com>
2016-04-27 14:54:43 -0700

    cleanup scriptHash

--- lib/src/util/constants.dart
+++ lib/src/util/constants.dart
@@ -11,6 +11,5 @@ final String assetGraphPath = '$cacheDir/$scriptHash/asset_graph.json';
 /// Relative path to the cache directory from the root package dir.
 const String cacheDir = '.dart_tool/build';
 
-final String scriptHash = () {
-  return md5.convert(Platform.script.path.codeUnits).toString();
-}();
+final String scriptHash =
+    md5.convert(Platform.script.path.codeUnits).toString();
