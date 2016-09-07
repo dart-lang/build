a24212186ea4a35e56976998280812a9388b70ce
Nate Bosch <nbosch1@gmail.com>
2016-09-07 09:26:50 -0700

    Restore the example CopyTransformer
    This is still useful as an example since the `asPlugin` ctor has meaning
    to pub. Upgrade to work with the changes in BuilderTransformer and add a
    Doc comment.

---
+++ e2e_example/lib/transformer.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import 'package:build/build.dart';
+
+import 'copy_builder.dart';
+
+/// A pub compatible Transformer built on top of [BuilderTransformer].
+class CopyTransformer extends BuilderTransformer {
+  CopyTransformer.asPlugin(_) : super([new CopyBuilder()]);
+}
