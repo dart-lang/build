a1594be69d036e3745859e033ced3a2a72505082
Kevin Moore <kevmoo@google.com>
2017-03-21 16:35:10 -0700

    Removing coveralls from everywhere
    We’ll need to figure out how to teach it to deal with subdirectories

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,18 +7,6 @@
 # Fast fail the script on failures.
 set -e
 
-function run_coveralls {
-  if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-    dart tool/create_test_all.dart
-    pub global activate dart_coveralls
-    pub global run dart_coveralls report \
-      --retry 2 \
-      --exclude-test-files \
-      tool/test_all.dart
-    rm tool/test_all.dart
-  fi
-}
-
 pushd build_test
 pub upgrade
 dartanalyzer --fatal-warnings lib/build_test.dart
@@ -28,19 +16,16 @@ pushd build
 pub upgrade
 dartanalyzer --fatal-warnings lib/build.dart
 pub run test
-run_coveralls
 popd
 
 pushd build_runner
 pub upgrade
 dartanalyzer --fatal-warnings lib/build_runner.dart
 pub run test
-run_coveralls
 popd
 
 pushd build_barback
 pub upgrade
 dartanalyzer --fatal-warnings lib/build_barback.dart
 pub run test
-run_coveralls
 popd
