3916596e482eb4e10e067c6aa64340c2ef81debc
Shams Zakhour <shaza@google.com>
2017-04-04 17:44:18 -0700

    Adding direct links to the files from the README. (dart-lang/mockito#61)
    * Adding direct links to the files from the README.
    
    * Fixing the markdown.
    
    * Tweak
    
    * Fixing typo.

--- builder_pkgs/mockito/test/example/iss/iss_test.dart
+++ builder_pkgs/mockito/test/example/iss/iss_test.dart
@@ -33,6 +33,8 @@ void main() {
       Point<double> london = new Point(51.5073, -0.1277);
       Point<double> paris = new Point(48.8566, 2.3522);
       double d = sphericalDistanceKm(london, paris);
+      // London should be approximately 343.5km
+      // (+/- 0.1km) from Paris.
       expect(d, closeTo(343.5, 0.1));
     });
 
@@ -40,6 +42,8 @@ void main() {
       Point<double> sf = new Point(37.783333, -122.416667);
       Point<double> mtv = new Point(37.389444, -122.081944);
       double d = sphericalDistanceKm(sf, mtv);
+      // San Francisco should be approximately 52.8km
+      // (+/- 0.1km) from Mountain View.
       expect(d, closeTo(52.8, 0.1));
     });
   });
