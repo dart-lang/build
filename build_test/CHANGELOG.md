397778223ed66b80fb225c8b2b0dc0319b2d67aa
Matan Lurey <matanl@google.com>
2017-04-18 09:44:05 -0700

    Export MultiAssetReader.

--- build_test/CHANGELOG.md
+++ build_test/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.5.2
+
+- Add `MultiAssetReader` to the public API.
+
 ## 0.5.1
 
 - Add `PackageAssetReader`, a standalone asset reader that uses a
--- build_test/lib/build_test.dart
+++ build_test/lib/build_test.dart
@@ -7,6 +7,7 @@ export 'src/fake_watcher.dart';
 export 'src/in_memory_reader.dart';
 export 'src/in_memory_writer.dart';
 export 'src/matchers.dart';
+export 'src/multi_asset_reader.dart' show MultiAssetReader;
 export 'src/package_reader.dart' show PackageAssetReader;
 export 'src/resolve_source.dart';
 export 'src/stub_reader.dart';
