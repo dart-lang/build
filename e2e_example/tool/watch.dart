90fe798f0d5cd9b92dd1f9b06f589bbf31edcfc0
Nate Bosch <nbosch@google.com>
2017-04-24 14:28:37 -0700

    Update e2e_example to build 0.9.0
    - Change Builder implementation to use buildExtensions
    - Drop PackagesDirBuilder since it is incompatible with buildExtensions
    - Update dependencies

--- e2e_example/tool/watch.dart
+++ e2e_example/tool/watch.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 import 'package:build_runner/build_runner.dart';
 
 import 'package:e2e_example/copy_builder.dart';
-import 'package:e2e_example/packages_dir_builder.dart';
 
 Future main() async {
   // Builds a full package dependency graph for the current package.
@@ -14,12 +13,6 @@ Future main() async {
   var phases = new PhaseGroup()
     ..newPhase().addAction(
         new CopyBuilder(), new InputSet(graph.root.name, ['**/*.txt']));
-
-  var packagesDirBuilder = new PackagesDirBuilder(graph.root.name, 'web');
-  var phase = phases.newPhase();
-  for (var package in graph.allPackages.values) {
-    phase.addAction(packagesDirBuilder, new InputSet(package.name, ['lib/**']));
-  }
 
   watch(phases);
 }
