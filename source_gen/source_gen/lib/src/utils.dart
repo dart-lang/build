0a66e2ca765f805f49c1c6d31140b915ac44968f
Matan Lurey <matanlurey@users.noreply.github.com>
2017-06-18 15:36:04 -0700

    Add and conform license headers. (dart-lang/source_gen#154)

--- source_gen/source_gen/lib/src/utils.dart
+++ source_gen/source_gen/lib/src/utils.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/standard_resolution_map.dart';
 import 'package:analyzer/dart/element/element.dart';
--- source_gen/source_gen/test/test_files/annotated_classes.dart
+++ source_gen/source_gen/test/test_files/annotated_classes.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 library source_gen.test.annotation_test.classes;
 
 import 'package:source_gen/generators/json_serializable.dart';
