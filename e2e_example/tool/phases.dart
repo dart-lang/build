acfa486be631901e1ac171abbe28c041b46d0de0
Nate Bosch <nbosch@google.com>
2017-08-07 14:49:05 -0700

    Update usage in e2e-example

--- e2e_example/tool/phases.dart
+++ e2e_example/tool/phases.dart
@@ -1,6 +1,7 @@
 import 'package:build_runner/build_runner.dart';
 import 'package:e2e_example/copy_builder.dart';
 
-final phases = new PhaseGroup()
-  ..newPhase().addAction(new CopyBuilder(),
-      new InputSet(new PackageGraph.forThisPackage().root.name, ['**/*.txt']));
+final phases = [
+  new BuildAction(new CopyBuilder(),
+      new PackageGraph.forThisPackage().root.name, ['**/*.txt'])
+];
