fc97fcd00181d42cf8c8038d992e7296646c1915
Nate Bosch <nbosch@google.com>
2017-08-15 12:46:03 -0700

    Add package graph to make test pass

--- build_runner/test/generate/build_test.dart
+++ build_runner/test/generate/build_test.dart
@@ -79,9 +79,16 @@ void main() {
     });
 
     test('can\'t output files in non-root packages', () async {
+      var packageB = new PackageNode(
+          'b', '0.1.0', PackageDependencyType.path, new Uri.file('a/b/'));
+      var packageA = new PackageNode(
+          'a', '0.1.0', PackageDependencyType.path, new Uri.file('a/'))
+        ..dependencies.add(packageB);
+      ;
+      var packageGraph = new PackageGraph.fromRoot(packageA);
       await testActions(
           [new BuildAction(new CopyBuilder(), 'b')], {'b|lib/b.txt': 'b'},
-          outputs: {}, status: BuildStatus.failure);
+          packageGraph: packageGraph, outputs: {}, status: BuildStatus.failure);
     });
   });
 
