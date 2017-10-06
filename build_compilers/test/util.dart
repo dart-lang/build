84134eebdeeabbb72b4c363ae456d033a97e3cb0
Jacob MacDonald <jakemac@google.com>
2017-10-06 15:08:35 -0700

    clean up comments

--- build_compilers/test/util.dart
+++ build_compilers/test/util.dart
@@ -10,10 +10,8 @@ import 'package:build_test/build_test.dart';
 /// Forwards to [testBuilder], and adds all output assets to [assets].
 Future<Null> testBuilderAndAddAssets(
     Builder builder, Map<String, dynamic> assets) async {
-  // Use `testBuilder` to create unlinked summaries for the actual test.
   var writer = new InMemoryAssetWriter();
   await testBuilder(builder, assets, writer: writer);
-  // Add the serialized modules to `assets`.
   writer.assets.forEach((id, datedValue) {
     assets['${id.package}|${id.path}'] = datedValue.bytesValue;
   });
