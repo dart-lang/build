6069567d9eb71d2df51c729dc0527ee067e11a1c
Matan Lurey <matan@lurey.org>
2017-10-15 22:51:17 -0700

    Add an integration-y test for serving.

---
+++ build_runner/test/server/serve_integration_test.dart
@@ -0,0 +1,111 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:build/build.dart';
+import 'package:build_runner/build_runner.dart';
+import 'package:logging/logging.dart';
+import 'package:path/path.dart' as p;
+import 'package:shelf/shelf.dart';
+import 'package:test/test.dart';
+import 'package:watcher/watcher.dart';
+
+import '../common/common.dart';
+
+void main() {
+  FutureOr<Response> Function(Request) handler;
+  InMemoryRunnerAssetReader reader;
+  InMemoryRunnerAssetWriter writer;
+  StreamSubscription subscription;
+  Completer<Null> nextBuild;
+
+  final path = p.absolute('example');
+
+  setUp(() async {
+    final graph = new PackageGraph.fromRoot(
+      new PackageNode.noPubspec('example', path: path),
+    );
+    reader = new InMemoryRunnerAssetReader({}, 'example');
+    writer = new InMemoryRunnerAssetWriter();
+    final server = (await watch(
+      [
+        new BuildAction(const UppercaseBuilder(), 'example'),
+      ],
+      packageGraph: graph,
+      reader: reader,
+      writer: writer,
+      logLevel: Level.ALL,
+      directoryWatcherFactory: (path) => new FakeWatcher(path),
+    ));
+    handler = server.handlerFor('web');
+
+    nextBuild = new Completer<Null>();
+    subscription = server.buildResults.listen((_) {
+      if (!nextBuild.isCompleted) {
+        nextBuild.complete();
+      }
+    });
+    await nextBuild.future;
+  });
+
+  tearDown(() => subscription.cancel());
+
+  test('should serve original files', () async {
+    final getHello = Uri.parse('http://localhost/hello.txt');
+    reader.cacheStringAsset(new AssetId('example', 'web/hello.txt'), 'Hello');
+    final response = await handler(new Request('GET', getHello));
+    expect(await response.readAsString(), 'Hello');
+  });
+
+  test('should serve built files', () async {
+    final getHello = Uri.parse('http://localhost/hello.g.txt');
+    reader.cacheStringAsset(new AssetId('example', 'web/hello.g.txt'), 'HELLO');
+    final response = await handler(new Request('GET', getHello));
+    expect(await response.readAsString(), 'HELLO');
+  });
+
+  test('should 404 on missing files', () async {
+    final get404 = Uri.parse('http://localhost/404.txt');
+    final response = await handler(new Request('GET', get404));
+    expect(await response.readAsString(), 'Not Found');
+  });
+
+  test('should serve newly added files', () async {
+    final getNew = Uri.parse('http://localhost/new.txt');
+    reader.cacheStringAsset(new AssetId('example', 'web/new.txt'), 'New');
+    final response = await handler(new Request('GET', getNew));
+    expect(await response.readAsString(), 'New');
+  });
+
+  test('should serve built newly added files', () async {
+    final getNew = Uri.parse('http://localhost/new.g.txt');
+    reader.cacheStringAsset(new AssetId('example', 'web/new.txt'), 'New');
+    await new Future.value();
+    FakeWatcher.notifyWatchers(
+      new WatchEvent(ChangeType.ADD, '$path/web/new.txt'),
+    );
+    await (nextBuild = new Completer<Null>()).future;
+    final response = await handler(new Request('GET', getNew));
+    expect(await response.readAsString(), 'NEW');
+  }, skip: 'Enable on fix of https://github.com/dart-lang/build/issues/500');
+}
+
+class UppercaseBuilder implements Builder {
+  const UppercaseBuilder();
+
+  @override
+  Future<Null> build(BuildStep buildStep) async {
+    final content = await buildStep.readAsString(buildStep.inputId);
+    await buildStep.writeAsString(
+      buildStep.inputId.changeExtension('.g.txt'),
+      content.toUpperCase(),
+    );
+  }
+
+  @override
+  final buildExtensions = const {
+    'txt': const ['g.txt']
+  };
+}
