ea8693f3ad99a42093c6c22c5166b05d31c81232
Jacob MacDonald <jakemac@google.com>
2017-10-16 13:53:21 -0700

    Update e2e_example to be simpler and run an integration test. (#519)

--- e2e_example/tool/build.dart
+++ e2e_example/tool/build.dart
@@ -1,12 +1,55 @@
-// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
 import 'dart:async';
+import 'dart:io';
 
+import 'package:build/build.dart';
+import 'package:build_compilers/build_compilers.dart';
 import 'package:build_runner/build_runner.dart';
-
-import 'phases.dart';
+import 'package:build_test/builder.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
 Future main() async {
-  await build(phases);
+  var graph = new PackageGraph.forThisPackage();
+  var buildActions = <BuildAction>[
+    new BuildAction(new TestBootstrapBuilder(), graph.root.name,
+        inputs: ['test/**_test.dart']),
+  ];
+
+  void addBuilderForAll(Builder builder, String inputExtension) {
+    for (var packageNode in graph.orderedPackages) {
+      buildActions
+          .add(new BuildAction(builder, packageNode.name, isOptional: true));
+    }
+  }
+
+  addBuilderForAll(new ModuleBuilder(), '.dart');
+  addBuilderForAll(new UnlinkedSummaryBuilder(), moduleExtension);
+  addBuilderForAll(new LinkedSummaryBuilder(), moduleExtension);
+  addBuilderForAll(new DevCompilerBuilder(), moduleExtension);
+
+  buildActions.add(new BuildAction(
+      new DevCompilerBootstrapBuilder(), graph.root.name,
+      inputs: ['web/**.dart', 'test/**.browser_test.dart']));
+
+  var serveHandler = await watch(
+    buildActions,
+    deleteFilesByDefault: true,
+    writeToCache: true,
+  );
+
+  var server =
+      await shelf_io.serve(serveHandler.handlerFor('web'), 'localhost', 8080);
+  var testServer =
+      await shelf_io.serve(serveHandler.handlerFor('test'), 'localhost', 8081);
+
+  await serveHandler.currentBuild;
+  stderr.writeln('Serving `web` at http://localhost:8080/');
+  stderr.writeln('Serving `test` at http://localhost:8081/');
+
+  await serveHandler.buildResults.drain();
+  await server.close();
+  await testServer.close();
 }
