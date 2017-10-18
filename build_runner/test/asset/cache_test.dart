3bc5188f4b64594156106cee354cb552734a483a
Jacob MacDonald <jakemac@google.com>
2017-10-18 14:38:47 -0700

    add unit test for CachingAssetReader

---
+++ build_runner/test/asset/cache_test.dart
@@ -0,0 +1,121 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:build/build.dart';
+import 'package:build_test/build_test.dart';
+import 'package:test/test.dart';
+
+import 'package:build_runner/src/asset/cache.dart';
+
+import '../common/common.dart';
+
+void main() {
+  var fooTxt = new AssetId('a', 'foo.txt');
+  var missingTxt = new AssetId('a', 'missing.txt');
+  var fooContent = 'bar';
+  var fooUTF8Bytes = UTF8.encode('bar');
+  var assets = <AssetId, DatedValue>{
+    fooTxt: new DatedString('bar'),
+  };
+  InMemoryAssetReader delegate;
+  CachingAssetReader reader;
+
+  setUp(() {
+    delegate = new InMemoryAssetReader(sourceAssets: assets);
+    reader = new CachingAssetReader(delegate);
+  });
+
+  group('canRead', () {
+    test('should read from the delegate', () async {
+      expect(await reader.canRead(fooTxt), isTrue);
+      expect(await reader.canRead(missingTxt), isFalse);
+      expect(delegate.assetsRead, [fooTxt, missingTxt]);
+    });
+
+    test('should not re-read from the delegate', () async {
+      expect(await reader.canRead(fooTxt), isTrue);
+      delegate.assetsRead.clear();
+      expect(await reader.canRead(fooTxt), isTrue);
+      expect(delegate.assetsRead, []);
+    });
+
+    test('can be invalidated with invalidate', () async {
+      expect(await reader.canRead(fooTxt), isTrue);
+      delegate.assetsRead.clear();
+      expect(delegate.assetsRead, []);
+
+      reader.invalidate([fooTxt]);
+      expect(await reader.canRead(fooTxt), isTrue);
+      expect(delegate.assetsRead, [fooTxt]);
+    });
+  });
+
+  group('readAsBytes', () {
+    test('should read from the delegate', () async {
+      expect(await reader.readAsBytes(fooTxt), fooUTF8Bytes);
+      expect(delegate.assetsRead, [fooTxt]);
+    });
+
+    test('should not re-read from the delegate', () async {
+      expect(await reader.readAsBytes(fooTxt), fooUTF8Bytes);
+      delegate.assetsRead.clear();
+      expect(await reader.readAsBytes(fooTxt), fooUTF8Bytes);
+      expect(delegate.assetsRead, []);
+    });
+
+    test('can be invalidated with invalidate', () async {
+      expect(await reader.readAsBytes(fooTxt), fooUTF8Bytes);
+      delegate.assetsRead.clear();
+      expect(delegate.assetsRead, []);
+
+      reader.invalidate([fooTxt]);
+      expect(await reader.readAsBytes(fooTxt), fooUTF8Bytes);
+      expect(delegate.assetsRead, [fooTxt]);
+    });
+
+    test('caches bytes from readAsString calls', () async {
+      expect(await reader.readAsString(fooTxt), fooContent);
+      expect(delegate.assetsRead, [fooTxt]);
+      delegate.assetsRead.clear();
+
+      expect(await reader.readAsBytes(fooTxt), fooUTF8Bytes);
+      expect(delegate.assetsRead, []);
+    });
+  });
+
+  group('readAsString', () {
+    test('should read from the delegate', () async {
+      expect(await reader.readAsString(fooTxt), fooContent);
+      expect(delegate.assetsRead, [fooTxt]);
+    });
+
+    test('should not re-read from the delegate', () async {
+      expect(await reader.readAsString(fooTxt), fooContent);
+      delegate.assetsRead.clear();
+      expect(await reader.readAsString(fooTxt), fooContent);
+      expect(delegate.assetsRead, []);
+    });
+
+    test('can be invalidated with invalidate', () async {
+      expect(await reader.readAsString(fooTxt), fooContent);
+      delegate.assetsRead.clear();
+      expect(delegate.assetsRead, []);
+
+      reader.invalidate([fooTxt]);
+      expect(await reader.readAsString(fooTxt), fooContent);
+      expect(delegate.assetsRead, [fooTxt]);
+    });
+
+    test('uses cached bytes if available', () async {
+      expect(await reader.readAsBytes(fooTxt), fooUTF8Bytes);
+      expect(delegate.assetsRead, [fooTxt]);
+      delegate.assetsRead.clear();
+
+      expect(await reader.readAsString(fooTxt), fooContent);
+      expect(delegate.assetsRead, []);
+    });
+  });
+}
