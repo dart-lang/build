ab2f03b245310adb32736c6411764f86facef0bb
Jacob MacDonald <jakemac@google.com>
2017-10-27 14:33:06 -0700

    serve build errors directly on build failures

--- build_runner/lib/src/server/server.dart
+++ build_runner/lib/src/server/server.dart
@@ -21,8 +21,13 @@ Future<ServeHandler> createServeHandler(WatchImpl watch) async {
 class ServeHandler implements BuildState {
   final BuildState _state;
   final AssetHandler _assetHandler;
+  BuildResult _lastBuildResult;
 
-  ServeHandler._(this._state, this._assetHandler);
+  ServeHandler._(this._state, this._assetHandler) {
+    _state.buildResults.listen((result) {
+      _lastBuildResult = result;
+    });
+  }
 
   @override
   Future<BuildResult> get currentBuild => _state.currentBuild;
@@ -39,8 +44,24 @@ class ServeHandler implements BuildState {
 
   FutureOr<Response> _handle(Request request, String rootDir) async {
     await currentBuild;
+    if (_lastBuildResult.status == BuildStatus.failure) {
+      return new Response(200,
+          body: '<h1>Build failed!</h1>'
+              '<p><strong>Error:</strong></p>'
+              '<p><strong style="color: red">'
+              '${_htmlify(_lastBuildResult.exception)}'
+              '</strong></p>'
+              '<p><strong>Stack Trace:</strong></p>'
+              '<p>${_htmlify(_lastBuildResult.stackTrace)}</p>',
+          headers: {
+            HttpHeaders.CONTENT_TYPE: 'text/html',
+          });
+    }
     return _assetHandler.handle(request, rootDir);
   }
+
+  String _htmlify(content) =>
+      content.toString().replaceAll('\n', '<br/>').replaceAll(' ', '&nbsp;');
 }
 
 class AssetHandler {
