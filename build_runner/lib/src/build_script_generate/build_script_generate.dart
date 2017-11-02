41157aef74cab62b554d4a9479ef7d63b95bc983
Nate Bosch <nbosch@google.com>
2017-11-02 13:55:47 -0700

    Outline binary to generate and run build script
    This script does not do anything useful yet - demonstrate finding
    builders, writing a script to disk, and running it with `dart`.
    
    - Add a binary ad bin/serve.dart
    - Create a PackageGraph and find packages which have a `build.yaml`
    - Write a script to `.dart_tool/build/build.dart` and execute it

---
+++ build_runner/lib/src/build_script_generate/build_script_generate.dart
@@ -0,0 +1,41 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:build_runner/build_runner.dart';
+import 'package:build_config/build_config.dart';
+
+const _scriptDir = '.dart_tool/build';
+const scriptLocation = '$_scriptDir/build.dart';
+
+Future<Null> ensureBuildScript() async {
+  var scriptFile = new File(scriptLocation);
+  // TODO - how can we invalidate this?
+  if (scriptFile.existsSync()) return;
+  var scriptDir = new Directory(_scriptDir);
+  if (!scriptDir.existsSync()) scriptDir.createSync(recursive: true);
+  await scriptFile.writeAsString(await _generateBuildScript());
+}
+
+Future<String> _generateBuildScript() async {
+  var result = new StringBuffer();
+  var packageGraph = new PackageGraph.forThisPackage();
+  var buildConfigs =
+      await Future.wait(packageGraph.orderedPackages.map(_packageBuildConfig));
+  result.writeln('void main() {');
+  for (var config in buildConfigs) {
+    if (config.builderDefinitions.isNotEmpty) {
+      result.writeln('print("I would run: ${config.packageName} - '
+          '${config.builderDefinitions.keys.toList()}");');
+    }
+  }
+  result.writeln('}');
+  return '$result';
+}
+
+Future<BuildConfig> _packageBuildConfig(PackageNode package) async =>
+    BuildConfig.fromPackageDir(
+        await Pubspec.fromPackageDir(package.path), package.path);
