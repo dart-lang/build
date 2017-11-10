63ed30b06d41f60f8fb0ca0abd9c6ba68e8c6f57
Nate Bosch <nbosch@google.com>
2017-11-10 12:35:30 -0800

    Generate BuilderApplication style config

--- build_runner/lib/src/build_script_generate/build_script_generate.dart
+++ build_runner/lib/src/build_script_generate/build_script_generate.dart
@@ -38,24 +38,25 @@ Future<String> _generateBuildScript() async {
   return new DartFormatter().format('${library.accept(emitter)}');
 }
 
-Method _findBuildActions(Iterable<BuildConfig> buildConfigs) {
-  var statements = <Code>[
-    types.buildActions.newInstance([]).assignVar('actions').statement,
-    // TODO - Pass actual arguments
-    literalList([], new TypeReference((b) => b..symbol = 'String'))
-        .assignVar('args')
-        .statement,
-  ];
-  statements.addAll(_addBuildActions(buildConfigs));
-  statements.add(refer('actions').returned.statement);
-  return new Method((b) => b
-    ..name = '_buildActions'
-    ..requiredParameters.add(new Parameter((b) => b
-      ..name = 'packageGraph'
-      ..type = types.packageGraph))
-    ..returns = types.buildActions
-    ..body = new Block((b) => b..statements.addAll(statements)));
-}
+Method _findBuildActions(Iterable<BuildConfig> buildConfigs) =>
+    new Method((b) => b
+      ..name = '_buildActions'
+      ..requiredParameters.add(new Parameter((b) => b
+        ..name = 'packageGraph'
+        ..type = types.packageGraph))
+      ..returns = types.buildActions
+      ..body = new Block((b) => b
+        ..statements.addAll([
+          // TODO - Pass actual arguments
+          literalList([], new TypeReference((b) => b..symbol = 'String'))
+              .assignVar('args')
+              .statement,
+          _findBuilders(buildConfigs).assignVar('builders').statement,
+          refer('createBuildActions', 'package:build_runner/build_runner.dart')
+              .call([refer('packageGraph'), refer('builders')])
+              .returned
+              .statement,
+        ])));
 
 Method _main() => new Method((b) => b
   ..name = 'main'
@@ -91,55 +92,23 @@ Method _main() => new Method((b) => b
       refer('server').property('close').call([]).awaited.statement
     ])));
 
-Iterable<Code> _addBuildActions(Iterable<BuildConfig> configs) {
-  var statements = <Code>[];
-  for (var config in configs) {
-    var varName = 'buildersFor${config.packageName}';
-    statements.addAll([
-      _packageBuilders(config, varName),
-      refer('packageGraph')
-          .property('dependentsOf')
-          .call([literalString(config.packageName)])
-          .property('map')
-          .call([
-            new Method((b) => b
-              ..requiredParameters.add(new Parameter((b) => b..name = 'p'))
-              ..body = refer('p').property('name').code
-              ..lambda = true).closure
-          ])
-          .property('forEach')
-          .call([
-            new Method((b) => b
-              ..requiredParameters.add(new Parameter((b) => b..name = 'p'))
-              ..lambda = true
-              ..body = refer('actions').property('addAll').call([
-                refer(varName).property('map').call([
-                  new Method((b) => b
-                    ..requiredParameters
-                        .add(new Parameter((b) => b..name = 'b'))
-                    ..lambda = true
-                    ..body = types.buildAction
-                        .newInstance([refer('b'), refer('p')]).code).closure
-                ])
-              ]).code).closure
-          ])
-          .statement
-    ]);
-  }
-  return statements;
-}
-
-Code _packageBuilders(BuildConfig config, String varName) =>
-    literalList(config.builderDefinitions.values
-            .expand(_builderInstantiations)
-            .toList())
-        .assignVar(varName)
-        .statement;
-
-Iterable<Expression> _builderInstantiations(BuilderDefinition builder) =>
-    builder.builderFactories
-        .map((f) => refer(f, builder.import).call([refer('args')]));
-
 Future<BuildConfig> _packageBuildConfig(PackageNode package) async =>
     BuildConfig.fromPackageDir(
         await Pubspec.fromPackageDir(package.path), package.path);
+
+/// Returns a [LiteralListExpression] with `BuilderApplication` instances
+/// created using `apply`.
+Expression _findBuilders(Iterable<BuildConfig> configs) =>
+    literalList(configs.expand(_applyBuilders).toList());
+
+Iterable<Expression> _applyBuilders(BuildConfig config) =>
+    config.builderDefinitions.values.expand((definition) => definition
+        .builderFactories
+        .map((factory) => _applyBuilder(definition, factory)));
+
+Expression _applyBuilder(BuilderDefinition definition, String factory) =>
+    refer('apply', 'package:build_runner/build_runner.dart').call([
+      refer(factory, definition.import).call([refer('args')]),
+      refer('toDependentsOf', 'package:build_runner/build_runner.dart')
+          .call([literalString(definition.package)])
+    ]);
