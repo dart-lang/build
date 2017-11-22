8f86407e23a96e07be50fc3b06e100018b667955
Nate Bosch <nbosch@google.com>
2017-11-21 16:19:08 -0800

    Add builder ordering
    Towards #639
    
    Allows Builders to be applied in an order satisfiying their requirements
    
    - Use the `reuired_inputs` configuration to find all builders which may
      output a required file and treat them as a builder dependency.
    - Use `stronglyConnectedComponents` to order builders by dependency.
      Consider a cycle to be a failure case.
    - Add `auto_apply` to the DDC builder. Now that we can guarantee it is
      run in the correct order we don't need to add it manually at the end.
    
    The `ddc_bootstrap` builder is still applied manually since we don't
    have a solution to specify the input globs.

---
+++ build_runner/lib/src/build_script_generate/builder_ordering.dart
@@ -0,0 +1,40 @@
+import 'package:build_config/build_config.dart';
+
+import '../package_graph/dependency_ordering.dart';
+
+/// Put [builders] into an order such that any builder which specifies
+/// [BuilderDefinition.requiredInputs] will come after any builder which
+/// produces a desired output.
+///
+/// All builders which don't specify required inputs will come first, followed
+/// by the builders with required inputs. If there is a cycle in required inputs
+/// this will throw.
+Iterable<BuilderDefinition> findBuilderOrder(
+    Iterable<BuilderDefinition> builders) {
+  final result = <BuilderDefinition>[];
+  result.addAll(builders.where((b) => b.requiredInputs.isEmpty));
+  final toOrder = builders.where((b) => b.requiredInputs.isNotEmpty);
+  result.addAll(_findOrder(toOrder));
+  return result;
+}
+
+List<BuilderDefinition> _findOrder(Iterable<BuilderDefinition> builders) {
+  Iterable<BuilderDefinition> dependencies(BuilderDefinition parent) =>
+      builders.where((child) => _hasDependency(parent, child));
+  var components = stronglyConnectedComponents<String, BuilderDefinition>(
+      builders, _builderKey, dependencies);
+  return components.map((component) {
+    if (component.length > 1) {
+      throw new ArgumentError('Required input cycle for ${component.toList()}');
+    }
+    return component.single;
+  }).toList();
+}
+
+bool _hasDependency(BuilderDefinition parent, BuilderDefinition child) {
+  final childOutputs = child.buildExtensions.values.expand((v) => v).toSet();
+  return parent.requiredInputs
+      .any((input) => childOutputs.any((output) => output.endsWith(input)));
+}
+
+String _builderKey(BuilderDefinition b) => '${b.package}|${b.name}';
--- e2e_example/test/goldens/generated_build_script.dart
+++ e2e_example/test/goldens/generated_build_script.dart
@@ -10,19 +10,6 @@ List<_i1.BuildAction> _buildActions(_i1.PackageGraph packageGraph) {
         _i1.toNoneByDefault()),
     _i1.apply('provides_builder', 'some_builder', [_i2.someBuilder],
         _i1.toDependentsOf('provides_builder')),
-    _i1.apply('build_compilers', 'ddc_bootstrap',
-        [_i3.devCompilerBootstrapBuilder], _i1.toNoneByDefault()),
-    _i1.apply(
-        'build_compilers',
-        'ddc',
-        [
-          _i3.moduleBuilder,
-          _i3.unlinkedSummaryBuilder,
-          _i3.linkedSummaryBuilder,
-          _i3.devCompilerBuilder
-        ],
-        _i1.toNoneByDefault(),
-        isOptional: true),
     _i1.apply(
         'build_compilers',
         'ddc',
@@ -34,6 +21,8 @@ List<_i1.BuildAction> _buildActions(_i1.PackageGraph packageGraph) {
         ],
         _i1.toAllPackages(),
         isOptional: true),
+    _i1.apply('build_compilers', 'ddc_bootstrap',
+        [_i3.devCompilerBootstrapBuilder], _i1.toNoneByDefault()),
     _i1.apply('build_compilers', 'ddc_bootstrap',
         [_i3.devCompilerBootstrapBuilder], _i1.toPackage('e2e_example'),
         inputs: ['web/**.dart', 'test/**.browser_test.dart'])
