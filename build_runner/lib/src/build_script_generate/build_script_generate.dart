a95005ab99bb9b78c67e630c9e8691901e3d40cb
Nate Bosch <nbosch@google.com>
2017-11-22 22:49:50 -0800

    Minor cleanup - use Block.of

--- build_runner/lib/src/build_script_generate/build_script_generate.dart
+++ build_runner/lib/src/build_script_generate/build_script_generate.dart
@@ -79,55 +79,51 @@ Method _findBuildActions(Iterable<Expression> builderApplications) =>
         ..name = 'packageGraph'
         ..type = types.packageGraph))
       ..returns = types.buildActions
-      ..body = new Block((b) => b
-        ..statements.addAll([
-          // TODO - Pass actual arguments
-          literalList([], new TypeReference((b) => b..symbol = 'String'))
-              .assignVar('args')
-              .statement,
-          literalList(builderApplications.toList())
-              .assignVar('builders')
-              .statement,
-          refer('createBuildActions', 'package:build_runner/build_runner.dart')
-              .call([refer('packageGraph'), refer('builders')],
-                  {'args': refer('args')})
-              .returned
-              .statement,
-        ])));
+      ..body = new Block.of([
+        // TODO - Pass actual arguments
+        literalList([], new TypeReference((b) => b..symbol = 'String'))
+            .assignVar('args')
+            .statement,
+        literalList(builderApplications.toList())
+            .assignVar('builders')
+            .statement,
+        refer('createBuildActions', 'package:build_runner/build_runner.dart')
+            .call([refer('packageGraph'), refer('builders')],
+                {'args': refer('args')})
+            .returned
+            .statement,
+      ]));
 
 Method _main() => new Method((b) => b
   ..name = 'main'
   ..modifier = MethodModifier.async
-  ..body = new Block((b) => b
-    ..statements.addAll([
-      refer('_buildActions')
-          .call([types.packageGraph.newInstanceNamed('forThisPackage', [])])
-          .assignVar('actions')
-          .statement,
-      refer('watch', 'package:build_runner/build_runner.dart')
-          .call([refer('actions')], {'writeToCache': literalTrue})
-          .awaited
-          .assignVar('handler')
-          .statement,
-      refer('serve', 'package:shelf/shelf_io.dart')
-          .call([
-            refer('handler')
-                .property('handlerFor')
-                .call([literalString('web')]),
-            literalString('localhost'),
-            literal(8000)
-          ])
-          .awaited
-          .assignVar('server')
-          .statement,
-      refer('handler')
-          .property('buildResults')
-          .property('drain')
-          .call([])
-          .awaited
-          .statement,
-      refer('server').property('close').call([]).awaited.statement
-    ])));
+  ..body = new Block.of([
+    refer('_buildActions')
+        .call([types.packageGraph.newInstanceNamed('forThisPackage', [])])
+        .assignVar('actions')
+        .statement,
+    refer('watch', 'package:build_runner/build_runner.dart')
+        .call([refer('actions')], {'writeToCache': literalTrue})
+        .awaited
+        .assignVar('handler')
+        .statement,
+    refer('serve', 'package:shelf/shelf_io.dart')
+        .call([
+          refer('handler').property('handlerFor').call([literalString('web')]),
+          literalString('localhost'),
+          literal(8000)
+        ])
+        .awaited
+        .assignVar('server')
+        .statement,
+    refer('handler')
+        .property('buildResults')
+        .property('drain')
+        .call([])
+        .awaited
+        .statement,
+    refer('server').property('close').call([]).awaited.statement
+  ]));
 
 Future<BuildConfig> _packageBuildConfig(PackageNode package) async =>
     BuildConfig.fromPackageDir(
