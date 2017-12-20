2db6de89abac613284fc4400f8295b24c01d69f6
Nate Bosch <nbosch@google.com>
2017-12-20 14:22:49 -0800

    Factor our normalization functions and doc comments

--- build_config/lib/src/key_normalization.dart
+++ build_config/lib/src/key_normalization.dart
@@ -2,26 +2,36 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-String normalizeBuilderKeyDefinition(String builderKey, String packageName) {
-  if (builderKey.startsWith('|')) return '$packageName$builderKey';
-  if (!builderKey.contains('|')) return '$packageName|$builderKey';
-  return builderKey;
+String normalizeBuilderKeyDefinition(String builderKey, String packageName) =>
+    _normalizeDefinition(builderKey, packageName, '|');
+
+String normalizeBuilderKeyUsage(String builderKey, String packageName) =>
+    _normalizeUsage(builderKey, packageName, '|');
+
+String normalizeTargetKeyDefinition(String targetKey, String packageName) =>
+    _normalizeDefinition(targetKey, packageName, ':');
+
+String normalizeTargetKeyUsage(String targetKey, String packageName) =>
+    _normalizeUsage(targetKey, packageName, ':');
+
+/// Gives a full unique key for [name] used from [packageName].
+///
+/// If [name] omits the separator we assume it's referring to a target or
+/// builder named after a package (which is not this package). If [name] starts
+/// with the separator we assume it's referring to a target within the package
+/// it's used from.
+String _normalizeUsage(String name, String packageName, String separator) {
+  if (name.startsWith(separator)) return '$packageName$name';
+  if (!name.contains(separator)) return '$name|$name';
+  return name;
 }
 
-String normalizeBuilderKeyUsage(String builderKey, String packageName) {
-  if (builderKey.startsWith('|')) return '$packageName$builderKey';
-  if (!builderKey.contains('|')) return '$builderKey|$builderKey';
-  return builderKey;
-}
-
-String normalizeTargetKeyDefinition(String targetKey, String packageName) {
-  if (targetKey.startsWith(':')) return '$packageName$targetKey';
-  if (!targetKey.contains(':')) return '$packageName:$targetKey';
-  return targetKey;
-}
-
-String normalizeTargetKeyUsage(String targetKey, String packageName) {
-  if (targetKey.startsWith(':')) return '$packageName$targetKey';
-  if (!targetKey.contains(':')) return '$targetKey:$targetKey';
-  return targetKey;
+/// Gives a full unique key for [name] definied within [packageName].
+///
+/// The result is always '$packageName$separator$name since at definition the
+/// key must be referring to something within [packageName].
+String _normalizeDefinition(String name, String packageName, String separator) {
+  if (name.startsWith(separator)) return '$packageName$name';
+  if (!name.contains(separator)) return '$packageName$separator$name';
+  return name;
 }
