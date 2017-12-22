7d678d4833df3ed2a2212b01c4b38b50201087ac
Jacob MacDonald <jakemac@google.com>
2017-12-22 14:05:55 -0800

    add test command which uses create_merged_dir and --precompiled and adds html files as necessary

--- build_runner/bin/build_runner.dart
+++ build_runner/bin/build_runner.dart
@@ -18,7 +18,9 @@ Future<Null> main(List<String> args) async {
   final innerArgs = [scriptLocation]..addAll(args);
   if (stdioType(stdin) == StdioType.TERMINAL &&
       !args.any((a) => a.contains('assume-tty'))) {
-    innerArgs.add('--assume-tty');
+    // We want to insert this as the first arg after the command, trailing args
+    // might get forwarded elsewhere (such as package:test).
+    innerArgs.insert(2, '--assume-tty');
   }
   var buildRun = await new ProcessManager().spawn(dart, innerArgs);
   await buildRun.exitCode;
