a1707f96f61be1e781e52cc0b3c45bedc0b2f381
Jacob MacDonald <jakemac@google.com>
2018-01-02 12:42:15 -0800

    Release 0.7.2 with the test command (#794)

--- build_runner/bin/build_runner.dart
+++ build_runner/bin/build_runner.dart
@@ -9,20 +9,40 @@ import 'package:io/io.dart';
 import 'package:logging/logging.dart';
 
 import 'package:build_runner/src/build_script_generate/build_script_generate.dart';
+import 'package:build_runner/src/entrypoint/options.dart';
 import 'package:build_runner/src/logging/std_io_logging.dart';
 
 Future<Null> main(List<String> args) async {
   var logListener = Logger.root.onRecord.listen(stdIOLogListener);
   await ensureBuildScript();
   var dart = Platform.resolvedExecutable;
+
+  // Use the actual command runner to parse the args and immediately print the
+  // usage information if there is no command provided or the help command was
+  // explicitly invoked.
+  var commandRunner = new BuildCommandRunner([]);
+  var parsedArgs = commandRunner.parse(args);
+  var commandName = parsedArgs.command?.name;
+  if (commandName == null || commandName == 'help') {
+    commandRunner.printUsage();
+    return;
+  }
+
+  // The actual args we will pass to the generated entrypoint script.
   final innerArgs = [scriptLocation]..addAll(args);
+
+  // For commands that support the `assume-tty` flag, we want to force the right
+  // setting unless it was explicitly provided.
+  var command = commandRunner.commands[commandName];
+  var commandParser = command.argParser;
   if (stdioType(stdin) == StdioType.TERMINAL &&
-      !args.any((a) => a.contains('assume-tty')) &&
-      args.isNotEmpty) {
+      commandParser.options.containsKey('assume-tty') &&
+      !args.any((a) => a.contains('assume-tty'))) {
     // We want to insert this as the first arg after the command, trailing args
     // might get forwarded elsewhere (such as package:test).
-    innerArgs.insert(2, '--assume-tty');
+    innerArgs.insert(innerArgs.indexOf(commandName) + 1, '--assume-tty');
   }
+
   var buildRun = await new ProcessManager().spawn(dart, innerArgs);
   await buildRun.exitCode;
   await ProcessManager.terminateStdIn();
