974fbf829a446d73fff372f087b723a9d3fcddc6
Jacob MacDonald <jakemac@google.com>
2018-01-10 13:28:10 -0800

    clean up merge, remove unnecessary findAssets override

--- build_runner/test/common/in_memory_reader.dart
+++ build_runner/test/common/in_memory_reader.dart
@@ -1,13 +1,14 @@
 // Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-import 'dart:async';
 
 import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
-import 'package:glob/glob.dart';
 
-class InMemoryRunnerAssetReader extends InMemoryAssetReader {
+import 'package:build_runner/src/asset/reader.dart';
+
+class InMemoryRunnerAssetReader extends InMemoryAssetReader
+    implements RunnerAssetReader {
   InMemoryRunnerAssetReader(
       [Map<AssetId, dynamic> sourceAssets, String rootPackage])
       : super(sourceAssets: sourceAssets, rootPackage: rootPackage);
@@ -15,11 +16,4 @@ class InMemoryRunnerAssetReader extends InMemoryAssetReader {
   InMemoryRunnerAssetReader.shareAssetCache(Map<AssetId, List<int>> assetCache,
       {String rootPackage})
       : super.shareAssetCache(assetCache, rootPackage: rootPackage);
-
-  @override
-  Stream<AssetId> findAssets(Glob glob, {String package}) {
-    package ??= rootPackage;
-    return new Stream.fromIterable(assets.keys
-        .where((id) => id.package == package && glob.matches(id.path)));
-  }
 }
