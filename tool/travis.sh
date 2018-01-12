f0302d90fc7dde0c6faa8da1a044dd9e8d0ccddd
Jacob MacDonald <jakemac@google.com>
2018-01-12 13:12:50 -0800

    restore skipping info messages from analyzer and use fixed mono_repo

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,34 +5,43 @@
 set -e
 
 if [ -z "$PKG" ]; then
-  echo -e "PKG environment variable must be set!"
+  echo -e "[31mPKG environment variable must be set![0m"
   exit 1
 elif [ -z "$TASK" ]; then
-  echo -e "TASK environment variable must be set!"
+  echo -e "[31mTASK environment variable must be set![0m"
   exit 1
 fi
 
 pushd $PKG
 pub upgrade
 
+case $PKG in
+*) echo -e "No before_script specified for PKG '${PKG}'."
+  ;;
+esac
+
 case $TASK in
 dartanalyzer) echo
-  echo -e "TASK: dartanalyzer"
+  echo -e "[1mTASK: dartanalyzer[22m"
   dartanalyzer --fatal-infos --fatal-warnings .
   ;;
+dartanalyzer_1) echo
+  echo -e "[1mTASK: dartanalyzer_1[22m"
+  dartanalyzer --fatal-warnings .
+  ;;
 dartfmt) echo
-  echo -e "TASK: dartfmt"
+  echo -e "[1mTASK: dartfmt[22m"
   dartfmt -n --set-exit-if-changed .
   ;;
 test) echo
-  echo -e "TASK: test"
+  echo -e "[1mTASK: test[22m"
   pub run test
   ;;
 test_1) echo
-  echo -e "TASK: test_1"
+  echo -e "[1mTASK: test_1[22m"
   pub run test --run-skipped
   ;;
-*) echo -e "Not expecting TASK '${TASK}'. Error!"
+*) echo -e "[31mNot expecting TASK '${TASK}'. Error![0m"
   exit 1
   ;;
 esac
