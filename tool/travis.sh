d8650fc5df41e6cfe08a5c7aceb9e02e2fdd14b3
Kevin Moore <kevmoo@users.noreply.github.com>
2018-01-31 09:03:01 -0800

    Update tool/travis.sh w/ mono_repo 0.2.1 (#936)

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,39 +5,34 @@
 set -e
 
 if [ -z "$PKG" ]; then
-  echo -e "[31mPKG environment variable must be set![0m"
+  echo -e "\033[31mPKG environment variable must be set!\033[0m"
   exit 1
 elif [ -z "$TASK" ]; then
-  echo -e "[31mTASK environment variable must be set![0m"
+  echo -e "\033[31mTASK environment variable must be set!\033[0m"
   exit 1
 fi
 
 pushd $PKG
 pub upgrade
 
-case $PKG in
-*) echo -e "No before_script specified for PKG '${PKG}'."
-  ;;
-esac
-
 case $TASK in
 dartanalyzer) echo
-  echo -e "[1mTASK: dartanalyzer[22m"
+  echo -e "\033[1mTASK: dartanalyzer\033[22m"
   dartanalyzer --fatal-infos --fatal-warnings .
   ;;
 dartfmt) echo
-  echo -e "[1mTASK: dartfmt[22m"
+  echo -e "\033[1mTASK: dartfmt\033[22m"
   dartfmt -n --set-exit-if-changed .
   ;;
 test) echo
-  echo -e "[1mTASK: test[22m"
+  echo -e "\033[1mTASK: test\033[22m"
   pub run test
   ;;
 test_1) echo
-  echo -e "[1mTASK: test_1[22m"
+  echo -e "\033[1mTASK: test_1\033[22m"
   pub run test --run-skipped
   ;;
-*) echo -e "[31mNot expecting TASK '${TASK}'. Error![0m"
+*) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
   exit 1
   ;;
 esac
