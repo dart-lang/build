79980a3e366bfd62eb8575413db71e1daa458f35
Jacob MacDonald <jakemac@google.com>
2018-02-01 14:06:56 -0800

    run all tasks regardless of exit code

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Created with https://github.com/dart-lang/mono_repo
 
-# Fast fail the script on failures.
-set -e
-
 if [ -z "$PKG" ]; then
   echo -e '\033[31mPKG environment variable must be set!\033[0m'
   exit 1
@@ -13,9 +10,10 @@ if [ "$#" == "0" ]; then
   echo -e "\033[31mExpected at least one task!\033[0m"
   exit 1
 fi
+EXIT_CODE=0
 
 pushd $PKG
-pub upgrade
+pub upgrade || EXIT_CODE=$?
 
 while (( "$#" )); do
   TASK=$1
@@ -24,27 +22,29 @@ while (( "$#" )); do
   dartanalyzer) echo
     echo -e '\033[1mTASK: dartanalyzer\033[22m'
     echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
-    dartanalyzer --fatal-infos --fatal-warnings .
+    dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
     ;;
   dartfmt) echo
     echo -e '\033[1mTASK: dartfmt\033[22m'
     echo -e 'dartfmt -n --set-exit-if-changed .'
-    dartfmt -n --set-exit-if-changed .
+    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
     ;;
   test_0) echo
     echo -e '\033[1mTASK: test_0\033[22m'
     echo -e 'pub run test'
-    pub run test
+    pub run test || EXIT_CODE=$?
     ;;
   test_1) echo
     echo -e '\033[1mTASK: test_1\033[22m'
     echo -e 'pub run test --run-skipped'
-    pub run test --run-skipped
+    pub run test --run-skipped || EXIT_CODE=$?
     ;;
   *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
-    exit 1
+    EXIT_CODE=$?
     ;;
   esac
 
   shift
 done
+
+exit EXIT_CODE
