62290c2db7df647f3368eea4a6b013d4441f115a
Jacob MacDonald <jakemac@google.com>
2018-02-01 11:24:09 -0800

    move back to env vars for package names within each stage

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,16 +4,13 @@
 # Fast fail the script on failures.
 set -e
 
-if [ -z "$1" ]; then
-  echo -e '\033[31mExpected first arg which is a package name!\033[0m'
+if [ -z "$PKG" ]; then
+  echo -e '\033[31mPKG environment variable must be set!\033[0m'
   exit 1
-else
-  PKG=$1
-  shift
 fi
 
 if [ "$#" == "0" ]; then
-	echo -e "\033[31mExpected at least one task after the package name!\033[0m"
+	echo -e "\033[31mExpected at least one task!\033[0m"
 	exit 1
 fi
 
