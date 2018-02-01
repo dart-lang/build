1cefacd60d5304af1e760523b9bdd2ea201134a1
Jacob MacDonald <jakemac@google.com>
2018-02-01 09:01:35 -0800

    try out stages

---
+++ tool/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+if [ -z "$PKG" ]; then
+  echo -e '\033[31mPKG environment variable must be set!\033[0m'
+  exit 1
+fi
+
+pushd $PKG
+pub upgrade
+pub run build_runner build
