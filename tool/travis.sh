be0d2909f80106463c48ca082de2bb428a8405a1
Jacob MacDonald <jakemac@google.com>
2018-02-01 10:26:46 -0800

    try moving package/task to args and adding stages

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,39 +4,50 @@
 # Fast fail the script on failures.
 set -e
 
-if [ -z "$PKG" ]; then
-  echo -e '\033[31mPKG environment variable must be set!\033[0m'
+if [ -z "$1" ]; then
+  echo -e '\033[31mExpected first arg which is a package name!\033[0m'
   exit 1
-elif [ -z "$TASK" ]; then
-  echo -e '\033[31mTASK environment variable must be set!\033[0m'
-  exit 1
+else
+  PKG=$1
+  shift
+fi
+
+if [ "$#" == "0" ]; then
+	echo -e "\033[31mExpected at least one task after the package name!\033[0m"
+	exit 1
 fi
 
 pushd $PKG
 pub upgrade
 
-case $TASK in
-dartanalyzer) echo
-  echo -e '\033[1mTASK: dartanalyzer\033[22m'
-  echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
-  dartanalyzer --fatal-infos --fatal-warnings .
-  ;;
-dartfmt) echo
-  echo -e '\033[1mTASK: dartfmt\033[22m'
-  echo -e 'dartfmt -n --set-exit-if-changed .'
-  dartfmt -n --set-exit-if-changed .
-  ;;
-test_0) echo
-  echo -e '\033[1mTASK: test_0\033[22m'
-  echo -e 'pub run test'
-  pub run test
-  ;;
-test_1) echo
-  echo -e '\033[1mTASK: test_1\033[22m'
-  echo -e 'pub run test --run-skipped'
-  pub run test --run-skipped
-  ;;
-*) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
-  exit 1
-  ;;
-esac
+while (( "$#" )); do
+  TASK=$1
+
+  case $TASK in
+  dartanalyzer) echo
+    echo -e '\033[1mTASK: dartanalyzer\033[22m'
+    echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+    dartanalyzer --fatal-infos --fatal-warnings .
+    ;;
+  dartfmt) echo
+    echo -e '\033[1mTASK: dartfmt\033[22m'
+    echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt -n --set-exit-if-changed .
+    ;;
+  test_0) echo
+    echo -e '\033[1mTASK: test_0\033[22m'
+    echo -e 'pub run test'
+    pub run test
+    ;;
+  test_1) echo
+    echo -e '\033[1mTASK: test_1\033[22m'
+    echo -e 'pub run test --run-skipped'
+    pub run test --run-skipped
+    ;;
+  *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    exit 1
+    ;;
+  esac
+
+  shift
+done
