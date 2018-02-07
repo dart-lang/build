bfeacd4a9db0e4787da810f53ee0a5df5ae32df6
Jacob MacDonald <jakemac@google.com>
2018-02-07 13:48:59 -0800

    Use task groups from latest mono_repo (#978)

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Created with https://github.com/dart-lang/mono_repo
 
-# Fast fail the script on failures.
-set -e
-
 if [ -z "$PKG" ]; then
   echo -e '\033[31mPKG environment variable must be set!\033[0m'
   exit 1
@@ -15,7 +12,9 @@ if [ "$#" == "0" ]; then
 fi
 
 pushd $PKG
-pub upgrade
+pub upgrade || exit $?
+
+EXIT_CODE=0
 
 while (( "$#" )); do
   TASK=$1
@@ -23,52 +22,54 @@ while (( "$#" )); do
   command) echo
     echo -e '\033[1mTASK: command\033[22m'
     echo -e 'pub run build_runner test -- -p chrome'
-    pub run build_runner test -- -p chrome
+    pub run build_runner test -- -p chrome || EXIT_CODE=$?
     ;;
   dartanalyzer) echo
     echo -e '\033[1mTASK: dartanalyzer\033[22m'
     echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
-    dartanalyzer --fatal-infos --fatal-warnings .
+    dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
     ;;
   dartfmt) echo
     echo -e '\033[1mTASK: dartfmt\033[22m'
     echo -e 'dartfmt -n --set-exit-if-changed .'
-    dartfmt -n --set-exit-if-changed .
+    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
     ;;
   test_0) echo
     echo -e '\033[1mTASK: test_0\033[22m'
     echo -e 'pub run test'
-    pub run test
+    pub run test || EXIT_CODE=$?
     ;;
   test_1) echo
     echo -e '\033[1mTASK: test_1\033[22m'
     echo -e 'pub run test --run-skipped'
-    pub run test --run-skipped
+    pub run test --run-skipped || EXIT_CODE=$?
     ;;
   test_2) echo
     echo -e '\033[1mTASK: test_2\033[22m'
     echo -e 'pub run test --total-shards 4 --shard-index 0'
-    pub run test --total-shards 4 --shard-index 0
+    pub run test --total-shards 4 --shard-index 0 || EXIT_CODE=$?
     ;;
   test_3) echo
     echo -e '\033[1mTASK: test_3\033[22m'
     echo -e 'pub run test --total-shards 4 --shard-index 1'
-    pub run test --total-shards 4 --shard-index 1
+    pub run test --total-shards 4 --shard-index 1 || EXIT_CODE=$?
     ;;
   test_4) echo
     echo -e '\033[1mTASK: test_4\033[22m'
     echo -e 'pub run test --total-shards 4 --shard-index 2'
-    pub run test --total-shards 4 --shard-index 2
+    pub run test --total-shards 4 --shard-index 2 || EXIT_CODE=$?
     ;;
   test_5) echo
     echo -e '\033[1mTASK: test_5\033[22m'
     echo -e 'pub run test --total-shards 4 --shard-index 3'
-    pub run test --total-shards 4 --shard-index 3
+    pub run test --total-shards 4 --shard-index 3 || EXIT_CODE=$?
     ;;
   *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
-    exit 1
+    EXIT_CODE=1
     ;;
   esac
 
   shift
 done
+
+exit $EXIT_CODE
