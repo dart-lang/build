3e683c037b10ea586d6294630624d3e44752886e
Jacob MacDonald <jakemac@google.com>
2018-03-01 13:25:09 -0800

    revert build_runner change

--- build_runner/lib/src/asset_graph/node.dart
+++ build_runner/lib/src/asset_graph/node.dart
@@ -146,12 +146,8 @@ class GeneratedAssetNode extends AssetNode {
   /// Whether the asset should be placed in the build cache.
   final bool isHidden;
 
-  /// If this output was created as a part of a `PostProcessBuilder`.
-  final bool isPostProcessAsset;
-
   GeneratedAssetNode(
     AssetId id, {
-    bool isPostProcessAsset,
     Digest lastKnownDigest,
     Set<Glob> globs,
     Iterable<AssetId> inputs,
@@ -166,7 +162,6 @@ class GeneratedAssetNode extends AssetNode {
         this.inputs = inputs != null
             ? new SplayTreeSet.from(inputs)
             : new SplayTreeSet<AssetId>(),
-        this.isPostProcessAsset = isPostProcessAsset ?? false,
         super(id, lastKnownDigest: lastKnownDigest);
 
   @override
