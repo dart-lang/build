5f2176005ff397dffefe3f25816e8f889d4a146d
Jacob MacDonald <jakemac@google.com>
2018-03-02 12:22:21 -0800

    try adding an after_failure script that pings the build chat

---
+++ tool/report_failure.sh
@@ -0,0 +1,6 @@
+TRAVIS_BUILDS_URI="https://travis-ci.org/dart-lang/build/builds"
+if [ $TRAVIS_EVENT_TYPE -ne "pull_request" ]; then
+  curl -H "Content-Type: application/json" -X POST -d \
+    '{"text":"Build failed! $TRAVIS_BUILDS_URI/$TRAVIS_BUILD_ID"}' \
+    "$CHAT_HOOK_URI"
+fi
