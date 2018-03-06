e5a9f5bf1ef190718ceccc3734629d402e45fb13
Jacob MacDonald <jakemac@google.com>
2018-03-06 11:00:57 -0800

    validate minification in e2e tests as well

--- e2e_example/test/dart2js_integration_test.dart
+++ e2e_example/test/dart2js_integration_test.dart
@@ -18,7 +18,7 @@ void main() {
           usePrecompiled: true,
           useManualScript: false,
           args: ['--config=dart2js']);
-      await expectWasCompiledWithDart2Js();
+      await expectWasCompiledWithDart2Js(minified: false);
     }, onPlatform: {'windows': const Skip('flaky on windows')});
 
     test('via --define flag', () async {
@@ -29,17 +29,38 @@ void main() {
             '--define',
             'build_web_compilers|entrypoint=compiler=dart2js',
             '--define',
-            'build_web_compilers|entrypoint=dart2js_args=["--checked","--trust-primitives"]',
+            'build_web_compilers|entrypoint=dart2js_args=["--minify","--checked"]',
           ]);
-      await expectWasCompiledWithDart2Js();
+      await expectWasCompiledWithDart2Js(minified: true);
+    }, onPlatform: {'windows': const Skip('flaky on windows')});
+
+    test('--define overrides --config', () async {
+      await expectTestsPass(
+          usePrecompiled: true,
+          useManualScript: false,
+          args: [
+            '--config',
+            'dart2js',
+            '--define',
+            'build_web_compilers|entrypoint=compiler=dart2js',
+            '--define',
+            'build_web_compilers|entrypoint=dart2js_args=["--minify","--checked"]',
+          ]);
+      await expectWasCompiledWithDart2Js(minified: true);
     }, onPlatform: {'windows': const Skip('flaky on windows')});
   });
 }
 
-Future<Null> expectWasCompiledWithDart2Js() async {
+Future<Null> expectWasCompiledWithDart2Js({bool minified: false}) async {
   var jsFile =
       new File('.dart_tool/build/generated/e2e_example/web/main.dart.js');
   expect(await jsFile.exists(), isTrue);
   // sanity check that it was indeed compiled with dart2js
-  expect(await jsFile.readAsString(), contains('dart2js'));
+  var content = await jsFile.readAsString();
+  expect(content, contains('function generateAccessor('));
+  if (minified) {
+    expect(content, isNot(startsWith('//')));
+  } else {
+    expect(content, startsWith('//'));
+  }
 }
