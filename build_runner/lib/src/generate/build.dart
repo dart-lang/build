8584cef4b7cbfc48ad303a7e7b820766fc4abb5a
Gary Roumanis <grouma@google.com>
2018-04-04 19:15:54 -0700

    fix doc comment

--- build_runner/lib/src/generate/build.dart
+++ build_runner/lib/src/generate/build.dart
@@ -42,8 +42,9 @@ import 'watch_impl.dart' as watch_impl;
 /// will simply consume the first event and allow the build to continue.
 /// Multiple termination events will cause a normal shutdown.
 ///
-/// If [outputDir] is supplied then after each build a merged output directory
-/// will be created containing all original sources and built sources.
+/// If [outputMap] is supplied then after each build a merged output directory
+/// will be created for each value in the map which contains all original 
+/// sources and built sources contained in the provided path.
 ///
 /// If [verbose] is `true` then verbose logging will be enabled. This changes
 /// the default [logLevel] to [Level.ALL] and removes stack frame folding, among
