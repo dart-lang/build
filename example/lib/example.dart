1b2d87e955a8cea81246972ec3033f1625432878
Kevin Moore <kevmoo@google.com>
2018-04-06 10:47:17 -0700

    Big simplify and cleanup

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -2,15 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: annotate_overrides
-
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:build/build.dart';
 
-/// A really simple [Builder], it just makes copies!
+/// Copy contents of a `txt` files into `name.txt.copy`.
+///
+/// A header row is added pointing to the input file name.
 class CopyBuilder implements Builder {
+  @override
   Future build(BuildStep buildStep) async {
     /// Each [buildStep] has a single input.
     var inputId = buildStep.inputId;
@@ -21,38 +21,30 @@ class CopyBuilder implements Builder {
     var copy = inputId.addExtension('.copy');
 
     /// Write out the new asset.
-    ///
-    /// There is no need to `await` here, the system handles waiting on these
-    /// files as necessary before advancing to the next phase.
-    // ignore: unawaited_futures
-    buildStep.writeAsString(copy, '// Copied from $inputId\n$contents');
+    await buildStep.writeAsString(copy, '// Copied from $inputId\n$contents');
   }
 
-  /// Configure output extensions. All possible inputs match the empty input
-  /// extension. For each input 1 output is created with `extension` appended to
-  /// the path.
+  @override
   final buildExtensions = const {
-    '.txt': ['.txt.copy']
+    '.txt': const ['.txt.copy']
   };
 }
 
+/// Adds `generated.css` to the `web` directory.
 class CssBuilder implements Builder {
+  @override
   Future build(BuildStep buildStep) async {
-    // ignore: unawaited_futures
-    buildStep.writeAsString(
+    await buildStep.writeAsString(
         new AssetId(buildStep.inputId.package, 'web/generated.css'),
         _cssContent(buildStep.inputId));
   }
 
-  /// Configure output extensions. All possible inputs match the empty input
-  /// extension. For each input 1 output is created with `extension` appended to
-  /// the path.
+  @override
   final buildExtensions = const {
-    r'$web$': ['generated.css']
+    r'$web$': const ['generated.css']
   };
-}
 
-String _cssContent(AssetId inputId) => '''
+  static String _cssContent(AssetId inputId) => '''
 /*
 Generated at: ${new DateTime.now()}
      AssetId: $inputId
@@ -60,39 +52,31 @@ Generated at: ${new DateTime.now()}
 pre {
   font-size: 200%;
 }''';
+}
 
+/// For a source `dart` file, generate `file.dart.info.json` containing
+/// information about the
 class ResolvingBuilder implements Builder {
+  @override
   Future build(BuildStep buildStep) async {
-    // Get the [LibraryElement] for the primary input.
+    /// Get the `LibraryElement` for the primary input.
     var entryLib = await buildStep.inputLibrary;
 
-    // Resolves all libraries reachable from the primary input.
+    /// Resolves all libraries reachable from the primary input.
     var resolver = buildStep.resolver;
     var visibleLibraries = await resolver.libraries.length;
 
-    var info = buildStep.inputId.addExtension('.info.json');
+    var info = buildStep.inputId.addExtension('.info');
 
-    /// Write out the new asset.
-    ///
-    /// There is no need to `await` here, the system handles waiting on these
-    /// files as necessary before advancing to the next phase.
-    // ignore: unawaited_futures
-    buildStep.writeAsString(
-        info,
-        _prettyToJson({
-          'name': entryLib.name,
-          'member count': entryLib.unit.declarations.length,
-          'visible libraries': visibleLibraries
-        }));
+    await buildStep.writeAsString(info, '''
+         Input ID: ${buildStep.inputId}
+     Member count: ${entryLib.unit.declarations.length}
+Visible libraries: $visibleLibraries
+''');
   }
 
-  /// Configure output extensions. All possible inputs match the empty input
-  /// extension. For each input 1 output is created with `extension` appended to
-  /// the path.
+  @override
   final buildExtensions = const {
-    '.dart': ['.dart.info.json']
+    '.dart': const ['.dart.info']
   };
 }
-
-String _prettyToJson(Object object) =>
-    const JsonEncoder.withIndent(' ').convert(object) + '\n';
