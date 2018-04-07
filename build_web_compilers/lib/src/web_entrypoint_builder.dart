3c919fcfa6f688ecb8caad6bb9ce3eeaaf1efe6c
Nate Bosch <nbosch1@gmail.com>
2018-04-06 17:50:27 -0700

    Compile with dart2js by default in release mode (#1266)
    Closes #1109
    
    - Add default `release_options` to switch compilers in release mode.
    - Move the `--minify` default args to the `build.yaml` file since it is
      more discoverable.
    - Bump minimum required version of `build_config` to one that supports
      the default options.
    - Remove `target` and `input_extensions` fields since they are ignored
      and no longer required.
    - Add a test which checks dart2js compilation using the `--release`
      flag.

--- build_web_compilers/lib/src/web_entrypoint_builder.dart
+++ build_web_compilers/lib/src/web_entrypoint_builder.dart
@@ -73,8 +73,7 @@ class WebEntrypointBuilder implements Builder {
             'Only `dartdevc` and `dart2js` are supported.');
     }
 
-    var dart2JsArgs =
-        options.config[_dart2jsArgs] ?? const <String>['--minify'];
+    var dart2JsArgs = options.config[_dart2jsArgs] ?? const <String>[];
     if (dart2JsArgs is! List<String>) {
       throw new ArgumentError.value(dart2JsArgs, _dart2jsArgs,
           'Expected a list of strings, but got a ${dart2JsArgs.runtimeType}:');
--- e2e_example/test/goldens/generated_build_script.dart
+++ e2e_example/test/goldens/generated_build_script.dart
@@ -4,7 +4,9 @@ import 'package:build_test/builder.dart' as _i3;
 import 'package:build_config/build_config.dart' as _i4;
 import 'package:build_modules/builders.dart' as _i5;
 import 'package:build_web_compilers/builders.dart' as _i6;
-import 'dart:isolate' as _i7;
+import 'package:build/build.dart' as _i7;
+import 'dart:convert' as _i8;
+import 'dart:isolate' as _i9;
 
 final _builders = [
   _i1.apply('provides_builder|some_not_applied_builder', [_i2.notApplied],
@@ -41,6 +43,10 @@ final _builders = [
         'test/**.node_test.dart',
         'test/**.vm_test.dart'
       ]),
+      defaultOptions: new _i7.BuilderOptions(
+          _i8.json.decode('{"dart2js_args":["--minify"]}')),
+      defaultReleaseOptions:
+          new _i7.BuilderOptions(_i8.json.decode('{"compiler":"dart2js"}')),
       appliesBuilders: ['build_web_compilers|dart2js_archive_extractor']),
   _i1.applyPostProcess(
       'provides_builder|some_post_process_builder', _i2.somePostProcessBuilder,
@@ -49,7 +55,7 @@ final _builders = [
       _i6.dart2JsArchiveExtractor,
       defaultGenerateFor: const _i4.InputSet())
 ];
-main(List<String> args, [_i7.SendPort sendPort]) async {
+main(List<String> args, [_i9.SendPort sendPort]) async {
   var result = await _i1.run(args, _builders);
   sendPort?.send(result);
 }
