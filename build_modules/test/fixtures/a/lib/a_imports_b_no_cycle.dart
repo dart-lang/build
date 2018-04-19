235a3abe22a9bf759e5292bc05b734b1c07fa6b0
Nate Bosch <nbosch1@gmail.com>
2018-04-19 14:53:18 -0700

    Return module dependencies in reverse dependency order (#1316)
    This will be useful to limit the number of in flight lazy builds by
    building from the bottom up.

--- build_modules/test/fixtures/a/lib/a_imports_b_no_cycle.dart
+++ build_modules/test/fixtures/a/lib/a_imports_b_no_cycle.dart
@@ -2,5 +2,6 @@ library a_imports_b_no_cycle;
 
 // ignore_for_file: unused_import
 import 'package:b/b_imports_a_no_cycle.dart';
+import 'package:b/b_second_import_to_a_no_cycle.dart';
 
 part 'a_part_library_name.dart';
