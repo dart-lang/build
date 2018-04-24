ea97f5837a9003c441d840212990a3608f5c9b14
Nate Bosch <nbosch@google.com>
2018-04-24 16:22:54 -0700

    Add module_cleaup builder
    Towards #1308
    
    Removes all the outputs of the build_modules builder.

--- build_modules/lib/builders.dart
+++ build_modules/lib/builders.dart
@@ -6,6 +6,7 @@ import 'package:build/build.dart';
 import 'package:build_modules/build_modules.dart';
 import 'package:build_modules/src/meta_module_builder.dart';
 import 'package:build_modules/src/meta_module_clean_builder.dart';
+import 'package:build_modules/src/module_cleanup.dart';
 
 Builder moduleBuilder(BuilderOptions options) =>
     new ModuleBuilder.forOptions(options);
@@ -14,3 +15,5 @@ Builder linkedSummaryBuilder(_) => const LinkedSummaryBuilder();
 Builder metaModuleBuilder(_) => const MetaModuleBuilder();
 Builder metaModuleCleanBuilder(BuilderOptions options) =>
     const MetaModuleCleanBuilder();
+
+PostProcessBuilder moduleCleanup(_) => const ModuleCleanup();
