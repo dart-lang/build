35b64d11afdde9c10e6aed52a69445a668e918b1
Kevin Moore <kevmoo@google.com>
2018-04-26 12:36:28 -0700

    Log some semi-helpful messages if the graph page fails a query request
    Fixes https://github.com/dart-lang/build/issues/1372

--- build_runner/lib/src/server/graph_viz_main.dart
+++ build_runner/lib/src/server/graph_viz_main.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
 import 'dart:js';
@@ -15,21 +16,42 @@ main() async {
   searchForm.onSubmit.listen((e) async {
     e.preventDefault();
     var query = searchBox.value;
-    _focus(query);
+    await _focus(query);
     return null;
   });
   _graphReference.callMethod('initializeGraph', [_focus]);
 }
 
-void _focus(String query) async {
-  var parts = query.split('|');
-  var package = parts[0];
-  var path = parts[1];
-  var requestPath = path.startsWith('lib/')
-      ? 'packages/$package/${path.substring(4)}'
-      : '${path.split('/').skip(1).join('/')}';
-  var nodeInfo =
-      json.decode(await HttpRequest.getString('/\$graph/$requestPath'));
+void _error(String message, Object error, StackTrace stack) {
+  // TODO: update the user page some how?
+  window.console.error(message);
+  window.console.log(error);
+  window.console.log(stack);
+}
+
+Future _focus(String query) async {
+  String requestPath;
+  try {
+    var parts = query.split('|');
+    var package = parts[0];
+    var path = parts[1];
+    requestPath = path.startsWith('lib/')
+        ? 'packages/$package/${path.substring(4)}'
+        : '${path.split('/').skip(1).join('/')}';
+  } catch (e, stack) {
+    _error('The query you provided ($query) could not be parsed.', e, stack);
+    return;
+  }
+
+  Map nodeInfo;
+  try {
+    nodeInfo = json.decode(await HttpRequest.getString('/\$graph/$requestPath'))
+        as Map<String, dynamic>;
+  } catch (e, stack) {
+    _error('Got an error making a request.', e, stack);
+    return;
+  }
+
   var graphData = {'edges': nodeInfo['edges'], 'nodes': nodeInfo['nodes']};
   _graphReference.callMethod('setData', [new JsObject.jsify(graphData)]);
   var primaryNode = nodeInfo['primary'];
