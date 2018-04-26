b50271c08608cb4b0e8a69684d99ddea858cc789
Kevin Moore <kevmoo@users.noreply.github.com>
2018-04-26 16:44:11 -0700

    Log some semi-helpful messages if the graph page fails a query request (#1373)
    Fixes https://github.com/dart-lang/build/issues/1372

--- build_runner/lib/src/server/graph_viz_main.dart
+++ build_runner/lib/src/server/graph_viz_main.dart
@@ -2,36 +2,69 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
-import 'dart:js';
+import 'dart:js' as js;
 
-final _graphReference = context[r'$build'];
+final _graphReference = js.context[r'$build'];
 final _details = document.getElementById('details');
 
 main() async {
   var searchBox = document.getElementById('searchbox') as InputElement;
   var searchForm = document.getElementById('searchform');
-  searchForm.onSubmit.listen((e) async {
+  searchForm.onSubmit.listen((e) {
     e.preventDefault();
-    var query = searchBox.value;
-    _focus(query);
+    _focus(searchBox.value);
     return null;
   });
   _graphReference.callMethod('initializeGraph', [_focus]);
 }
 
-void _focus(String query) async {
-  var parts = query.split('|');
-  var package = parts[0];
-  var path = parts[1];
-  var requestPath = path.startsWith('lib/')
-      ? 'packages/$package/${path.substring(4)}'
-      : '${path.split('/').skip(1).join('/')}';
-  var nodeInfo =
-      json.decode(await HttpRequest.getString('/\$graph/$requestPath'));
+void _error(String message, [Object error, StackTrace stack]) {
+  var msg = [message, error, stack].where((e) => e != null).join('\n');
+  _details.innerHtml = '<pre>$msg</pre>';
+}
+
+Future _focus(String query) async {
+  String requestPath;
+  try {
+    var parts = query.split('|');
+    var package = parts[0];
+    var path = parts[1];
+    requestPath = path.startsWith('lib/')
+        ? 'packages/$package/${path.substring(4)}'
+        : '${path.split('/').skip(1).join('/')}';
+  } catch (e, stack) {
+    _error('The query you provided "$query" could not be parsed.', e, stack);
+    return;
+  }
+
+  var url = '/\$graph/$requestPath';
+  Map nodeInfo;
+  try {
+    nodeInfo =
+        json.decode(await HttpRequest.getString(url)) as Map<String, dynamic>;
+  } catch (e, stack) {
+    var msg = 'Error making a request: $url';
+    if (e is ProgressEvent) {
+      var target = e.target;
+      if (target is HttpRequest) {
+        msg = [
+          msg,
+          '${target.status} ${target.statusText}',
+          target.responseText
+        ].join('\n');
+      }
+      _error(msg);
+    } else {
+      _error(msg, e, stack);
+    }
+    return;
+  }
+
   var graphData = {'edges': nodeInfo['edges'], 'nodes': nodeInfo['nodes']};
-  _graphReference.callMethod('setData', [new JsObject.jsify(graphData)]);
+  _graphReference.callMethod('setData', [new js.JsObject.jsify(graphData)]);
   var primaryNode = nodeInfo['primary'];
   _details.innerHtml = '<strong>ID:</strong> ${primaryNode['id']} <br />'
       '<strong>Generated:</strong> ${primaryNode['isGenerated']} <br />'
