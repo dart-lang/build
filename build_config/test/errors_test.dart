91fcbd42d7bf70804b68c073e22e879d58580d1a
Nate Bosch <nbosch@google.com>
2018-05-16 12:34:41 -0700

    Add test

---
+++ build_config/test/errors_test.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+import 'package:build_config/build_config.dart';
+
+void main() {
+  group('parse errors', () {
+    test('for missing default target', () {
+      var buildYaml = r'''
+targets:
+  not_package_name:
+    sources: ["lib/**"]
+''';
+      expect(() => new BuildConfig.parse('package_name', [], buildYaml),
+          throwsArgumentError);
+      ;
+    });
+  });
+}
