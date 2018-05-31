babc07722e24dcafc40dfa331f1c2fb7a3393895
Gary Roumanis <grouma@google.com>
2018-05-31 15:09:00 -0700

    fix lint issues

--- build_runner/lib/build_runner.dart
+++ build_runner/lib/build_runner.dart
@@ -3,12 +3,9 @@
 // BSD-style license that can be found in the LICENSE file.
 
 export 'package:build/build.dart' show PostProcessBuilder, PostProcessBuildStep;
-
 export 'package:build_runner_core/src/asset/file_based.dart';
 export 'package:build_runner_core/src/asset/reader.dart' show RunnerAssetReader;
 export 'package:build_runner_core/src/asset/writer.dart';
-export 'src/entrypoint/run.dart' show run;
-export 'src/generate/build.dart';
 export 'package:build_runner_core/src/generate/build_result.dart';
 export 'package:build_runner_core/src/generate/exceptions.dart'
     show CannotBuildException;
@@ -28,4 +25,7 @@ export 'package:build_runner_core/src/package_graph/apply_builders.dart'
         toPackages,
         toRoot;
 export 'package:build_runner_core/src/package_graph/package_graph.dart';
+
+export 'src/entrypoint/run.dart' show run;
+export 'src/generate/build.dart';
 export 'src/server/server.dart' show ServeHandler;
