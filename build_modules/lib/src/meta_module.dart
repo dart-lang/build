23aff96c1a8af13b37db438adaeae84fb4777e52
Nikolay Samokhin <nickclmb@gmail.com>
2018-06-14 16:58:39 +0300

    use parseDirectives only when "public == true" check passes
    Signed-off-by: Nikolay Samokhin <nickclmb@gmail.com>

--- build_modules/lib/src/meta_module.dart
+++ build_modules/lib/src/meta_module.dart
@@ -262,8 +262,10 @@ Future<List<Module>> _computeModules(
   for (var asset in assets) {
     var content = await reader.readAsString(asset);
     // Skip errors here, dartdevc gives nicer messages.
-    var parsed =
-        parseDirectives(content, name: asset.path, suppressErrors: true);
+    var parsed = public
+        ? parseDirectives(content, name: asset.path, suppressErrors: true)
+        : parseCompilationUnit(content,
+            name: asset.path, suppressErrors: true, parseFunctionBodies: false);
     parsedAssetsById[asset] = parsed;
 
     // Skip any files which contain a `dart:_` import.
