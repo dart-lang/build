447ac70781e9c35de13e7de74f9ee2baacaac2b7
Nate Bosch <nbosch@google.com>
2018-06-20 12:48:29 -0700

    Drop build_barback package
    - Switch to build_resolvers everywhere.
    - Drop the package.
    - Remove references in the README.
    - Remove Travis config.

--- build_barback/lib/build_barback.dart
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-export 'src/analyzer/resolver.dart';
-export 'src/builder/transformer_builder.dart';
-export 'src/transformer/transformer.dart';
