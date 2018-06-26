9bb85308477a1729392d0398d3fc944cfc05daa8
Jacob MacDonald <jakemac@google.com>
2018-06-26 15:16:41 -0700

    add test for multiple mains

--- build_vm_compilers/test/vm_kernel_integration_test.dart
+++ build_vm_compilers/test/vm_kernel_integration_test.dart
@@ -8,43 +8,61 @@ import 'package:test_descriptor/test_descriptor.dart' as d;
 import 'package:_test_common/common.dart';
 
 void main() {
-  setUp(() async {
-    await d.dir('a', [
-      await pubspec('a', currentIsolateDependencies: [
-        'build',
-        'build_config',
-        'build_modules',
-        'build_resolvers',
-        'build_runner',
-        'build_runner_core',
-        'build_vm_compilers',
-      ], versionDependencies: {
-        'glob': 'any',
-        'path': 'any',
-      }),
-      d.dir('bin', [
-        d.file('hello.dart', '''
+  group('can compile vm apps to kernel', () {
+    setUpAll(() async {
+      await d.dir('a', [
+        await pubspec('a', currentIsolateDependencies: [
+          'build',
+          'build_config',
+          'build_modules',
+          'build_resolvers',
+          'build_runner',
+          'build_runner_core',
+          'build_vm_compilers',
+        ], versionDependencies: {
+          'glob': 'any',
+          'path': 'any',
+        }),
+        d.dir('bin', [
+          d.file('hello.dart', '''
 import 'package:path/path.dart' as p;
 
 void main() {
   print(p.url.join('hello', 'world'));
 }
 '''),
-      ]),
-    ]).create();
+          d.file('goodbye.dart', '''
+import 'package:path/path.dart' as p;
 
-    await pubGet('a');
+import 'hello.dart';
+
+void main() {
+  print(p.url.join('goodbye', 'world'));
+}
+'''),
+        ]),
+      ]).create();
+
+      await pubGet('a');
+
+      // Run a build and expect it to succeed.
+      var buildResult = await runPub('a', 'run',
+          args: ['build_runner', 'build', '-o', 'out']);
+      expect(buildResult.exitCode, 0, reason: buildResult.stderr as String);
+    });
+
+    test(' and run them', () async {
+      var runResult = await runDart('a', 'out/bin/hello.vm.app.dill');
+
+      expect(runResult.exitCode, 0, reason: runResult.stderr as String);
+      expect(runResult.stdout, 'hello/world\n');
+    });
+
+    test(' and run root libraries main', () async {
+      var runResult = await runDart('a', 'out/bin/goodbye.vm.app.dill');
+
+      expect(runResult.exitCode, 0, reason: runResult.stderr as String);
+      expect(runResult.stdout, 'goodbye/world\n');
+    });
   });
-
-  test('can compile vm apps to kernel and run them', () async {
-    // Run a build and expect it to succeed.
-    var buildResult =
-        await runPub('a', 'run', args: ['build_runner', 'build', '-o', 'out']);
-    expect(buildResult.exitCode, 0, reason: buildResult.stderr as String);
-    var runResult = await runDart('a', 'out/bin/hello.vm.app.dill');
-
-    // Run the app and expect certain output.
-    expect(runResult.exitCode, 0, reason: runResult.stderr as String);
-    expect(runResult.stdout, 'hello/world\n');
-  }, skip: 'Blocked on https://dart-review.googlesource.com/c/sdk/+/62120');
 }
