e157b1e02e29ab6d745d2ab85a638b86c7319a9d
Jacob MacDonald <jakemac@google.com>
2018-06-29 13:19:49 -0700

    add more integration test tags, shard the build_runner integration tests, and increase sharding of _test tests

--- build_runner/test/entrypoint/clean_integration_test.dart
+++ build_runner/test/entrypoint/clean_integration_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@Tags(const ['integration'])
+
 import 'package:test/test.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
 
