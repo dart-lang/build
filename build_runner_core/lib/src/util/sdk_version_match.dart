ad66466127bd239441162e0ac58e8b471bc4de7c
Nikolay Samokhin <nickclmb@gmail.com>
2018-07-04 11:51:00 +0300

    remove dependency on pub_semver
    Signed-off-by: Nikolay Samokhin <nickclmb@gmail.com>

--- build_runner_core/lib/src/util/sdk_version_match.dart
+++ build_runner_core/lib/src/util/sdk_version_match.dart
@@ -1,14 +1,7 @@
-import 'package:pub_semver/pub_semver.dart';
-
 bool areSdkVersionsSame(String thisVersion, String thatVersion) {
   if (thisVersion == null || thatVersion == null) {
-    return false;
-  }
-
-  try {
-    return new Version.parse(thisVersion.split(' ').first) ==
-        new Version.parse(thatVersion.split(' ').first);
-  } on FormatException {
     return thisVersion == thatVersion;
   }
+
+  return thisVersion.split(' ').first == thatVersion.split(' ').first;
 }
