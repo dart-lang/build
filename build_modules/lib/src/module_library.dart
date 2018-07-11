ded62f51d1577e627b22d4e208ddbcc47dfcb813
Nate Bosch <nbosch@google.com>
2018-07-11 14:46:31 -0700

    Add a `ModuleLibrary` class
    Towards #1644
    
    Explicitly separate out the step of parsing a Dart source file and
    classifying directives from the other work happening in the meta module.
    The `ModuleLibrary` class is essentially the `_AssetNode` class from
    before, except it is also responsible for determining which files should
    be skipped and which are entrypoints rather than that responsibilty
    being split with `_computeModules`.
    
    "internal" and "external" dependencies are also not tracked separately
    since it is applied as a filter only for the strongly connected
    components call.
    
    Eventually we will add in a capability to store a `ModuleLibrary` which
    will let us write it as a separate Asset and then restore it to
    compute meta modules.
    
    One minor change in behavior is that we no longer throw if a "part"
    directive reaches across packages or top-level directories. This should
    be resolved if there are package level cycles by the next iteration of
    strongly connected components.

---
+++ build_modules/lib/src/module_library.dart
@@ -0,0 +1,112 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/analyzer.dart';
+import 'package:build/build.dart';
+import 'package:meta/meta.dart';
+
+class ModuleLibrary {
+  /// The AssetId of the original Dart source file.
+  final AssetId id;
+
+  /// Whether this library can be imported.
+  ///
+  /// This will be false if the source file is a "part of", or imports code that
+  /// can't be used outside the SDK.
+  final bool isImportable;
+
+  /// Whether this library is an entrypoint.
+  ///
+  /// True if the library is in `lib/` but not `lib/src`, or if it is outside of
+  /// `lib/` and contains a `main` method. Always false if this is not an
+  /// importable library.
+  final bool isEntryPoint;
+
+  /// The IDs of libraries that are imported or exported by this library.
+  ///
+  /// Null if this is not an importable library.
+  final Set<AssetId> deps;
+
+  /// The "part" files for this library.
+  ///
+  /// Null if this is not an importable library.
+  final Set<AssetId> parts;
+
+  ModuleLibrary._(this.id,
+      {@required this.isEntryPoint, @required this.deps, @required this.parts})
+      : isImportable = true;
+
+  ModuleLibrary._nonImportable(this.id)
+      : isImportable = false,
+        isEntryPoint = false,
+        deps = null,
+        parts = null;
+
+  factory ModuleLibrary._fromCompilationUnit(
+      AssetId id, bool isEntryPoint, CompilationUnit parsed) {
+    var deps = new Set<AssetId>();
+    var parts = new Set<AssetId>();
+    for (var directive in parsed.directives) {
+      if (directive is! UriBasedDirective) continue;
+      var path = (directive as UriBasedDirective).uri.stringValue;
+      if (Uri.parse(path).scheme == 'dart') continue;
+      var linkedId = new AssetId.resolve(path, from: id);
+      if (linkedId == null) continue;
+      if (directive is PartDirective) {
+        parts.add(linkedId);
+        continue;
+      }
+
+      List<Configuration> conditionalDirectiveConfigurations;
+
+      if (directive is ImportDirective && directive.configurations.isNotEmpty) {
+        conditionalDirectiveConfigurations = directive.configurations;
+      } else if (directive is ExportDirective &&
+          directive.configurations.isNotEmpty) {
+        conditionalDirectiveConfigurations = directive.configurations;
+      }
+      deps.add(linkedId);
+      if (conditionalDirectiveConfigurations != null) {
+        deps.addAll(conditionalDirectiveConfigurations
+            .map((c) => Uri.parse(c.uri.stringValue))
+            .where((u) => u.scheme != 'dart')
+            .map((u) => new AssetId.resolve(u.toString(), from: id)));
+      }
+    }
+    return new ModuleLibrary._(id,
+        isEntryPoint: isEntryPoint, deps: deps, parts: parts);
+  }
+
+  /// Parse the directives from [source] and compute the library information.
+  static ModuleLibrary fromSource(AssetId id, String source) {
+    final isLibDir = id.path.startsWith('lib/');
+    final parsed = isLibDir
+        ? parseDirectives(source, name: id.path, suppressErrors: true)
+        : parseCompilationUnit(source,
+            name: id.path, suppressErrors: true, parseFunctionBodies: false);
+    // Packages within the SDK but published might have libraries that can't be
+    // used outside the SDK.
+    if (parsed.directives.any((d) =>
+        d is UriBasedDirective &&
+        d.uri.stringValue.startsWith('dart:_') &&
+        id.package != 'dart_internal')) {
+      return new ModuleLibrary._nonImportable(id);
+    }
+    if (_isPart(parsed)) {
+      return new ModuleLibrary._nonImportable(id);
+    }
+
+    final isEntryPoint =
+        (isLibDir && !id.path.startsWith('lib/src/')) || _hasMainMethod(parsed);
+    return new ModuleLibrary._fromCompilationUnit(id, isEntryPoint, parsed);
+  }
+}
+
+bool _isPart(CompilationUnit dart) =>
+    dart.directives.any((directive) => directive is PartOfDirective);
+
+bool _hasMainMethod(CompilationUnit dart) => dart.declarations.any((node) =>
+    node is FunctionDeclaration &&
+    node.name.name == 'main' &&
+    node.functionExpression.parameters.parameters.length <= 2);
