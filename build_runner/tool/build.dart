54f0916546a2f5b94c055273dedbd09b175e0853
Jacob MacDonald <jakemac@google.com>
2018-08-08 14:33:02 -0700

    add a custom tool/build.dart script for generating the graph viz js file

---
+++ build_runner/tool/build.dart
@@ -0,0 +1,34 @@
+import 'package:build/build.dart';
+import 'package:build_config/build_config.dart';
+import 'package:build_modules/builders.dart';
+import 'package:build_runner/build_runner.dart';
+import 'package:build_web_compilers/builders.dart';
+
+main(List<String> args) async {
+  await run(args, [
+    apply(
+        'build_modules|modules',
+        [
+          moduleLibraryBuilder,
+          metaModuleBuilder,
+          metaModuleCleanBuilder,
+          moduleBuilder,
+        ],
+        toAllPackages(),
+        isOptional: true,
+        hideOutput: true),
+    apply(
+        'build_web_compilers|entrypoint',
+        [
+          (_) => webEntrypointBuilder(BuilderOptions({
+                'compiler': 'dart2js',
+                'dart2js_args': ['--minify', '--omit-implicit-checks']
+              }))
+        ],
+        toAllPackages(),
+        isOptional: false,
+        hideOutput: false,
+        defaultGenerateFor:
+            InputSet(include: ['lib/src/server/graph_viz_main.dart'])),
+  ]);
+}
