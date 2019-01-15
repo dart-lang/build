a9107b1f6098975bf00c28936b150b6f91d26c04
Jacob MacDonald <jakemac@google.com>
2019-01-15 14:25:03 -0800

    add overrideGeneratedOutputDirectory and lockGeneratedOutputDirectory

--- build_runner_core/lib/src/generate/build_runner.dart
+++ build_runner_core/lib/src/generate/build_runner.dart
@@ -9,6 +9,7 @@ import 'package:watcher/watcher.dart';
 
 import '../environment/build_environment.dart';
 import '../package_graph/apply_builders.dart';
+import '../util/constants.dart';
 import 'build_impl.dart';
 import 'build_result.dart';
 import 'options.dart';
@@ -24,12 +25,17 @@ class BuildRunner {
       _build.run(updates, buildDirs: buildDirs);
 
   static Future<BuildRunner> create(
-          BuildOptions options,
-          BuildEnvironment environment,
-          List<BuilderApplication> builders,
-          Map<String, Map<String, dynamic>> builderConfigOverrides,
-          {bool isReleaseBuild = false}) async =>
-      BuildRunner._(await BuildImpl.create(
-          options, environment, builders, builderConfigOverrides,
-          isReleaseBuild: isReleaseBuild));
+      BuildOptions options,
+      BuildEnvironment environment,
+      List<BuilderApplication> builders,
+      Map<String, Map<String, dynamic>> builderConfigOverrides,
+      {bool isReleaseBuild = false}) async {
+    // Don't allow any changes to the generated asset directory after this
+    // point.
+    lockGeneratedOutputDirectory();
+
+    return BuildRunner._(await BuildImpl.create(
+        options, environment, builders, builderConfigOverrides,
+        isReleaseBuild: isReleaseBuild));
+  }
 }
