cad9fadca8d689c767c630fa64fdba66066c9324
Gary Roumanis <grouma@google.com>
2019-03-21 16:55:20 -0700

    hashcode and ==

--- build_runner_core/lib/src/generate/build_target.dart
+++ build_runner_core/lib/src/generate/build_target.dart
@@ -6,6 +6,20 @@ class BuildTarget {
   final String directory;
   final OutputLocation outputLocation;
   BuildTarget(this.directory, this.outputLocation);
+
+  @override
+  operator ==(other) =>
+      other is BuildTarget &&
+      other.directory == directory &&
+      other.outputLocation == outputLocation;
+
+  @override
+  int get hashCode {
+    var hash = 0;
+    hash = _hashCombine(hash, directory.hashCode);
+    hash = _hashCombine(hash, outputLocation.hashCode);
+    return _hashComplete(hash);
+  }
 }
 
 class OutputLocation {
@@ -19,4 +33,32 @@ class OutputLocation {
       throw ArgumentError('Can not build everything and hoist');
     }
   }
+
+  @override
+  operator ==(other) =>
+      other is OutputLocation &&
+      other.output == output &&
+      other.useSymlinks == useSymlinks &&
+      other.hoist == hoist;
+
+  @override
+  int get hashCode {
+    var hash = 0;
+    hash = _hashCombine(hash, output.hashCode);
+    hash = _hashCombine(hash, useSymlinks.hashCode);
+    hash = _hashCombine(hash, hoist.hashCode);
+    return _hashComplete(hash);
+  }
+}
+
+int _hashCombine(int hash, int value) {
+  hash = 0x1fffffff & (hash + value);
+  hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+  return hash ^ (hash >> 6);
+}
+
+int _hashComplete(int hash) {
+  hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+  hash = hash ^ (hash >> 11);
+  return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
 }
