40d4409c32519a425474499d288f2b7605b8d875
Nate Bosch <nbosch@google.com>
2019-04-02 10:00:43 -0700

    Add docs. Add a  static method

--- build_modules/lib/src/module_cache.dart
+++ build_modules/lib/src/module_cache.dart
@@ -10,23 +10,38 @@ import 'package:build/build.dart';
 import 'meta_module.dart';
 import 'modules.dart';
 
-final metaModuleCache = Resource<DecodingCache<MetaModule>>(
-    () => DecodingCache((m) => MetaModule.fromJson(m)),
-    dispose: (c) => c.dispose());
+final metaModuleCache = DecodingCache.resource((m) => MetaModule.fromJson(m));
 
-final moduleCache = Resource<DecodingCache<Module>>(
-    () => DecodingCache((m) => Module.fromJson(m)),
-    dispose: (c) => c.dispose());
+final moduleCache = DecodingCache.resource((m) => Module.fromJson(m));
 
+/// A cache of objects decoded from written assets suitable for use as a
+/// [Resource].
+///
+/// Instances that are decoded will be cached throughout the duration of a build
+/// and invalidated between builds. Instances that are shared through the cache
+/// should be treated as immutable to avoid leaking any information which was
+/// not loaded from the underlying asset.
 class DecodingCache<T> {
+  /// Create a [Resource] which can decoded instances of [T] serialized via json
+  /// to assets.
+  static Resource<DecodingCache<T>> resource<T>(
+          T Function(Map<String, dynamic>) _fromJson) =>
+      Resource<DecodingCache<T>>(() => DecodingCache._(_fromJson),
+          dispose: (c) => c._dispose());
+
   final _cached = <AssetId, Future<Result<T>>>{};
 
-  final T Function(Map<String, dynamic>) _construct;
+  final T Function(Map<String, dynamic>) _fromJson;
 
-  DecodingCache(this._construct);
+  DecodingCache._(this._fromJson);
 
-  void dispose() => _cached.clear();
+  void _dispose() => _cached.clear();
 
+  /// Find and deserialize a [T] stored in [id].
+  ///
+  /// If the asset at [id] is unreadable the returned future will resolve to
+  /// null. If the isntance is cached it will not be decoded again, but the
+  /// content dependencies will be tracked through [reader].
   Future<T> find(AssetId id, AssetReader reader) async {
     if (!await reader.canRead(id)) return null;
     var result = _cached.putIfAbsent(
@@ -34,7 +49,7 @@ class DecodingCache<T> {
         () => Result.capture(reader
             .readAsString(id)
             .then((c) => jsonDecode(c) as Map<String, dynamic>)
-            .then(_construct)));
+            .then(_fromJson)));
     return Result.release(result);
   }
 }
