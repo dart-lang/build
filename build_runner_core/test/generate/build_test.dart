f2181a237c813cdea6ae5c16f4f3df395a17670d
Nate Bosch <nbosch@google.com>
2019-04-03 09:25:50 -0700

    Add regression test

--- build_runner_core/test/generate/build_test.dart
+++ build_runner_core/test/generate/build_test.dart
@@ -1296,6 +1296,25 @@ void main() {
         expect(node.isFailure, isTrue);
       }
     });
+
+    test('a glob should not be an output of an anchor node', () async {
+      // https://github.com/dart-lang/build/issues/2017
+      var builders = [
+        apply(
+            'test_builder',
+            [
+              (_) => TestBuilder(build: (buildStep, _) {
+                    buildStep.findAssets(Glob('**'));
+                  })
+            ],
+            toRoot(),
+            appliesBuilders: ['a|copy_builder']),
+        applyPostProcess('a|copy_builder', (_) => CopyingPostProcessBuilder())
+      ];
+      var writer = InMemoryRunnerAssetWriter();
+      // A build does not crash in `_cleanUpStaleOutputs`
+      await testBuilders(builders, {'a|lib/a.txt': 'a'}, writer: writer);
+    });
   });
 }
 
