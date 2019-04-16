4508cda22d9dbb0139bc9a9e6836dd2c4e01e70c
Nate Bosch <nbosch@google.com>
2019-04-16 09:52:08 -0700

    Sort imports

--- build_runner_core/lib/src/util/constants.dart
+++ build_runner_core/lib/src/util/constants.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:crypto/crypto.dart';
 import 'package:path/path.dart' as p;
