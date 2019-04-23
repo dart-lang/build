5d9f560077a7f24fe4a9f398781ae916424cf28c
Kevin Moore <kevmoo@google.com>
2019-04-23 16:54:36 -0700

    use latest mono_repo

--- source_gen/source_gen/tool/travis.sh
+++ source_gen/source_gen/tool/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with package:mono_repo v2.0.0
+# Created with package:mono_repo v2.1.0
 
 if [[ -z ${PKGS} ]]; then
   echo -e '\033[31mPKGS environment variable must be set!\033[0m'
@@ -19,33 +19,31 @@ for PKG in ${PKGS}; do
   pub upgrade --no-precompile || exit $?
 
   for TASK in "$@"; do
+    echo
+    echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
     case ${TASK} in
-    command) echo
-      echo -e '\033[1mTASK: command\033[22m'
-      echo -e 'pub run build_runner test -- -j 1'
+    command)
+      echo 'pub run build_runner test -- -j 1'
       pub run build_runner test -- -j 1 || EXIT_CODE=$?
       ;;
-    dartanalyzer_0) echo
-      echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
-      echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+    dartanalyzer_0)
+      echo 'dartanalyzer --fatal-infos --fatal-warnings .'
       dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
       ;;
-    dartanalyzer_1) echo
-      echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
-      echo -e 'dartanalyzer --fatal-warnings .'
+    dartanalyzer_1)
+      echo 'dartanalyzer --fatal-warnings .'
       dartanalyzer --fatal-warnings . || EXIT_CODE=$?
       ;;
-    dartfmt) echo
-      echo -e '\033[1mTASK: dartfmt\033[22m'
-      echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt)
+      echo 'dartfmt -n --set-exit-if-changed .'
       dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
       ;;
-    test) echo
-      echo -e '\033[1mTASK: test\033[22m'
-      echo -e 'pub run test --run-skipped'
+    test)
+      echo 'pub run test --run-skipped'
       pub run test --run-skipped || EXIT_CODE=$?
       ;;
-    *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    *)
+      echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
       EXIT_CODE=1
       ;;
     esac
