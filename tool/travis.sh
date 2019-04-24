ec8b0f18f096be67b76fed98727995c292cf9ea0
Nate Bosch <nbosch1@gmail.com>
2019-04-24 10:05:23 -0700

    Drop dependency overrides from build_config (#2217)
    Everything has been published such that we can get a pub solve without
    overrides.
    
    Migrate to `pub run test` since we can't solve a version of `build_modules`
    that works on an older SDK and also allows this version of `build_config`.

--- tool/travis.sh
+++ tool/travis.sh
@@ -34,11 +34,7 @@ for PKG in ${PKGS}; do
       echo 'pub run build_runner test'
       pub run build_runner test || EXIT_CODE=$?
       ;;
-    command_3)
-      echo 'dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs'
-      dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs || EXIT_CODE=$?
-      ;;
-    command_5)
+    command_4)
       echo 'dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs-- -P presubmit'
       dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs-- -P presubmit || EXIT_CODE=$?
       ;;
