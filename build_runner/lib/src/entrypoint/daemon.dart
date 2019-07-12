8e144301cde92c8a2ef9c758225185ed78a6756c
Gary Roumanis <grouma@google.com>
2019-07-12 13:05:47 -0700

    Expose assetServerPort

--- build_runner/lib/src/entrypoint/daemon.dart
+++ build_runner/lib/src/entrypoint/daemon.dart
@@ -19,6 +19,15 @@ import '../daemon/daemon_builder.dart';
 import 'base_command.dart';
 import 'options.dart';
 
+/// Returns the port of the daemon asset server.
+int assetServerPort(String workingDirectory) {
+  var portFile = File(assetServerPortFilePath(workingDirectory));
+  if (!portFile.existsSync()) {
+    throw Exception('Unable to read daemon asset port file.');
+  }
+  return int.parse(portFile.readAsStringSync());
+}
+
 /// A command that starts the Build Daemon.
 class DaemonCommand extends BuildRunnerCommand {
   @override
