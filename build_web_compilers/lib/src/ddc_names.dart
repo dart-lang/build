262624775d1579d1fc7c12ec2131f06ec0e52ca2
MarkZ <mzhou620@gmail.com>
2019-07-23 17:45:51 -0700

    Renaming _p -> p

--- build_web_compilers/lib/src/ddc_names.dart
+++ build_web_compilers/lib/src/ddc_names.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:path/path.dart' as _p;
+import 'package:path/path.dart' as p;
 
 /// Transforms a path to a valid JS identifier.
 ///
