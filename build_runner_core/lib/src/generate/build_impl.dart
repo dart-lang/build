4279a676a2b6450c7741086b600e6ba1a8ff3122
Jacob MacDonald <jakemac@google.com>
2019-08-07 13:17:03 -0700

    create windows compatible directory names when doing performance logs (#2397)
    - create windows compatible directory names when doing performance logs
    - update pubspec/changelog for release

--- build_runner_core/lib/src/generate/build_impl.dart
+++ build_runner_core/lib/src/generate/build_impl.dart
@@ -277,8 +277,12 @@ class _SingleBuild {
       // Log performance information if requested
       if (_logPerformanceDir != null) {
         assert(result.performance != null);
-        var logPath =
-            p.join(_logPerformanceDir, DateTime.now().toIso8601String());
+        var now = DateTime.now();
+        var logPath = p.join(
+            _logPerformanceDir,
+            '${now.year}-${_twoDigits(now.month)}-${_twoDigits(now.day)}'
+            '_${_twoDigits(now.hour)}-${_twoDigits(now.minute)}-'
+            '${_twoDigits(now.second)}');
         await logTimedAsync(_logger, 'Writing performance log to $logPath', () {
           var performanceLogId = AssetId(_packageGraph.root.name, logPath);
           var serialized = jsonEncode(result.performance);
@@ -855,3 +859,5 @@ String _actionLoggerName(
       : primaryInput.uri;
   return '${phase.builderLabel} on $asset';
 }
+
+String _twoDigits(int n) => '$n'.padLeft(2, '0');
