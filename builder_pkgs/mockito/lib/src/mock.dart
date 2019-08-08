be84f3684028a974913540e4dc0380c6517cd2ae
Sam Rawlins <srawlins@google.com>
2019-08-08 13:28:58 -0700

    New docs for verifyInOrder (dart-lang/mockito#208)
    Add docs for verifyInOrder

--- builder_pkgs/mockito/lib/src/mock.dart
+++ builder_pkgs/mockito/lib/src/mock.dart
@@ -917,6 +917,20 @@ Verification _makeVerify(bool never) {
   };
 }
 
+/// Verify that a list of methods on a mock object have been called with the
+/// given arguments. For example:
+///
+/// ```dart
+/// verifyInOrder([cat.eatFood("Milk"), cat.sound(), cat.eatFood(any)]);
+/// ```
+///
+/// This verifies that `eatFood` was called with `"Milk"`, sound` was called
+/// with no arguments, and `eatFood` was then called with some argument.
+///
+/// Note: [verifyInOrder] only verifies that each call was made in the order
+/// given, but not that those were the only calls. In the example above, if
+/// other calls were made to `eatFood` or `sound` between the three given
+/// calls, or before or after them, the verification will still succeed.
 _InOrderVerification get verifyInOrder {
   if (_verifyCalls.isNotEmpty) {
     throw StateError(_verifyCalls.join());
