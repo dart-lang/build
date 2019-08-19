16106ce3bff2bcb8cf0025c6ad03ac7740e442e3
Kevin Moore <kevmoo@users.noreply.github.com>
2019-08-19 10:48:31 -0700

    Ignore deprecated API usage in pkg:analyzer (dart-lang/source_gen#426)

--- source_gen/source_gen/example/lib/src/utils.dart
+++ source_gen/source_gen/example/lib/src/utils.dart
@@ -8,6 +8,9 @@ import 'package:source_gen/source_gen.dart';
 /// Returns all [TopLevelVariableElement] members in [reader]'s library that
 /// have a type of [num].
 Iterable<TopLevelVariableElement> topLevelNumVariables(LibraryReader reader) =>
-    reader.allElements.whereType<TopLevelVariableElement>().where((element) =>
-        element.type
+    reader.allElements
+        .whereType<TopLevelVariableElement>()
+        .where((element) => element.type
+            // TODO: migrate to supported API when min pkg:analyzer is bumped
+            // ignore: deprecated_member_use
             .isAssignableTo(reader.element.context.typeProvider.numType));
