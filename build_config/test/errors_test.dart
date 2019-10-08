01b03d01c69edafd242ee04cc4644eed1e774da3
Jacob MacDonald <jakemac@google.com>
2019-10-08 13:21:00 -0700

    add windows tests where they pass, fix some bugs (#2488)
    Will remove appveyor config later - have to get the webhook deleted first
    
    This adds windows bots for all our unit and integration tests with a few skips here and there.
    
    The major exception are tests in the build_runner package which were having a lot of problems, and all the meaningful build_daemon tests. Both were primarily caused by issues with cleaning up temp directories after the test but the build daemon is still running in that directory.
    
    I also had to do a few minor fixes in the scratch_space and build_test packages so this releases new versions of each of those.

--- build_config/test/errors_test.dart
+++ build_config/test/errors_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@OnPlatform({'windows': Skip('yaml errors look different on windows')})
 import 'package:build_config/build_config.dart';
 import 'package:test/test.dart';
 
