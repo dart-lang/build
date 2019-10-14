270f401b9ac81b7c5f972750808f2c2f322462cb
Nate Bosch <nbosch@google.com>
2019-10-14 12:04:22 -0700

    Update test for help output
    Check for specific output from subcommand help

--- _test/test/help_test.dart
+++ _test/test/help_test.dart
@@ -4,7 +4,6 @@
 
 @TestOn('vm')
 import 'dart:async';
-import 'dart:io';
 
 import 'package:test/test.dart';
 
@@ -12,32 +11,43 @@ import 'common/utils.dart';
 
 void main() {
   test('pub run build_runner help', () async {
-    await _testHelpCommand(() => runCommand(['help']));
-  });
-
-  test('pub run build_runner --help', () async {
-    await _testHelpCommand(() => runCommand(['--help']));
+    await _testHelpCommand(['help'],
+        checkContent: 'Unified interface for running Dart builds.');
+    await _testHelpCommand(['--help'],
+        checkContent: 'Unified interface for running Dart builds.');
   });
 
   test('pub run build_runner build --help', () async {
-    await _testHelpCommand(() => runCommand(['build', '--help']));
+    await _testHelpCommand(['build', '--help'],
+        checkContent: 'Performs a single build on the specified targets');
+    await _testHelpCommand(['help', 'build'],
+        checkContent: 'Performs a single build on the specified targets');
   });
 
   test('pub run build_runner serve --help', () async {
-    await _testHelpCommand(() => runCommand(['serve', '--help']));
+    await _testHelpCommand(['serve', '--help'],
+        checkContent: 'Runs a development server');
+    await _testHelpCommand(['help', 'serve'],
+        checkContent: 'Runs a development server');
   });
 
   test('pub run build_runner test --help', () async {
-    await _testHelpCommand(() => runCommand(['test', '--help']));
+    await _testHelpCommand(['test', '--help'],
+        checkContent: 'and then runs tests using');
+    await _testHelpCommand(['help', 'test'],
+        checkContent: 'and then runs tests using');
   });
 
   test('pub run build_runner watch --help', () async {
-    await _testHelpCommand(() => runCommand(['watch', '--help']));
+    await _testHelpCommand(['watch', '--help'],
+        checkContent: 'watching the file system for updates');
+    await _testHelpCommand(['help', 'watch'],
+        checkContent: 'watching the file system for updates');
   });
 }
 
-Future<void> _testHelpCommand(Future<ProcessResult> runCommand()) async {
-  var asyncResult = runCommand();
+Future<void> _testHelpCommand(List<String> args, {String checkContent}) async {
+  var asyncResult = runCommand(args);
   expect(asyncResult, completes,
       reason: 'should not cause the auto build script to hang');
   var result = await asyncResult;
@@ -46,4 +56,7 @@ Future<void> _testHelpCommand(Future<ProcessResult> runCommand()) async {
   expect(result.stderr, isEmpty, reason: 'Should output nothing on stderr');
   expect(result.stdout, isNot(contains('"Unhandled exception"')),
       reason: 'Should not print an unhandled exception');
+  if (checkContent != null) {
+    expect(result.stdout, contains(checkContent));
+  }
 }
