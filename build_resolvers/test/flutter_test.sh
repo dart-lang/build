c6f58c16da85946a9cfd59a535724634f99584e0
Jacob MacDonald <jakemac@google.com>
2019-10-15 09:24:00 -0700

    fix up flutter_test.sh more

--- build_resolvers/test/flutter_test.sh
+++ build_resolvers/test/flutter_test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Fast fail the script on failures.
-set -ex
+set -e
 
 # Get Flutter.
 echo "Cloning master Flutter branch"
 git clone https://github.com/flutter/flutter.git ./flutter
 
-./flutter/bin/cache/dart-sdk/bin/dart test/resolver_test
+./flutter/bin/flutter config --no-analytics
+
+./flutter/bin/cache/dart-sdk/bin/dart test/resolver_test.dart
+
+rm -rf ./flutter
