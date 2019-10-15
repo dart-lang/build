94165657425f3cc8005dea0e251231627e2982d0
Jacob MacDonald <jakemac@google.com>
2019-10-15 10:01:00 -0700

    faster precache

--- build_resolvers/test/flutter_test.sh
+++ build_resolvers/test/flutter_test.sh
@@ -8,9 +8,9 @@ echo "Cloning master Flutter branch"
 git clone https://github.com/flutter/flutter.git ./flutter
 
 if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-  ./flutter/bin/flutter.bat precache
+  ./flutter/bin/flutter.bat precache --no-ios --no-android
 else
-  ./flutter/bin/flutter precache
+  ./flutter/bin/flutter precache --no-ios --no-android
 fi
 
 ./flutter/bin/cache/dart-sdk/bin/dart test/resolver_test.dart
