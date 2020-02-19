61a22ccf8297ec84035fe171e4d37f5fa9cb5b91
Nate Bosch <nbosch@google.com>
2020-02-19 14:13:10 -0800

    Randomize all test orders on travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -55,22 +55,26 @@ for PKG in ${PKGS}; do
     echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
     case ${TASK} in
     command_0)
-      echo 'pub run build_runner test -- -p chrome'
-      pub run build_runner test -- -p chrome || EXIT_CODE=$?
+      echo 'pub run build_runner test -- -p chrome --test-randomize-ordering-seed=random'
+      pub run build_runner test -- -p chrome --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     command_1)
-      echo 'pub run build_runner test -- -p vm test/configurable_uri_test.dart'
-      pub run build_runner test -- -p vm test/configurable_uri_test.dart || EXIT_CODE=$?
+      echo 'pub run build_runner test -- -p vm test/configurable_uri_test.dart --test-randomize-ordering-seed=random'
+      pub run build_runner test -- -p vm test/configurable_uri_test.dart --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     command_2)
-      echo 'pub run build_runner test'
-      pub run build_runner test || EXIT_CODE=$?
+      echo 'pub run build_runner test -- --test-randomize-ordering-seed=random'
+      pub run build_runner test -- --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     command_4)
-      echo 'dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs -- -P presubmit'
-      dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs -- -P presubmit || EXIT_CODE=$?
+      echo 'dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs -- -P presubmit --test-randomize-ordering-seed=random'
+      dart $(pub run build_runner generate-build-script) test --delete-conflicting-outputs -- -P presubmit --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     command_5)
+      echo 'pub run build_runner test -- -test-randomize-ordering-seed=random'
+      pub run build_runner test -- -test-randomize-ordering-seed=random || EXIT_CODE=$?
+      ;;
+    command_6)
       echo 'test/flutter_test.sh'
       test/flutter_test.sh || EXIT_CODE=$?
       ;;
@@ -87,52 +91,56 @@ for PKG in ${PKGS}; do
       dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
       ;;
     test_00)
-      echo 'pub run test --total-shards 2 --shard-index 0'
-      pub run test --total-shards 2 --shard-index 0 || EXIT_CODE=$?
+      echo 'pub run test --total-shards 2 --shard-index 0 --test-randomize-ordering-seed=random'
+      pub run test --total-shards 2 --shard-index 0 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_01)
-      echo 'pub run test --total-shards 2 --shard-index 1'
-      pub run test --total-shards 2 --shard-index 1 || EXIT_CODE=$?
+      echo 'pub run test --total-shards 2 --shard-index 1 --test-randomize-ordering-seed=random'
+      pub run test --total-shards 2 --shard-index 1 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_02)
-      echo 'pub run test --total-shards 3 --shard-index 0'
-      pub run test --total-shards 3 --shard-index 0 || EXIT_CODE=$?
+      echo 'pub run test --total-shards 3 --shard-index 0 --test-randomize-ordering-seed=random'
+      pub run test --total-shards 3 --shard-index 0 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_03)
-      echo 'pub run test --total-shards 3 --shard-index 1'
-      pub run test --total-shards 3 --shard-index 1 || EXIT_CODE=$?
+      echo 'pub run test --total-shards 3 --shard-index 1 --test-randomize-ordering-seed=random'
+      pub run test --total-shards 3 --shard-index 1 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_04)
-      echo 'pub run test --total-shards 3 --shard-index 2'
-      pub run test --total-shards 3 --shard-index 2 || EXIT_CODE=$?
+      echo 'pub run test --total-shards 3 --shard-index 2 --test-randomize-ordering-seed=random'
+      pub run test --total-shards 3 --shard-index 2 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_05)
       echo 'pub run test'
       pub run test || EXIT_CODE=$?
       ;;
     test_06)
-      echo 'pub run test -x integration'
-      pub run test -x integration || EXIT_CODE=$?
+      echo 'pub run test -x integration --test-randomize-ordering-seed=random'
+      pub run test -x integration --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_07)
-      echo 'pub run test -t integration --total-shards 5 --shard-index 0'
-      pub run test -t integration --total-shards 5 --shard-index 0 || EXIT_CODE=$?
+      echo 'pub run test -t integration --total-shards 5 --shard-index 0 --test-randomize-ordering-seed=random'
+      pub run test -t integration --total-shards 5 --shard-index 0 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_08)
-      echo 'pub run test -t integration --total-shards 5 --shard-index 1'
-      pub run test -t integration --total-shards 5 --shard-index 1 || EXIT_CODE=$?
+      echo 'pub run test -t integration --total-shards 5 --shard-index 1 --test-randomize-ordering-seed=random'
+      pub run test -t integration --total-shards 5 --shard-index 1 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_09)
-      echo 'pub run test -t integration --total-shards 5 --shard-index 2'
-      pub run test -t integration --total-shards 5 --shard-index 2 || EXIT_CODE=$?
+      echo 'pub run test -t integration --total-shards 5 --shard-index 2 --test-randomize-ordering-seed=random'
+      pub run test -t integration --total-shards 5 --shard-index 2 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_10)
-      echo 'pub run test -t integration --total-shards 5 --shard-index 3'
-      pub run test -t integration --total-shards 5 --shard-index 3 || EXIT_CODE=$?
+      echo 'pub run test -t integration --total-shards 5 --shard-index 3 --test-randomize-ordering-seed=random'
+      pub run test -t integration --total-shards 5 --shard-index 3 --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     test_11)
-      echo 'pub run test -t integration --total-shards 5 --shard-index 4'
-      pub run test -t integration --total-shards 5 --shard-index 4 || EXIT_CODE=$?
+      echo 'pub run test -t integration --total-shards 5 --shard-index 4 --test-randomize-ordering-seed=random'
+      pub run test -t integration --total-shards 5 --shard-index 4 --test-randomize-ordering-seed=random || EXIT_CODE=$?
+      ;;
+    test_12)
+      echo 'pub run test --test-randomize-ordering-seed=random'
+      pub run test --test-randomize-ordering-seed=random || EXIT_CODE=$?
       ;;
     *)
       echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
