8d4110ef3612222ea99380ac91beb4bafc8e96d9
Kevin Moore <kevmoo@users.noreply.github.com>
2020-09-17 11:26:15 -0700

    Small cleanup to failure reporting script (#2816)
    No need to construct the build URL – travis gives it to us!

--- tool/report_failure.sh
+++ tool/report_failure.sh
@@ -1,6 +1,6 @@
-TRAVIS_BUILDS_URI="https://travis-ci.org/dart-lang/build/builds"
-if [ "$TRAVIS_EVENT_TYPE" != "pull_request" -a "$TRAVIS_ALLOW_FAILURE" != "true" ]; then
+#!/bin/bash
+if [ "$TRAVIS_EVENT_TYPE" != "pull_request" ] && [ "$TRAVIS_ALLOW_FAILURE" != "true" ]; then
   curl -H "Content-Type: application/json" -X POST -d \
-    "{'text':'Build failed! ${TRAVIS_BUILDS_URI}/${TRAVIS_BUILD_ID}'}" \
+    "{'text':'Build failed! ${TRAVIS_BUILD_WEB_URL}'}" \
     "${CHAT_HOOK_URI}"
 fi
