bd21f1af55022fdfed7434c1df49d782fbf43bc5
Kevin Moore <kevmoo@google.com>
2020-10-21 19:33:29 -0700

    silly

--- tool/mono_repo_self_validate.sh
+++ tool/mono_repo_self_validate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Created with package:mono_repo v2.5.0
+# Created with package:mono_repo v3.0.0-dev
 
 # Support built in commands on windows out of the box.
-function pub {
-       if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-        command pub.bat "$@"
-    else
-        command pub "$@"
-    fi
+function pub() {
+  if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+    command pub.bat "$@"
+  else
+    command pub "$@"
+  fi
 }
 
 set -v -e
-pub global activate mono_repo 2.5.0
+pub global activate mono_repo 3.0.0-dev
 pub global run mono_repo travis --validate
