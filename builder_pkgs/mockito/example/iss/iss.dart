2fb19c90ef03c8024453f1d37257d3eacc419985
srawlins <srawlins@google.com>
2020-12-21 18:09:34 -0500

    Do not compile example or tests with DDC using null safety yet.
    The example app, `example/iss`, uses package:http, which is not migrated yet.
    
    This prevents a crash in DDC, as in this travis task: https://travis-ci.org/github/dart-lang/mockito/jobs/750256243
    
    PiperOrigin-RevId: 348534584

--- builder_pkgs/mockito/example/iss/iss.dart
+++ builder_pkgs/mockito/example/iss/iss.dart
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// @dart=2.9
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
@@ -22,8 +24,8 @@ import 'package:http/http.dart';
 class IssLocator {
   final Client client;
 
-  late Point<double> _position;
-  Future<void>? _ongoingRequest;
+  /*late*/ Point<double> _position;
+  Future<void> /*?*/ _ongoingRequest;
 
   IssLocator(this.client);
 
