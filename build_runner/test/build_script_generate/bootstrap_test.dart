00637c9e413ba1c59b0175a9a68b7590084b08b4
Simon Binder <oss@simonbinder.eu>
2021-01-13 23:13:25 +0100

    Run bootstrap test in sandbox folder

--- build_runner/test/build_script_generate/bootstrap_test.dart
+++ build_runner/test/build_script_generate/bootstrap_test.dart
@@ -1,25 +1,35 @@
 @Timeout.factor(2)
-import 'package:_test_common/package_graphs.dart';
+import 'dart:io';
+
 import 'package:build_runner/build_script_generate.dart';
 import 'package:build_runner_core/build_runner_core.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:test/test.dart';
+
+import '../integration_tests/utils/build_descriptor.dart';
 
 void main() {
   test('invokes custom error function', () async {
     Object error;
     StackTrace stackTrace;
 
-    await expectLater(
-      generateAndRun(
-        [],
-        generationOptions: _FailingGenerationOptions(),
-        handleUncaughtError: (err, trace) {
-          error = err;
-          stackTrace = trace;
-        },
-      ),
-      completion(1),
+    final pkgDir = (await package([])).rootPackageDir;
+
+    await IOOverrides.runZoned(
+      () {
+        return expectLater(
+          generateAndRun(
+            [],
+            generationOptions: _FailingGenerationOptions(pkgDir),
+            handleUncaughtError: (err, trace) {
+              error = err;
+              stackTrace = trace;
+            },
+          ),
+          completion(1),
+        );
+      },
+      getCurrentDirectory: () => Directory(pkgDir),
     );
 
     expect(error, 'expected error');
@@ -28,11 +38,13 @@ void main() {
 }
 
 class _FailingGenerationOptions extends BuildScriptGenerationOptions {
+  final String pkgDir;
+
+  _FailingGenerationOptions(this.pkgDir);
+
   @override
   Future<PackageGraph> packageGraph() {
-    return Future.value(buildPackageGraph({
-      rootPackage('root'): [],
-    }));
+    return PackageGraph.forPath(pkgDir);
   }
 
   @override
