9e5b8a1c36af8e866f5cb6a34f3aac76e8aa2e88
Simon Binder <oss@simonbinder.eu>
2021-07-31 18:56:10 +0200

    Format, again

--- build_runner_core/test/generate/build_test.dart
+++ build_runner_core/test/generate/build_test.dart
@@ -1047,7 +1047,9 @@ void main() {
         'a|web/a.txt', [], computeDigest(AssetId('a', 'web/a.txt'), 'a'));
     var bSourceNode = makeAssetNode(
         'a|lib/b.txt', [], computeDigest(AssetId('a', 'lib/b.txt'), 'b'));
-    expectedGraph..add(aSourceNode)..add(bSourceNode);
+    expectedGraph
+      ..add(aSourceNode)
+      ..add(bSourceNode);
 
     // Regular generated asset nodes and supporting nodes.
     var builderOptionsId = makeAssetId('a|Phase0.builderOptions');
@@ -1097,7 +1099,9 @@ void main() {
         aSourceNode.id, 0, postBuilderOptionsId);
     var bAnchorNode = PostProcessAnchorNode.forInputAndAction(
         bSourceNode.id, 0, postBuilderOptionsId);
-    expectedGraph..add(aAnchorNode)..add(bAnchorNode);
+    expectedGraph
+      ..add(aAnchorNode)
+      ..add(bAnchorNode);
 
     var aPostCopyNode = GeneratedAssetNode(makeAssetId('a|web/a.txt.post'),
         phaseNumber: 1,
