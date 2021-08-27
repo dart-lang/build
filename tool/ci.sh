2e926524f2d77c504306d00812b3dbeaf573d7ce
Jake Macdonald <jakemac@google.com>
2021-08-27 13:53:02 -0700

    re-run mono_repo

--- tool/ci.sh
+++ tool/ci.sh
@@ -76,20 +76,20 @@ for PKG in ${PKGS}; do
         dart analyze --fatal-infos || EXIT_CODE=$?
         ;;
       command_0)
-        echo 'pub run build_runner test -- -p chrome --test-randomize-ordering-seed=random'
-        pub run build_runner test -- -p chrome --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        echo 'dart run build_runner test -- -p chrome --test-randomize-ordering-seed=random'
+        dart run build_runner test -- -p chrome --test-randomize-ordering-seed=random || EXIT_CODE=$?
         ;;
       command_1)
-        echo 'pub run build_runner test -- -p vm test/configurable_uri_test.dart --test-randomize-ordering-seed=random'
-        pub run build_runner test -- -p vm test/configurable_uri_test.dart --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        echo 'dart run build_runner test -- -p vm test/configurable_uri_test.dart --test-randomize-ordering-seed=random'
+        dart run build_runner test -- -p vm test/configurable_uri_test.dart --test-randomize-ordering-seed=random || EXIT_CODE=$?
         ;;
       command_2)
-        echo 'pub run build_runner test -- -p chrome,vm --test-randomize-ordering-seed=random'
-        pub run build_runner test -- -p chrome,vm --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        echo 'dart run build_runner test -- -p chrome,vm --test-randomize-ordering-seed=random'
+        dart run build_runner test -- -p chrome,vm --test-randomize-ordering-seed=random || EXIT_CODE=$?
         ;;
       command_3)
-        echo 'pub run build_runner test --define="build_web_compilers:entrypoint=compiler=dart2js" -- -p chrome --test-randomize-ordering-seed=random'
-        pub run build_runner test --define="build_web_compilers:entrypoint=compiler=dart2js" -- -p chrome --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        echo 'dart run build_runner test --define="build_web_compilers:entrypoint=compiler=dart2js" -- -p chrome --test-randomize-ordering-seed=random'
+        dart run build_runner test --define="build_web_compilers:entrypoint=compiler=dart2js" -- -p chrome --test-randomize-ordering-seed=random || EXIT_CODE=$?
         ;;
       format)
         echo 'dart format --output=none --set-exit-if-changed .'
