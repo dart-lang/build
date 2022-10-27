2ed32406aca6c97ad9dead98fd7c60111989566f
Siva <a-siva@users.noreply.github.com>
2022-10-27 11:50:23 -0700

    Fix test. (#3395)
    Remove @dart specification from daemon_test.dart in preparation for https://dart-review.googlesource.com/c/sdk/+/261140.
    This is based on the observation from @emmanuel-p

--- build_daemon/test/daemon_test.dart
+++ build_daemon/test/daemon_test.dart
@@ -167,7 +167,6 @@ Future<String> _statusOf(Process daemon) async {
 
 Future<Process> _runDaemon(var workspace, {int timeout = 30}) async {
   await d.file('test.dart', '''
-    // @dart=2.9
     import 'package:build_daemon/daemon.dart';
     import 'package:build_daemon/daemon_builder.dart';
     import 'package:build_daemon/client.dart';
