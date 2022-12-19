46decef42f6bf57bc7f95008840b191858e9f208
Simon Binder <oss@simonbinder.eu>
2022-12-19 22:39:38 +0100

    Fix paths when notifying fake watchers

--- _test_common/lib/in_memory_writer.dart
+++ _test_common/lib/in_memory_writer.dart
@@ -14,12 +14,15 @@ class InMemoryRunnerAssetWriter extends InMemoryAssetWriter
     implements RunnerAssetWriter {
   void Function(AssetId)? onDelete;
 
+  String _assetPath(AssetId id) {
+    return p.absolute(p.joinAll([id.package, ...id.pathSegments]));
+  }
+
   @override
   Future writeAsBytes(AssetId id, List<int> bytes) async {
     var type = assets.containsKey(id) ? ChangeType.MODIFY : ChangeType.ADD;
     await super.writeAsBytes(id, bytes);
-    FakeWatcher.notifyWatchers(
-        WatchEvent(type, p.absolute(id.package, id.path)));
+    FakeWatcher.notifyWatchers(WatchEvent(type, _assetPath(id)));
   }
 
   @override
@@ -27,16 +30,14 @@ class InMemoryRunnerAssetWriter extends InMemoryAssetWriter
       {Encoding encoding = utf8}) async {
     var type = assets.containsKey(id) ? ChangeType.MODIFY : ChangeType.ADD;
     await super.writeAsString(id, contents, encoding: encoding);
-    FakeWatcher.notifyWatchers(
-        WatchEvent(type, p.absolute(id.package, id.path)));
+    FakeWatcher.notifyWatchers(WatchEvent(type, _assetPath(id)));
   }
 
   @override
   Future delete(AssetId id) async {
     onDelete?.call(id);
     assets.remove(id);
-    FakeWatcher.notifyWatchers(
-        WatchEvent(ChangeType.REMOVE, p.absolute(id.package, id.path)));
+    FakeWatcher.notifyWatchers(WatchEvent(ChangeType.REMOVE, _assetPath(id)));
   }
 
   @override
