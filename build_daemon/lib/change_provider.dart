dart-lang/build

commit 0b879c424291f70b8212a888ef3f712209521c04

Report and shutdown after file watch errors (#3411)
* Report and shutdown after file watch errors

* change error codes

build_daemon/lib/constants.dart
@@ -12,6 +12,12 @@ const optionsSkew = 'DIFFERENT OPTIONS';
 
 const buildModeFlag = 'build-mode';
 
+/// Daemon shuts down after this timeout if there is no active connection.
+const defaultIdleTimeout = Duration(seconds: 30);
+
+const fileChangeEventErrorCode = 101;
+const fileChangeStreamClosedErrorCode = 102;
+
 enum BuildMode {
   // ignore: constant_identifier_names
   Manual,

build_daemon/lib/src/fakes/fake_change_provider.dart
@@ -1,16 +1,15 @@
 // Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
 import 'dart:async';
 
 import 'package:build_daemon/change_provider.dart';
 import 'package:watcher/watcher.dart' show WatchEvent;
 
 class FakeChangeProvider implements ChangeProvider {
+  final changeStreamController = StreamController<List<WatchEvent>>();
   @override
-  Stream<List<WatchEvent>> get changes => Stream.empty();
-
+  Stream<List<WatchEvent>> get changes => changeStreamController.stream;
   @override
   Future<List<WatchEvent>> collectChanges() async => [];
 }

build_daemon/lib/src/server.dart
@@ -1,7 +1,6 @@
 // Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -17,6 +16,7 @@ import 'package:watcher/watcher.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import '../change_provider.dart';
+import '../constants.dart';
 import '../daemon_builder.dart';
 import '../data/build_request.dart';
 import '../data/build_target.dart';
@@ -31,24 +31,19 @@ import 'managers/build_target_manager.dart';
 /// Note the server will only notify clients of pertinent events.
 class Server {
   static final loggerName = 'BuildDaemonServer';
-
-  final _isDoneCompleter = Completer();
+  final _isDoneCompleter = Completer<int>();
   final BuildTargetManager _buildTargetManager;
   final _pool = Pool(1);
   final Serializers _serializers;
   final ChangeProvider _changeProvider;
   late final Timer _timeout;
-
   HttpServer? _server;
   final DaemonBuilder _builder;
   // Channels that are interested in the current build.
   var _interestedChannels = <WebSocketChannel>{};
-
   final _subs = <StreamSubscription>[];
-
   final _outputStreamController = StreamController<ServerLog>();
   late final Stream<ServerLog> _logs;
-
   Server(
     this._builder,
     Duration timeout,
@@ -61,9 +56,7 @@ class Server {
             BuildTargetManager(shouldBuildOverride: shouldBuild) {
     _logs = _outputStreamController.stream;
     _forwardData();
-
     _handleChanges(changeProvider.changes);
-
     // Stop the server if nobody connects.
     _timeout = Timer(timeout, () async {
       if (_buildTargetManager.isEmpty) {
@@ -72,7 +65,8 @@ class Server {
     });
   }
 
-  Future<void> get onDone => _isDoneCompleter.future;
+  /// Returns exit code.
+  Future<int> get onDone => _isDoneCompleter.future;
 
   /// Starts listening for build daemon clients.
   Future<int> listen() async {
@@ -98,7 +92,6 @@ class Server {
         _removeChannel(channel);
       });
     });
-
     var server = _server = await HttpMultiServer.loopback(0);
     // Serve requests in an error zone to prevent failures
     // when running from another error zone.
@@ -124,7 +117,7 @@ class Server {
       await sub.cancel();
     }
     await _outputStreamController.close();
-    if (!_isDoneCompleter.isCompleted) _isDoneCompleter.complete();
+    if (!_isDoneCompleter.isCompleted) _isDoneCompleter.complete(failureType);
   }
 
   Future<void> _build(
@@ -134,7 +127,6 @@ class Server {
             buildTargets.expand(_buildTargetManager.channels).toSet();
         return _builder.build(buildTargets, changes);
       });
-
   void _forwardData() {
     _subs
       ..add(_builder.logs.listen((log) {
@@ -147,14 +139,11 @@ class Server {
         // Don't serialize or send changed assets if the client isn't interested
         // in them.
         String? message, messageWithoutChangedAssets;
-
         for (var channel in _interestedChannels) {
           var targets = _buildTargetManager.targetsFor(channel);
           var wantsChangedAssets = targets
               .any((e) => e is DefaultBuildTarget && e.reportChangedAssets);
-
           String messageForChannel;
-
           if (wantsChangedAssets) {
             messageForChannel =
                 message ??= jsonEncode(_serializers.serialize(status));
@@ -163,7 +152,6 @@ class Server {
                 _serializers
                     .serialize(status.rebuild((b) => b.changedAssets = null)));
           }
-
           channel.sink.add(messageForChannel);
         }
       }))
@@ -183,7 +171,15 @@ class Server {
       var buildTargets = _buildTargetManager.targetsForChanges(changes);
       if (buildTargets.isEmpty) return;
       await _build(buildTargets, changes);
-    }).listen((_) {}));
+    }).listen((_) {}, onError: (e) {
+      stop(
+          message: 'Error in file change event: $e',
+          failureType: fileChangeEventErrorCode);
+    }, onDone: () {
+      stop(
+          message: 'File change stream closed',
+          failureType: fileChangeStreamClosedErrorCode);
+    }));
   }
 
   void _removeChannel(WebSocketChannel channel) async {

build_daemon/pubspec.yaml
@@ -1,5 +1,5 @@
 name: build_daemon
-version:  3.1.0
+version:  3.1.1-dev
 description: A daemon for running Dart builds.
 repository: https://github.com/dart-lang/build/tree/master/build_daemon
 

build_daemon/lib/daemon.dart
@@ -25,7 +25,7 @@ import 'src/server.dart';
 class Daemon {
   final String _workingDirectory;
   final RandomAccessFile? _lock;
-  final _doneCompleter = Completer();
+  final _doneCompleter = Completer<int>();
 
   Server? _server;
   StreamSubscription? _sub;
@@ -34,7 +34,8 @@ class Daemon {
       : _workingDirectory = workingDirectory,
         _lock = _tryGetLock(workingDirectory);
 
-  Future<void> get onDone => _doneCompleter.future;
+  /// Returns exit code.
+  Future<int> get onDone => _doneCompleter.future;
 
   Future<void> stop({String message = '', int failureType = 0}) =>
       _server!.stop(message: message, failureType: failureType);
@@ -65,7 +66,7 @@ class Daemon {
     ChangeProvider changeProvider, {
     Serializers? serializersOverride,
     bool Function(BuildTarget, Iterable<WatchEvent>)? shouldBuild,
-    Duration timeout = const Duration(seconds: 30),
+    Duration timeout = defaultIdleTimeout,
   }) async {
     if (_server != null || _lock == null) return;
     _handleGracefulExit();
@@ -83,12 +84,12 @@ class Daemon {
     var port = await server.listen();
     _createPortFile(port);
 
-    unawaited(server.onDone.then((_) async {
-      await _cleanUp();
+    unawaited(server.onDone.then((exitCode) async {
+      await _cleanUp(exitCode);
     }));
   }
 
-  Future<void> _cleanUp() async {
+  Future<void> _cleanUp(int exitCode) async {
     await _server?.stop();
     await _sub?.cancel();
     // We need to close the lock prior to deleting the file.
@@ -97,7 +98,7 @@ class Daemon {
     if (workspace.existsSync()) {
       workspace.deleteSync(recursive: true);
     }
-    if (!_doneCompleter.isCompleted) _doneCompleter.complete();
+    if (!_doneCompleter.isCompleted) _doneCompleter.complete(exitCode);
   }
 
   void _createPortFile(int port) =>

build_daemon/test/daemon_test.dart
@@ -1,10 +1,10 @@
 // Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
 @OnPlatform({
   'windows': Skip('Directories cant be deleted while processes are still open')
 })
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:isolate';
@@ -17,17 +17,16 @@ import 'package:test/test.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
 import 'package:uuid/uuid.dart';
 
+final defaultIdleTimeoutSec = defaultIdleTimeout.inSeconds;
 void main() {
   var testDaemons = <Process>[];
   var testWorkspaces = <String>[];
   var uuid = Uuid();
-
   group('Daemon', () {
     setUp(() {
       testDaemons.clear();
       testWorkspaces.clear();
     });
-
     tearDown(() async {
       for (var testDaemon in testDaemons) {
         testDaemon.kill(ProcessSignal.sigkill);
@@ -39,7 +38,6 @@ void main() {
         }
       }
     });
-
     test('can be stopped', () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
@@ -52,32 +50,29 @@ void main() {
       expect(daemon.onDone, completes);
       await daemon.stop();
     });
-
     test('can run if no other daemon is running', () async {
       var workspace = uuid.v1();
       var daemon = await _runDaemon(workspace);
       testDaemons.add(daemon);
       expect(await _statusOf(daemon), 'RUNNING');
     });
-
     test('shuts down if no client connects', () async {
       var workspace = uuid.v1();
       var daemon = await _runDaemon(workspace, timeout: 1);
       testDaemons.add(daemon);
       expect(await daemon.exitCode, isNotNull);
     });
-
     test('can not run if another daemon is running in the same workspace',
         () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
-      var daemonOne = await _runDaemon(workspace);
-      expect(await _statusOf(daemonOne), 'RUNNING');
+      var daemonOne =
+          await _runDaemon(workspace, timeout: defaultIdleTimeoutSec * 2);
+      expect(await _statusOf(daemonOne, logPrefix: 'one'), 'RUNNING');
       var daemonTwo = await _runDaemon(workspace);
       testDaemons.addAll([daemonOne, daemonTwo]);
-      expect(await _statusOf(daemonTwo), 'ALREADY RUNNING');
-    });
-
+      expect(await _statusOf(daemonTwo, logPrefix: 'two'), 'ALREADY RUNNING');
+    }, timeout: Timeout.factor(2));
     test('can run if another daemon is running in a different workspace',
         () async {
       var workspace1 = uuid.v1();
@@ -89,17 +84,15 @@ void main() {
       testDaemons.addAll([daemonOne, daemonTwo]);
       expect(await _statusOf(daemonTwo), 'RUNNING');
     }, timeout: Timeout.factor(2));
-
     test('can start two daemons at the same time', () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
       var daemonOne = await _runDaemon(workspace);
+      expect(await _statusOf(daemonOne), 'RUNNING');
       var daemonTwo = await _runDaemon(workspace);
-      expect([await _statusOf(daemonOne), await _statusOf(daemonTwo)],
-          containsAll(['RUNNING', 'ALREADY RUNNING']));
+      expect(await _statusOf(daemonTwo), 'ALREADY RUNNING');
       testDaemons.addAll([daemonOne, daemonTwo]);
     }, timeout: Timeout.factor(2));
-
     test('logs the version when running', () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
@@ -108,13 +101,11 @@ void main() {
       expect(await _statusOf(daemon), 'RUNNING');
       expect(await Daemon(workspace).runningVersion(), currentVersion);
     });
-
     test('does not set the current version if not running', () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
       expect(await Daemon(workspace).runningVersion(), null);
     });
-
     test('logs the options when running', () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
@@ -124,13 +115,11 @@ void main() {
       expect(
           (await Daemon(workspace).currentOptions()).contains('foo'), isTrue);
     });
-
     test('does not log the options if not running', () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
       expect((await Daemon(workspace).currentOptions()).isEmpty, isTrue);
     });
-
     test('cleans up after itself', () async {
       var workspace = uuid.v1();
       testWorkspaces.add(workspace);
@@ -143,50 +132,88 @@ void main() {
       await daemon.exitCode;
       expect(Directory(daemonWorkspace(workspace)).existsSync(), isFalse);
     });
+    test('daemon stops after file changes stream has error', () async {
+      var workspace = uuid.v1();
+      testWorkspaces.add(workspace);
+      var daemon =
+          await _runDaemon(workspace, errorChangeProviderAfterNSeconds: 1);
+      expect(await _statusOf(daemon), 'RUNNING');
+      await daemon.exitCode;
+      expect(Directory(daemonWorkspace(workspace)).existsSync(), isFalse);
+    });
+    test('daemon stops after file changes stream is closed', () async {
+      var workspace = uuid.v1();
+      testWorkspaces.add(workspace);
+      var daemon =
+          await _runDaemon(workspace, closeChangeProviderAfterNSeconds: 1);
+      expect(await _statusOf(daemon), 'RUNNING');
+      await daemon.exitCode;
+      expect(Directory(daemonWorkspace(workspace)).existsSync(), isFalse);
+    });
   });
 }
 
 /// Returns the daemon status.
-///
-/// If the status is null, the stderr ir returned.
-Future<String> _statusOf(Process daemon) async {
-  String? status;
-  try {
-    status = await daemon.stdout
-        .transform(utf8.decoder)
-        .transform(const LineSplitter())
-        .firstWhere((line) => line == 'RUNNING' || line == 'ALREADY RUNNING');
-  } catch (_) {}
-  status ??= (await daemon.stderr
-          .transform(utf8.decoder)
-          .transform(const LineSplitter())
-          .toList())
-      .join('\n');
-  return status;
+Future<String> _statusOf(Process daemon, {String logPrefix = ''}) async {
+  final statusCompleter = Completer<String>();
+  daemon.stdout
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .listen((line) {
+    print('$logPrefix [STDOUT] $line');
+    if (!statusCompleter.isCompleted &&
+        (line == 'RUNNING' || line == 'ALREADY RUNNING')) {
+      statusCompleter.complete(line);
+    }
+  });
+  daemon.stderr
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .listen((line) {
+    print('$logPrefix [STDERR] $line');
+  });
+  return statusCompleter.future;
 }
 
-Future<Process> _runDaemon(var workspace, {int timeout = 30}) async {
+Future<Process> _runDaemon(String workspace,
+    {int timeout = -1,
+    int errorChangeProviderAfterNSeconds = -1,
+    int closeChangeProviderAfterNSeconds = -1}) async {
+  timeout = timeout > 0 ? timeout : defaultIdleTimeoutSec;
   await d.file('test.dart', '''
+    // @dart=2.9
     import 'package:build_daemon/daemon.dart';
     import 'package:build_daemon/daemon_builder.dart';
     import 'package:build_daemon/client.dart';
     import 'package:build_daemon/src/fakes/fake_builder.dart';
     import 'package:build_daemon/src/fakes/fake_change_provider.dart';
-
     main() async {
       var options = ['foo'].toSet();
       var timeout = Duration(seconds: $timeout);
       var daemon = Daemon('$workspace');
+      var changeProvider = FakeChangeProvider();
       if(daemon.hasLock) {
         await daemon.start(
           options,
           FakeDaemonBuilder(),
-          FakeChangeProvider(),
+          changeProvider,
           timeout: timeout);
         // Real implementations of the daemon usually have
         // non-trivial set up time.
         await Future.delayed(Duration(seconds: 1));
         print('RUNNING');
+        if ($errorChangeProviderAfterNSeconds > 0) {
+          Future.delayed(Duration(seconds: $errorChangeProviderAfterNSeconds),
+            () {
+              changeProvider.changeStreamController.addError('ERROR');
+            });
+        }
+        if ($closeChangeProviderAfterNSeconds > 0) {
+          Future.delayed(Duration(seconds: $closeChangeProviderAfterNSeconds),
+            () {
+              changeProvider.changeStreamController.close();
+            });
+        }
       }else{
         // Mimic the behavior of actual daemon implementations.
         var version = await daemon.runningVersion();
@@ -195,7 +222,6 @@ Future<Process> _runDaemon(var workspace, {int timeout = 30}) async {
       }
     }
       ''').create();
-
   var args = [
     ...Platform.executableArguments,
     if (!Platform.executableArguments
@@ -205,6 +231,5 @@ Future<Process> _runDaemon(var workspace, {int timeout = 30}) async {
   ];
   var process = await Process.start(Platform.resolvedExecutable, args,
       workingDirectory: d.sandbox);
-
   return process;
 }

commit dbab06b604c4e76800a97a7fedbfd065c3171b75

Build daemon ChangeProvider cleanup (#3470)
Removes all methods from ChangeProvider, and extract only the relevant ones into separate subclasses which are now public (AutoChangeProvider vs ManualChangeProvider).

Previously these modes were conflated and the different impls provided essentially no-op implementations of the non-applicable apis.

This should fix the failures we are seeing on the bots, because the daemon was shutting down early due to an empty stream given by the manual change provider. This was broken in #3411, but we only recently published that change, and weren't using dependency overrides for the tests that were actually broken, so we only saw it after the publish.

build_daemon/lib/change_provider.dart
@@ -6,13 +6,12 @@ import 'dart:async';
 
 import 'package:watcher/watcher.dart' show WatchEvent;
 
-abstract class ChangeProvider {
-  /// Returns a list of file changes.
-  ///
-  /// Called immediately before a manual build. If the list is empty a no-op
-  /// build of all tracked targets will be attempted.
-  Future<List<WatchEvent>> collectChanges();
+/// Marker class for automatic and manual change providers, essentially acts
+/// as a union type.
+abstract class ChangeProvider {}
 
+/// A change provider which provides a stream of changes.
+abstract class AutoChangeProvider implements ChangeProvider {
   /// A stream of file changes.
   ///
   /// A build is triggered upon each stream event.
@@ -20,4 +19,13 @@ abstract class ChangeProvider {
   /// If multiple files change together then they should be sent in the same
   /// event. Otherwise, at least two builds will be triggered.
   Stream<List<WatchEvent>> get changes;
+}
+
+/// A change provider which returns a list of changes on demand.
+abstract class ManualChangeProvider implements ChangeProvider {
+  /// Returns a list of file changes.
+  ///
+  /// Called immediately before a manual build. If the list is empty a no-op
+  /// build of all tracked targets will be attempted.
+  Future<List<WatchEvent>> collectChanges();
 }

build_daemon/lib/constants.dart
@@ -36,7 +36,7 @@ const logStartMarker = 'BUILD DAEMON LOG START';
 const logEndMarker = 'BUILD DAEMON LOG END';
 
 // TODO(grouma) - use pubspec version when this is open sourced.
-const currentVersion = '8';
+const currentVersion = '9';
 
 var _username = Platform.environment['USER'] ?? '';
 String daemonWorkspace(String workingDirectory) {

build_daemon/lib/src/fakes/fake_change_provider.dart
@@ -6,7 +6,7 @@ import 'dart:async';
 import 'package:build_daemon/change_provider.dart';
 import 'package:watcher/watcher.dart' show WatchEvent;
 
-class FakeChangeProvider implements ChangeProvider {
+class FakeChangeProvider implements AutoChangeProvider, ManualChangeProvider {
   final changeStreamController = StreamController<List<WatchEvent>>();
   @override
   Stream<List<WatchEvent>> get changes => changeStreamController.stream;

build_daemon/lib/src/server.dart
@@ -56,7 +56,9 @@ class Server {
             BuildTargetManager(shouldBuildOverride: shouldBuild) {
     _logs = _outputStreamController.stream;
     _forwardData();
-    _handleChanges(changeProvider.changes);
+    if (changeProvider is AutoChangeProvider) {
+      _handleChanges(changeProvider.changes);
+    }
     // Stop the server if nobody connects.
     _timeout = Timer(timeout, () async {
       if (_buildTargetManager.isEmpty) {
@@ -82,7 +84,12 @@ class Server {
         if (request is BuildTargetRequest) {
           _buildTargetManager.addBuildTarget(request.target, channel);
         } else if (request is BuildRequest) {
-          var changes = await _changeProvider.collectChanges();
+          // We can only get explicit build requests if we have a manual change
+          // provider.
+          var changeProvider = _changeProvider;
+          var changes = changeProvider is ManualChangeProvider
+              ? await changeProvider.collectChanges()
+              : <WatchEvent>[];
           var targets = changes.isEmpty
               ? _buildTargetManager.targets
               : _buildTargetManager.targetsForChanges(changes);

build_daemon/pubspec.yaml
@@ -1,5 +1,5 @@
 name: build_daemon
-version:  3.1.1
+version:  4.0.0-dev
 description: A daemon for running Dart builds.
 repository: https://github.com/dart-lang/build/tree/master/build_daemon
 
@@ -29,3 +29,7 @@ dev_dependencies:
   test: ^1.16.0
   test_descriptor: ^2.0.0
   uuid: ^3.0.0
+
+dependency_overrides:
+  build_runner:
+    path: ../build_runner

build_runner/lib/src/daemon/change_providers.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:build_daemon/change_provider.dart';
 // ignore: implementation_imports
 import 'package:build_runner_core/src/asset_graph/graph.dart';
@@ -11,25 +13,20 @@ import 'package:watcher/watcher.dart' show WatchEvent;
 
 /// Continually updates the [changes] stream as watch events are seen on the
 /// input stream.
-///
-/// The [collectChanges] method is a no-op for this implementation.
-class AutoChangeProvider implements ChangeProvider {
+class AutoChangeProviderImpl implements AutoChangeProvider {
   @override
   final Stream<List<WatchEvent>> changes;
 
-  AutoChangeProvider(this.changes);
-
-  @override
-  Future<List<WatchEvent>> collectChanges() async => [];
+  AutoChangeProviderImpl(this.changes);
 }
 
 /// Computes changes with a file scan when requested by a call to
 /// [collectChanges].
-class ManualChangeProvider implements ChangeProvider {
+class ManualChangeProviderImpl implements ManualChangeProvider {
   final AssetGraph _assetGraph;
   final AssetTracker _assetTracker;
 
-  ManualChangeProvider(this._assetTracker, this._assetGraph);
+  ManualChangeProviderImpl(this._assetTracker, this._assetGraph);
 
   @override
   Future<List<WatchEvent>> collectChanges() async {
@@ -37,7 +34,4 @@ class ManualChangeProvider implements ChangeProvider {
     return List.of(updates.entries
         .map((entry) => WatchEvent(entry.value, '${entry.key}')));
   }
-
-  @override
-  Stream<List<WatchEvent>> get changes => Stream.empty();
 }

build_runner/lib/src/daemon/daemon_builder.dart
@@ -249,8 +249,8 @@ class BuildRunnerDaemonBuilder implements DaemonBuilder {
         .debounceBuffer(buildOptions.debounceDelay);
 
     var changeProvider = daemonOptions.buildMode == BuildMode.Auto
-        ? AutoChangeProvider(graphEvents())
-        : ManualChangeProvider(
+        ? AutoChangeProviderImpl(graphEvents())
+        : ManualChangeProviderImpl(
             AssetTracker(daemonEnvironment.reader, buildOptions.targetGraph),
             builder.assetGraph);
 

build_runner/pubspec.yaml
@@ -17,7 +17,7 @@ dependencies:
   analyzer: '>=4.4.0 <6.0.0'
   build: ">=2.1.0 <2.4.0"
   build_config: ">=1.1.0 <1.2.0"
-  build_daemon: ^3.1.0
+  build_daemon: ^4.0.0
   build_resolvers: ^2.0.0
   build_runner_core: ^7.2.0
   code_builder: ^4.2.0