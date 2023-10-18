9a5235004cc4c34a4a0e763a139ca76441a551e9
Ben Vercammen <benvercammen@gmail.com>
2023-10-18 11:05:43 +0200

    Still cannot get "enabled: false" to impact the generated "build.dart" file

--- _test/test/generated_script_integration_test.dart
+++ _test/test/generated_script_integration_test.dart
@@ -14,6 +14,7 @@ void main() {
     await runCommand(['generate-build-script']);
   });
 
+  // TODO: this test fails, because the "not_enabled_builder" still shows up...
   test('Generates a build script matching the golden', () {
     var generatedScript =
         File('.dart_tool/build/entrypoint/build.dart').readAsStringSync();
@@ -22,4 +23,17 @@ void main() {
         .replaceAll('\r', '');
     expect(generatedScript, expected);
   });
+
+
+  /// Nog een ander probleem:
+  ///  of zo lijkt toch:
+  ///   - er worden té veel builders binnengepakt obv dependencies...
+  ///   - maar nu: both scorekeeper_codege:serializer_generator and scorekeeper_codege:serializer_generator may output lib/proto.config.json
+  ///
+  /// ik DENK dat het probleem als volgt is:
+  ///  - scorekeeper_example_domain wordt in tests / dev_dependencies van scorekeeper_codegen binnengetrokken
+  ///  - scorekeeper_example_domain is ook een dependency van ????
+  ///   -> kan ik dependency tree opzoeken van een dart project?
+
+
 }
