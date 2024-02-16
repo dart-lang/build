92b6187307a165d9dba1218dd61fd934f84a3511
Jake Macdonald <jakemac@google.com>
2024-02-16 20:57:26 +0000

    add macro e2e test

---
+++ _test/test/macro_test.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+import 'package:_test/macros/debug_to_string.dart';
+
+void main() {
+  test('macro stuff is generated', () {
+    expect(User('Jill', 25).debugToString(), equals('''
+User {
+  name: Jill
+  age: 25
+}'''));
+  });
+}
+
+@DebugToString()
+class User {
+  final String name;
+  final int age;
+
+  User(this.name, this.age);
+}
