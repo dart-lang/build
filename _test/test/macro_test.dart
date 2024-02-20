109769bb37755f5efc569a790b7b336c1709de82
Jake Macdonald <jakemac@google.com>
2024-02-20 19:07:00 +0000

    provide a valid package config and extra dependencies for macro compilation

--- _test/test/macro_test.dart
+++ _test/test/macro_test.dart
@@ -2,9 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:_test/macros/debug_to_string.dart';
+
 import 'package:test/test.dart';
 
-import 'package:_test/macros/debug_to_string.dart';
 
 void main() {
   test('macro stuff is generated', () {
