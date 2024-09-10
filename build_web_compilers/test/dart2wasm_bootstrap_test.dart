0653d8211d3b12da1cc1ca99ff22044b7b3945a8
Simon Binder <oss@simonbinder.eu>
2024-09-10 21:41:25 +0200

    Add missing copyright header

--- build_web_compilers/test/dart2wasm_bootstrap_test.dart
+++ build_web_compilers/test/dart2wasm_bootstrap_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:build/build.dart';
 import 'package:build_modules/build_modules.dart';
 import 'package:build_test/build_test.dart';
