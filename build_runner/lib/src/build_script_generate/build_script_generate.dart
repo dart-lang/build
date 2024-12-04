cf1fbbb1b8f813d62339de4b15778f2d9e41b284
Kevin Moore <kevmoo@google.com>
2024-12-04 13:59:20 -0600

    fix analyzer

--- build_runner/lib/src/build_script_generate/build_script_generate.dart
+++ build_runner/lib/src/build_script_generate/build_script_generate.dart
@@ -47,7 +47,8 @@ Future<String> _generateBuildScript() async {
       ..writeln('// ignore_for_file: directives_ordering')
       ..writeln(library.accept(emitter));
 
-    return DartFormatter().format(content.toString());
+    return DartFormatter(languageVersion: DartFormatter.latestLanguageVersion)
+        .format(content.toString());
   } on FormatterException {
     _log.severe('Generated build script could not be parsed.\n'
         'This is likely caused by a misconfigured builder definition.');
