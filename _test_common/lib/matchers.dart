374732be4416b95711bc09feb47f6fa7f69f5dcc
David Morgan <davidmorgan@google.com>
2025-03-11 10:23:56 +0100

    Use `built_value` for comparison for tests.

--- _test_common/lib/matchers.dart
+++ _test_common/lib/matchers.dart
@@ -8,7 +8,6 @@ import 'package:build_runner_core/src/asset_graph/exceptions.dart';
 import 'package:build_runner_core/src/asset_graph/graph.dart';
 // ignore: implementation_imports
 import 'package:build_runner_core/src/asset_graph/node.dart';
-import 'package:package_config/package_config_types.dart';
 import 'package:test/test.dart';
 
 final Matcher throwsCorruptedException = throwsA(
@@ -25,217 +24,50 @@ Matcher equalsAssetGraph(
 class _AssetGraphMatcher extends Matcher {
   final AssetGraph _expected;
   final bool checkPreviousInputsDigest;
+  final Matcher _matcher;
 
-  const _AssetGraphMatcher(this._expected, this.checkPreviousInputsDigest);
+  _AssetGraphMatcher(this._expected, this.checkPreviousInputsDigest)
+    : _matcher = equals(
+        _graphToList(
+          _expected,
+          checkPreviousInputsDigest: checkPreviousInputsDigest,
+        ),
+      );
+
+  /// Converts [graph] to a list of [AssetNode], sorted by ID, for comparison.
+  ///
+  /// If [checkPreviousInputsDigest] is false, removes `previousInputDigest`
+  /// fields so they won't be compared.
+  static List<AssetNode> _graphToList(
+    AssetGraph graph, {
+    required bool checkPreviousInputsDigest,
+  }) {
+    final result = <AssetNode>[];
+    for (var node in graph.allNodes) {
+      if (!checkPreviousInputsDigest) {
+        if (node.type == NodeType.generated) {
+          node = node.rebuild(
+            (b) => b.generatedNodeState.previousInputsDigest = null,
+          );
+        }
+        if (node.type == NodeType.postProcessAnchor) {
+          node = node.rebuild(
+            (b) => b..postProcessAnchorNodeState.previousInputsDigest = null,
+          );
+        }
+      }
+      result.add(node);
+    }
+    return result..sort((a, b) => a.id.toString().compareTo(b.id.toString()));
+  }
 
   @override
   bool matches(dynamic item, Map<dynamic, dynamic> matchState) {
     if (item is! AssetGraph) return false;
-    var matches = true;
-    if (item.allNodes.length != _expected.allNodes.length) matches = false;
-    for (var node in item.allNodes) {
-      var expectedNode = _expected.get(node.id);
-      if (node.isDeleted != expectedNode?.isDeleted) {
-        matchState['IsDeleted of ${node.id}'] = [
-          node.isDeleted,
-          expectedNode?.isDeleted,
-        ];
-        matches = false;
-      }
-      if (node.runtimeType != expectedNode.runtimeType) {
-        matchState['RuntimeType'] = [
-          node.runtimeType,
-          expectedNode.runtimeType,
-        ];
-        matches = false;
-      }
-      if (expectedNode == null || expectedNode.id != node.id) {
-        matchState['AssetId'] = [node.id, expectedNode!.id];
-        matches = false;
-      }
-      if (!unorderedEquals(node.outputs).matches(expectedNode.outputs, {})) {
-        matchState['Outputs of ${node.id}'] = [
-          node.outputs,
-          expectedNode.outputs,
-        ];
-        matches = false;
-      }
-      if (!unorderedEquals(
-        node.primaryOutputs,
-      ).matches(expectedNode.primaryOutputs, {})) {
-        matchState['Primary outputs of ${node.id}'] = [
-          node.primaryOutputs,
-          expectedNode.primaryOutputs,
-        ];
-        matches = false;
-      }
-      if (!unorderedEquals(
-        node.anchorOutputs,
-      ).matches(expectedNode.anchorOutputs, {})) {
-        matchState['Anchor outputs of ${node.id}'] = [
-          node.anchorOutputs,
-          expectedNode.anchorOutputs,
-        ];
-        matches = false;
-      }
-      if (node.lastKnownDigest != expectedNode.lastKnownDigest) {
-        matchState['Digest of ${node.id}'] = [
-          node.lastKnownDigest,
-          expectedNode.lastKnownDigest,
-        ];
-        matches = false;
-      }
-      if (node.type == NodeType.generated) {
-        if (expectedNode.type == NodeType.generated) {
-          final configuration = node.generatedNodeConfiguration!;
-          final expectedConfiguration =
-              expectedNode.generatedNodeConfiguration!;
-
-          if (configuration.primaryInput !=
-              expectedConfiguration.primaryInput) {
-            matchState['primaryInput of ${node.id}'] = [
-              configuration.primaryInput,
-              expectedConfiguration.primaryInput,
-            ];
-            matches = false;
-          }
-
-          final state = node.generatedNodeState!;
-          final expectedState = expectedNode.generatedNodeState!;
-
-          if (state.pendingBuildAction != expectedState.pendingBuildAction) {
-            matchState['pendingBuildAction of ${node.id}'] = [
-              state.pendingBuildAction,
-              expectedState.pendingBuildAction,
-            ];
-            matches = false;
-          }
-          if (!unorderedEquals(
-            state.inputs,
-          ).matches(expectedState.inputs, {})) {
-            matchState['Inputs of ${node.id}'] = [
-              state.inputs,
-              expectedState.inputs,
-            ];
-            matches = false;
-          }
-
-          if (state.wasOutput != expectedState.wasOutput) {
-            matchState['wasOutput of ${node.id}'] = [
-              state.wasOutput,
-              expectedState.wasOutput,
-            ];
-            matches = false;
-          }
-          if (state.isFailure != expectedState.isFailure) {
-            matchState['isFailure of ${node.id}'] = [
-              state.isFailure,
-              expectedState.isFailure,
-            ];
-            matches = false;
-          }
-          if (checkPreviousInputsDigest &&
-              state.previousInputsDigest !=
-                  expectedState.previousInputsDigest) {
-            matchState['previousInputDigest of ${node.id}'] = [
-              state.previousInputsDigest,
-              expectedState.previousInputsDigest,
-            ];
-            matches = false;
-          }
-        }
-      } else if (node.type == NodeType.glob) {
-        if (expectedNode.type == NodeType.glob) {
-          final state = node.globNodeState!;
-          final expectedState = expectedNode.globNodeState!;
-
-          if (state.pendingBuildAction != expectedState.pendingBuildAction) {
-            matchState['pendingBuildAction of ${node.id}'] = [
-              state.pendingBuildAction,
-              expectedState.pendingBuildAction,
-            ];
-            matches = false;
-          }
-          if (!unorderedEquals(
-            state.inputs,
-          ).matches(expectedState.inputs, {})) {
-            matchState['Inputs of ${node.id}'] = [
-              state.inputs,
-              expectedState.inputs,
-            ];
-            matches = false;
-          }
-
-          if (!unorderedEquals(
-            state.results,
-          ).matches(expectedState.results, {})) {
-            matchState['results of ${node.id}'] = [
-              state.results,
-              expectedState.results,
-            ];
-            matches = false;
-          }
-
-          final configuration = node.globNodeConfiguration!;
-          final expectedConfiguration = expectedNode.globNodeConfiguration!;
-          if (configuration.glob != expectedConfiguration.glob) {
-            matchState['glob of ${node.id}'] = [
-              configuration.glob,
-              expectedConfiguration.glob,
-            ];
-            matches = false;
-          }
-        }
-      } else if (node.type == NodeType.postProcessAnchor) {
-        if (expectedNode.type == NodeType.postProcessAnchor) {
-          final nodeConfiguration = node.postProcessAnchorNodeConfiguration!;
-          final expectedNodeConfiguration =
-              expectedNode.postProcessAnchorNodeConfiguration!;
-          if (nodeConfiguration.actionNumber !=
-              expectedNodeConfiguration.actionNumber) {
-            matchState['actionNumber of ${node.id}'] = [
-              nodeConfiguration.actionNumber,
-              expectedNodeConfiguration.actionNumber,
-            ];
-            matches = false;
-          }
-          if (nodeConfiguration.builderOptionsId !=
-              expectedNodeConfiguration.builderOptionsId) {
-            matchState['builderOptionsId of ${node.id}'] = [
-              nodeConfiguration.builderOptionsId,
-              expectedNodeConfiguration.builderOptionsId,
-            ];
-            matches = false;
-          }
-          final nodeState = node.postProcessAnchorNodeState!;
-          final expectedNodeState = expectedNode.postProcessAnchorNodeState!;
-          if (checkPreviousInputsDigest &&
-              nodeState.previousInputsDigest !=
-                  expectedNodeState.previousInputsDigest) {
-            matchState['previousInputsDigest of ${node.id}'] = [
-              nodeState.previousInputsDigest,
-              expectedNodeState.previousInputsDigest,
-            ];
-            matches = false;
-          }
-          if (nodeConfiguration.primaryInput !=
-              expectedNodeConfiguration.primaryInput) {
-            matchState['primaryInput of ${node.id}'] = [
-              nodeConfiguration.primaryInput,
-              expectedNodeConfiguration.primaryInput,
-            ];
-            matches = false;
-          }
-        }
-      }
-    }
-    if (!equals(_expected.packageLanguageVersions).matches(
-      item.packageLanguageVersions,
-      matchState['packageLanguageVersions'] = <String, LanguageVersion?>{},
-    )) {
-      matches = false;
-    }
-    return matches;
+    return _matcher.matches(
+      _graphToList(item, checkPreviousInputsDigest: checkPreviousInputsDigest),
+      matchState,
+    );
   }
 
   @override
@@ -248,13 +80,15 @@ class _AssetGraphMatcher extends Matcher {
     Description mismatchDescription,
     Map matchState,
     bool verbose,
-  ) {
-    matchState.forEach((k, v) {
-      if (v is List) {
-        mismatchDescription.add('$k: got ${v[0]} but expected ${v[1]}');
-      }
-    });
-
-    return mismatchDescription;
-  }
+  ) => _matcher.describeMismatch(
+    item is AssetGraph
+        ? _graphToList(
+          item,
+          checkPreviousInputsDigest: checkPreviousInputsDigest,
+        )
+        : '(not an AssetGraph!) $item',
+    mismatchDescription,
+    matchState,
+    verbose,
+  );
 }
