a04d1472217a55685ef5d6f0fbf4e9123a8e40e6
David Morgan <davidmorgan@google.com>
2025-09-05 08:38:48 +0200

    Use `testBuilders` instead of internal `testPhases` in most tests.

--- build_runner/test/generate/build_error_test.dart
+++ build_runner/test/generate/build_error_test.dart
@@ -6,7 +6,7 @@ import 'dart:async';
 
 import 'package:build/build.dart';
 import 'package:build_runner/src/generate/build_result.dart';
-import 'package:build_runner/src/package_graph/apply_builders.dart';
+import 'package:build_test/build_test.dart';
 import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
@@ -14,130 +14,116 @@ import '../common/common.dart';
 
 void main() {
   test('should fail if a severe logged', () async {
-    await testPhases(
-      [applyToRoot(_LoggingBuilder(Level.SEVERE))],
-      {'a|lib/a.dart': ''},
-      packageGraph: buildPackageGraph({rootPackage('a'): []}),
-      checkBuildStatus: true,
-      status: BuildStatus.failure,
-      outputs: {'a|lib/a.dart.empty': ''},
+    expect(
+      (await testBuilders(
+        [_LoggingBuilder(Level.SEVERE)],
+        {'a|lib/a.dart': ''},
+        outputs: {'a|lib/a.dart.empty': ''},
+      )).buildResult.status,
+      BuildStatus.failure,
     );
   });
 
   test('should fail if a severe was logged on a previous build', () async {
-    var packageGraph = buildPackageGraph({rootPackage('a'): []});
-    var builder = _LoggingBuilder(Level.SEVERE);
-    var builders = [applyToRoot(builder)];
-    final result = await testPhases(
-      builders,
+    var result = await testBuilders(
+      [_LoggingBuilder(Level.SEVERE)],
       {'a|lib/a.dart': ''},
-      packageGraph: packageGraph,
-      checkBuildStatus: true,
-      status: BuildStatus.failure,
       outputs: {'a|lib/a.dart.empty': ''},
     );
-    await testPhases(
-      builders,
-      {},
-      resumeFrom: result,
-      packageGraph: packageGraph,
-      checkBuildStatus: true,
-      status: BuildStatus.failure,
-      outputs: {},
+    expect(result.buildResult.status, BuildStatus.failure);
+
+    final builder = _LoggingBuilder(Level.SEVERE);
+    result = await testBuilders(
+      [builder],
+      {'a|lib/a.dart': ''},
+      // Resume from the previous builld.
+      readerWriter: result.readerWriter,
+      outputs: {'a|lib/a.dart.empty': ''},
     );
+    expect(result.buildResult.status, BuildStatus.failure);
+    // Should have failed without actually building again.
+    expect(builder.built, false);
   });
 
   test(
     'should succeed if a severe log is fixed on a subsequent build',
     () async {
-      var packageGraph = buildPackageGraph({rootPackage('a'): []});
-      var builder = _LoggingBuilder(Level.SEVERE);
-      var builders = [applyToRoot(builder)];
-      final result = await testPhases(
-        builders,
+      var result = await testBuilders(
+        [_LoggingBuilder(Level.SEVERE)],
         {'a|lib/a.dart': ''},
-        packageGraph: packageGraph,
-        checkBuildStatus: true,
-        status: BuildStatus.failure,
         outputs: {'a|lib/a.dart.empty': ''},
       );
-      builder.level = Level.WARNING;
-      await testPhases(
-        builders,
+      expect(result.buildResult.status, BuildStatus.failure);
+
+      result = await testBuilders(
+        [_LoggingBuilder(Level.WARNING)],
         {'a|lib/a.dart': 'changed'},
-        resumeFrom: result,
-        packageGraph: packageGraph,
-        checkBuildStatus: true,
-        status: BuildStatus.success,
+        // Resume from the previous builld.
+        readerWriter: result.readerWriter,
         outputs: {'a|lib/a.dart.empty': ''},
       );
+      expect(result.buildResult.status, BuildStatus.success);
     },
   );
 
   test('should fail if an exception is thrown', () async {
-    await testPhases(
-      [
-        applyToRoot(
-          TestBuilder(build: (_, _) => throw Exception('Some build failure')),
-        ),
-      ],
-      {'a|lib/a.txt': ''},
-      packageGraph: buildPackageGraph({rootPackage('a'): []}),
-      status: BuildStatus.failure,
+    expect(
+      (await testBuilders(
+        [TestBuilder(build: (_, _) => throw Exception('Some build failure'))],
+        {'a|lib/a.txt': ''},
+      )).buildResult.status,
+      BuildStatus.failure,
     );
   });
 
   test(
     'should throw an exception if a read is attempted on a failed file',
     () async {
-      await testPhases(
+      final result = await testBuilders(
         [
-          applyToRoot(
-            TestBuilder(
-              buildExtensions: replaceExtension('.txt', '.failed'),
-              build: (buildStep, _) async {
-                await buildStep.writeAsString(
-                  buildStep.inputId.changeExtension('.failed'),
-                  'failed',
-                );
-                log.severe('Wrote an output then failed');
-              },
-            ),
+          TestBuilder(
+            buildExtensions: replaceExtension('.txt', '.failed'),
+            build: (buildStep, _) async {
+              await buildStep.writeAsString(
+                buildStep.inputId.changeExtension('.failed'),
+                'failed',
+              );
+              log.severe('Wrote an output then failed');
+            },
           ),
-          applyToRoot(
-            TestBuilder(
-              buildExtensions: replaceExtension('.txt', '.success'),
-              build: expectAsync2((buildStep, _) async {
-                // Attempts to read the file that came from a failing build step
-                // and hides the exception.
-                var failedFile = buildStep.inputId.changeExtension('.failed');
-                await expectLater(
-                  buildStep.readAsString(failedFile),
-                  throwsA(anything),
-                );
-                await buildStep.writeAsString(
-                  buildStep.inputId.changeExtension('.success'),
-                  'success',
-                );
-              }),
-            ),
+          TestBuilder(
+            buildExtensions: replaceExtension('.txt', '.success'),
+            build: expectAsync2((buildStep, _) async {
+              // Attempts to read the file that came from a failing build step
+              // and hides the exception.
+              var failedFile = buildStep.inputId.changeExtension('.failed');
+              await expectLater(
+                buildStep.readAsString(failedFile),
+                throwsA(anything),
+              );
+              await buildStep.writeAsString(
+                buildStep.inputId.changeExtension('.success'),
+                'success',
+              );
+            }),
           ),
         ],
         {'a|lib/a.txt': ''},
-        packageGraph: buildPackageGraph({rootPackage('a'): []}),
-        status: BuildStatus.failure,
       );
+      expect(result.buildResult.status, BuildStatus.failure);
     },
   );
 }
 
 class _LoggingBuilder implements Builder {
   Level level;
+  bool built = false;
 
   _LoggingBuilder(this.level);
 
   @override
   Future<void> build(BuildStep buildStep) async {
+    built = true;
     log.log(level, buildStep.inputId.toString());
     await buildStep.canRead(buildStep.inputId);
     await buildStep.writeAsString(buildStep.inputId.addExtension('.empty'), '');
