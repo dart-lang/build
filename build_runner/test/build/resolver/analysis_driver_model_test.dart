668c0790f3e8d82d4be91c36bfd9f1f540cc0b1b
iliya <iliyazelenkog@gmail.com>
2026-04-09 08:57:51 +0300

    test(build_runner): cover incremental analyzer source updates

---
+++ build_runner/test/build/resolver/analysis_driver_model_test.dart
@@ -0,0 +1,92 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:build/build.dart';
+import 'package:build_runner/src/build/asset_graph/graph.dart';
+import 'package:build_runner/src/build/resolver/analysis_driver_filesystem.dart';
+import 'package:build_runner/src/build/resolver/analysis_driver_model.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late AnalysisDriverModel model;
+
+  setUp(() {
+    model = AnalysisDriverModel();
+  });
+
+  tearDown(() {
+    model.endBuildAndUnlock();
+  });
+
+  group('AnalysisDriverModel.takeLockAndStartBuild', () {
+    test('initial build clears all cached source paths', () async {
+      model.filesystem.write('/a/lib/a.dart', 'class A {}');
+      model.filesystem.write('/a/lib/b.dart', 'class B {}');
+      model.filesystem.clearChangedPaths();
+
+      await model.takeLockAndStartBuild(
+        AssetGraph.emptyForTesting(),
+        isInitialBuild: true,
+        changedSources: const {},
+        deletedSources: const {},
+      );
+
+      expect(model.filesystem.exists('/a/lib/a.dart'), isFalse);
+      expect(model.filesystem.exists('/a/lib/b.dart'), isFalse);
+      expect(model.filesystem.changedPaths, {
+        '/a/lib/a.dart',
+        '/a/lib/b.dart',
+      });
+    });
+
+    test('incremental build keeps unchanged sources cached', () async {
+      model.filesystem.write('/a/lib/a.dart', 'class A {}');
+      model.filesystem.clearChangedPaths();
+
+      await model.takeLockAndStartBuild(
+        AssetGraph.emptyForTesting(),
+        isInitialBuild: false,
+        changedSources: const {},
+        deletedSources: const {},
+      );
+
+      expect(model.filesystem.exists('/a/lib/a.dart'), isTrue);
+      expect(model.filesystem.read('/a/lib/a.dart'), 'class A {}');
+      expect(model.filesystem.changedPaths, isEmpty);
+    });
+
+    test(
+      'incremental build removes only changed and deleted sources',
+      () async {
+      model.filesystem.write('/a/lib/changed.dart', 'class Changed {}');
+      model.filesystem.write('/a/lib/deleted.dart', 'class Deleted {}');
+      model.filesystem.write('/a/lib/unchanged.dart', 'class Unchanged {}');
+      model.filesystem.clearChangedPaths();
+
+      await model.takeLockAndStartBuild(
+        AssetGraph.emptyForTesting(),
+        isInitialBuild: false,
+        changedSources: {AssetId.parse('a|lib/changed.dart')},
+        deletedSources: {AssetId.parse('a|lib/deleted.dart')},
+      );
+
+      expect(model.filesystem.exists('/a/lib/changed.dart'), isFalse);
+      expect(model.filesystem.exists('/a/lib/deleted.dart'), isFalse);
+      expect(model.filesystem.exists('/a/lib/unchanged.dart'), isTrue);
+      expect(model.filesystem.changedPaths, {
+        '/a/lib/changed.dart',
+        '/a/lib/deleted.dart',
+      });
+      },
+    );
+  });
+}
+
+extension _AnalysisDriverFilesystemExtensions on AnalysisDriverFilesystem {
+  void write(String path, String content) {
+    writeContent(
+      BuildRunnerFileContent(path, true, content, content.hashCode.toString()),
+    );
+  }
+}
