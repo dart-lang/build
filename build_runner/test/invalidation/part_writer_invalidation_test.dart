6e5547f93523364ae19e0481edb4ea61fc6651f8
davidmorgan <davidmorgan@google.com>
2026-07-17 14:27:13 +0200

    test: show builder resolving shared part doesn't incremental build on part change

--- build_runner/test/invalidation/part_writer_invalidation_test.dart
+++ build_runner/test/invalidation/part_writer_invalidation_test.dart
@@ -41,4 +41,58 @@ void main() {
       expect(await tester.build(), Result());
     });
   });
+
+  group('builder resolving shared part invalidation', () {
+    setUp(() {
+      tester.sources(['a.dart', 'b.dart', 'c.dart']);
+      // 'c.dart' resolves 'a.dart', which has a part contribution.
+      tester.importGraph({
+        'c.dart': ['a.dart'],
+      });
+      // Builder 1 writes a part for 'a.dart'. It depends on 'b.dart', so changing 'b.dart' forces it to rerun
+      // and generate a new _br_/a.dart.
+      tester.builder(from: '.dart', to: '.1.dart')
+        ..readsOther('b.dart')
+        ..writesPart('// part from builder 1');
+
+      // Builder 2 resolves 'a.dart'. It's applied to 'c.dart'.
+      tester.builder(from: '.dart', to: '.2.dart')
+        ..reads('.dart') // reads 'c.dart'
+        ..resolvesOther('a.dart')
+        ..writes('.2.dart'); // writes 'c.2.dart'
+    });
+
+    test('initial build writes the part and the output', () async {
+      final result = await tester.build();
+      expect(
+        result,
+        Result(
+          written: [
+            '_br_/a.dart',
+            '_br_/b.dart',
+            '_br_/c.dart',
+            'a.2.dart',
+            'b.2.dart',
+            'c.2.dart'
+          ],
+        ),
+      );
+    });
+
+    test('change b.dart, part is rewritten, but c.2.dart is NOT rebuilt', () async {
+      await tester.build();
+      // change b.dart invalidates builder 1 on a.dart, rewriting _br_/a.dart.
+      // But c.2.dart currently DOES NOT get rebuilt!
+      expect(
+        await tester.build(change: 'b.dart'),
+        Result(written: ['_br_/a.dart', '_br_/b.dart', '_br_/c.dart', 'b.2.dart']),
+      );
+      /* TODO: The correct behavior should trigger a rebuild of c.2.dart:
+      expect(
+        await tester.build(change: 'b.dart'),
+        Result(written: ['_br_/a.dart', '_br_/b.dart', '_br_/c.dart', 'b.2.dart', 'c.2.dart']),
+      );
+      */
+    });
+  });
 }
