5312d309ee32a70d2aa1651aa36b1c677d608f5e
Morgan :) <davidmorgan@google.com>
2026-07-29 11:50:40 +0200

    Fix race condition during daemon shutdown/startup. (#5045)

--- build_daemon/test/daemon_test.dart
+++ build_daemon/test/daemon_test.dart
@@ -216,6 +216,24 @@ void _testGroup({required bool useSharedPath}) {
         Directory(
           daemonWorkspace(workspace, daemonSharedPath: daemonSharedPath),
         ).existsSync(),
+        isTrue,
+      );
+      expect(
+        File(
+          portFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
+        isFalse,
+      );
+      expect(
+        File(
+          versionFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
+        isFalse,
+      );
+      expect(
+        File(
+          optionsFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
         isFalse,
       );
     });
@@ -232,6 +250,24 @@ void _testGroup({required bool useSharedPath}) {
         Directory(
           daemonWorkspace(workspace, daemonSharedPath: daemonSharedPath),
         ).existsSync(),
+        isTrue,
+      );
+      expect(
+        File(
+          portFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
+        isFalse,
+      );
+      expect(
+        File(
+          versionFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
+        isFalse,
+      );
+      expect(
+        File(
+          optionsFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
         isFalse,
       );
     });
@@ -247,6 +283,24 @@ void _testGroup({required bool useSharedPath}) {
       expect(
         Directory(
           daemonWorkspace(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
+        isTrue,
+      );
+      expect(
+        File(
+          portFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
+        isFalse,
+      );
+      expect(
+        File(
+          versionFilePath(workspace, daemonSharedPath: daemonSharedPath),
+        ).existsSync(),
+        isFalse,
+      );
+      expect(
+        File(
+          optionsFilePath(workspace, daemonSharedPath: daemonSharedPath),
         ).existsSync(),
         isFalse,
       );
