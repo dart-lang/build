f0f851596e5820007e207e58ee715fd8ce239a7c
Kevin Moore <kevmoo@google.com>
2026-08-18 17:20:49 +0000

    chore: remove orphaned builders.dart and inline daemon constants

--- build_runner/lib/src/commands/daemon/constants.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'package:build_daemon/constants.dart';
-import 'package:path/path.dart' as p;
-
-String assetServerPortFilePath(String workingDirectory) =>
-    p.join(daemonWorkspace(workingDirectory), '.asset_server_port');
