6fc8012065a45a9de7e9eb72d010b68c4cf089d5
davidmorgan <davidmorgan@google.com>
2026-08-31 15:43:36 +0200

    Evict deleted and displaced outputs from retainedOutputContents in BuildPlan._createIncremental.

--- build_runner/lib/src/build_plan/build_plan.dart
+++ build_runner/lib/src/build_plan/build_plan.dart
@@ -447,6 +447,9 @@ abstract class BuildPlan implements Built<BuildPlan, BuildPlanBuilder> {
     }
 
     final finalSources = buildInputs.sources.build();
+    for (final id in finalSources) {
+      buildInputs.retainedOutputContents.remove(id);
+    }
     for (final id in newCacheFiles) {
       if (!finalSources.contains(id)) {
         buildInputs.invalidOutputs.add(id);
@@ -472,6 +475,9 @@ abstract class BuildPlan implements Built<BuildPlan, BuildPlanBuilder> {
         .toSet();
     buildInputs.invalidOutputs.addAll(invalidOutputs);
     buildInputs.invalidOutputs.removeAll(finalSources);
+    for (final invalid in invalidOutputs) {
+      buildInputs.retainedOutputContents.remove(invalid);
+    }
 
     return BuildPlan((b) {
       b.buildSpec.replace(buildSpec);
