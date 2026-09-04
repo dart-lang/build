ff57ce3336785a9ca26c2bbf693e3da179bced35
Morgan :) <davidmorgan@google.com>
2026-09-04 10:07:18 +0200

    Add built_types packages to workspace and prefix internal packages (#5115)
    * Move internal built_types directories
    
    Move the internal built_types packages to prefix their directory names:
    - built_types/benchmark -> built_types/_built_value_benchmark
    - built_types/chat_example -> built_types/built_value_chat_example
    - built_types/end_to_end_test -> built_types/_built_value_end_to_end_test
    - built_types/example -> built_types/built_value_example
    
    This commit contains moves only.
    
    TAG=agy
    CONV=9eabb90a-3e69-4742-a07f-41be531b3bf3
    
    * Add built_types packages to workspace and update package names
    
    Update internal package names in built_types to _built_value_benchmark,
    built_value_chat_example, _built_value_end_to_end_test, and
    built_value_example. Update internal imports and pubspecs to use
    resolution: workspace. Add all seven built_types packages to the root
    workspace pubspec.
    
    TAG=agy
    CONV=9eabb90a-3e69-4742-a07f-41be531b3bf3

---
+++ built_types/built_value_chat_example/lib/client/http_client_connection.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2016, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:js_interop';
+
+import 'package:built_value_chat_example/client/client_connection.dart';
+import 'package:web/web.dart';
+
+/// [ClientConnection] using a web socket.
+class HttpClientConnection implements ClientConnection {
+  final WebSocket _websocket;
+  final StreamController<String> _streamController = StreamController<String>();
+
+  factory HttpClientConnection() {
+    return HttpClientConnection._(WebSocket('ws://${window.location.host}/ws'));
+  }
+
+  HttpClientConnection._(this._websocket) {
+    _websocket.onMessage.listen((message) {
+      _streamController.add(message.data as String);
+    });
+  }
+
+  @override
+  Stream<String> get dataFromServer => _streamController.stream;
+
+  @override
+  void sendToServer(String data) {
+    _websocket.send(data.toJS);
+  }
+}
